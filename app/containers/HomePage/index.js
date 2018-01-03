d55fd48b26a5e870a0f335a4a9231e7486466474
burcakulug <burcak.ulug@feisystems.com>
2018-01-03 16:49:25 -0500

    Add Golden Layout with placeholders to the home page container
    #17169:As a user, I can see a list of organizations in the OCP, so that I know all of the organizations in the system.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,86 +11,166 @@
 
 import React from 'react';
 // import { FormattedMessage } from 'react-intl';
-import { Card, CardHeader, CardText, Divider } from 'material-ui';
-// import messages from './messages';
-
 import styles from './HomePage.css';
 import Header from '../../components/Header/index';
 import SideBar from '../../components/SideBar/index';
+import GoldenLayout from '../../components/GoldenLayout/Loadable';
+import { createSampleComponent } from '../../components/SampleComponent';
 
+// import messages from './messages';
+
+const initialStateMetadata =
+  {
+    settings: {
+      hasHeaders: true,
+      constrainDragToContainer: false,
+      reorderEnabled: true,
+      selectionEnabled: false,
+      popoutWholeStack: false,
+      blockedPopoutsThrowError: true,
+      closePopoutsOnUnload: true,
+      showPopoutIcon: true,
+      showMaximiseIcon: true,
+      showCloseIcon: true,
+      responsiveMode: 'onload',
+      tabOverlapAllowance: 0,
+      reorderOnTabMenuClick: true,
+      tabControlOffset: 10,
+    },
+    dimensions: {
+      borderWidth: 5,
+      borderGrabWidth: 15,
+      minItemHeight: 10,
+      minItemWidth: 10,
+      headerHeight: 20,
+      dragProxyWidth: 300,
+      dragProxyHeight: 200,
+    },
+    labels: {
+      close: 'close',
+      maximise: 'maximise',
+      minimise: 'minimise',
+      popout: 'open in new window',
+      popin: 'pop in',
+      tabDropdown: 'additional tabs',
+    },
+    content: [{
+      type: 'row',
+      isClosable: true,
+      reorderEnabled: true,
+      title: '',
+      content: [{
+        type: 'column',
+        isClosable: true,
+        reorderEnabled: true,
+        title: '',
+        width: 50,
+        content: [{
+          type: 'stack',
+          header: {},
+          isClosable: true,
+          reorderEnabled: true,
+          title: '',
+          activeItemIndex: 0,
+          height: 50,
+          content: [{
+            title: 'Sample One',
+            type: 'component',
+            componentName: 'sample',
+            isClosable: true,
+            reorderEnabled: true,
+          },
+          ],
+        }, {
+          type: 'stack',
+          header: {},
+          isClosable: true,
+          reorderEnabled: true,
+          title: '',
+          activeItemIndex: 0,
+          width: 50,
+          height: 50,
+          content: [{
+            title: 'Sample Two',
+            type: 'component',
+            componentName: 'sample',
+            isClosable: true,
+            reorderEnabled: true,
+          },
+          ],
+        },
+        ],
+      }, {
+        type: 'column',
+        isClosable: true,
+        reorderEnabled: true,
+        title: '',
+        width: 50,
+        content: [{
+          type: 'stack',
+          header: {},
+          isClosable: true,
+          reorderEnabled: true,
+          title: '',
+          activeItemIndex: 0,
+          height: 50,
+          content: [{
+            title: 'Sample Three',
+            type: 'component',
+            componentName: 'sample',
+            isClosable: true,
+            reorderEnabled: true,
+          },
+          ],
+        }, {
+          type: 'stack',
+          width: 50,
+          height: 50,
+          isClosable: true,
+          reorderEnabled: true,
+          title: '',
+          activeItemIndex: 0,
+          content: [{
+            title: 'Sample Four',
+            type: 'component',
+            componentName: 'sample',
+            isClosable: true,
+            reorderEnabled: true,
+          },
+          ],
+        },
+        ],
+      },
+      ],
+    },
+    ],
+    isClosable: true,
+    reorderEnabled: true,
+    title: '',
+    openPopouts: [],
+    maximisedItemId: null,
+  };
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className={styles.container}>
-        <div className={`${styles.box} ${styles.header}`}>
+
+        <div className={`${styles.box} ${styles.header}`} >
           <Header />
         </div>
-        <div className={`${styles.box} ${styles.sidebar}`}>
+        <div className={`${styles.box} ${styles.sidebar}`} >
           <SideBar />
         </div>
-        <div className={`${styles.box} ${styles.task}`}>
-          <Card style={styles.taskCard}>
-            <CardHeader title="TASKS" />
-            <Divider />
-            <CardText>
-              <p>
-                There are many variations of passages of Lorem Ipsum available, but the
-                majority have suffered alteration in some form, by injected humour, or
-                randomised words which do not look even slightly believable. If you are going
-                to use a passage of Lorem Ipsum, you need to be sure there is not anything
-                embarrassing hidden in the middle of text. All the Lorem Ipsum generators on
-                the Internet tend to repeat predefined chunks as necessary, making this the
-                first true generator on the Internet. It uses a dictionary of over 200 Latin
-                words, combined with a handful of model sentence structures, to generate
-                Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore
-                always free from repetition, injected humour, or non-characteristic words
-                etc.
-              </p>
-            </CardText>
-          </Card>
-        </div>
-        <div className={`${styles.box} ${styles.consult}`}>
-          <Card className={styles.taskCard}>
-            <CardHeader title="OPEN CONSULTS" />s
-            <Divider />
-            <CardText>
-              <p>
-                There are many variations of passages of Lorem Ipsum available, but the
-                majority have suffered alteration in some form, by injected humour, or
-                randomised words which do not look even slightly believable. If you are going
-                to use a passage of Lorem Ipsum, you need to be sure there is not anything
-                embarrassing hidden in the middle of text. All the Lorem Ipsum generators on
-                the Internet tend to repeat predefined chunks as necessary, making this the
-                first true generator on the Internet. It uses a dictionary of over 200 Latin
-                words, combined with a handful of model sentence structures, to generate
-                Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore
-                always free from repetition, injected humour, or non-characteristic words
-                etc.
-              </p>
-            </CardText>
-          </Card>
-        </div>
-        <div className={`${styles.box} ${styles.request}`}>
-          <Card className={styles.requestCard}>
-            <CardHeader title="OPEN REQUESTS" />
-            <Divider />
-            <CardText>
-              <p>
-                There are many variations of passages of Lorem Ipsum available, but the
-                majority have suffered alteration in some form, by injected humour, or
-                randomised words which do not look even slightly believable. If you are going
-                to use a passage of Lorem Ipsum, you need to be sure there is not anything
-                embarrassing hidden in the middle of text. All the Lorem Ipsum generators on
-                the Internet tend to repeat predefined chunks as necessary, making this the
-                first true generator on the Internet. It uses a dictionary of over 200 Latin
-                words, combined with a handful of model sentence structures, to generate
-                Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore
-                always free from repetition, injected humour, or non-characteristic words
-                etc.
-              </p>
-            </CardText>
-          </Card>
+
+        <div>
+          <GoldenLayout
+            containerId="golden-home"
+            componentMetadata={[
+              { name: 'sample', text: 'sample', factoryMethod: createSampleComponent },
+            ]}
+            stateMetadata={initialStateMetadata}
+          />
         </div>
       </div>
     );
