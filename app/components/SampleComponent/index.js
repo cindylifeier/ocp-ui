2b60ab1b128fd0ff082ec41f06c20b9317a974d9
burcakulug <burcak.ulug@feisystems.com>
2018-01-04 14:34:46 -0500

    Utilize withContext HOC in SampleComponent and add i18n message support
    #17169:As a user, I can see a list of organizations in the OCP, so that I know all of the organizations in the system.

--- app/components/SampleComponent/index.js
+++ app/components/SampleComponent/index.js
@@ -6,12 +6,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import materialUiWrapperHOC from '../../utils/materialUiWrapperHOC';
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+import withContext from '../../utils/withContext';
 
 function SampleComponent() {
   return (
     <div>
+      <FormattedMessage {...messages.header} />
       <h1>
+        This is the sample component!
         This is the sample component!
       </h1>
     </div>
@@ -22,7 +26,7 @@ SampleComponent.propTypes = {};
 
 export function createSampleComponent(location) {
   ReactDOM.render(
-    materialUiWrapperHOC(SampleComponent),
+    withContext(SampleComponent),
     document.getElementById(location)
   );
 }
