3c2bbc180d0c3b86f2a76fe23b52a8eaea5dd060
burcakulug <burcak.ulug@feisystems.com>
2018-01-09 11:23:47 -0500

    Implement a generic refresh indicator for loading status
    #17169:As a user, I can see a list of organizations in the OCP, so that I know all of the organizations in the system.

---
+++ app/components/RefreshIndicatorLoading/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import RefreshIndicatorLoading from '../index';
+
+describe('<RefreshIndicatorLoading />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
