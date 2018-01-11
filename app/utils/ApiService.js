6a4cbf0649b63d6b0de8b2d94a3df14a6239cade
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-11 09:29:23 -0500

    Refactor ApiService to have just the fetch call.
    #17170 - As a user, I can see a list of locations in the OCP, so that I know all of the locations in the system

--- app/utils/ApiService.js
+++ app/utils/ApiService.js
@@ -1,34 +1,10 @@
 const ApiService = {};
 
-ApiService.getLocationsById = function (organizationId) {
-  const url = 'http://localhost:8444/organizations/'.concat(organizationId).concat('/locations');
-  return fetch(url)
-          .then((response) => response.json())
-          .then((json) => json)
-          .catch((error) => { throw error; });
-};
-
-ApiService.getLocationsByIdAndStatus = function (organizationId, status) {
-  const url = createUrl(organizationId, status);
+ApiService.getData = function getData(url) {
   return fetch(url)
     .then((response) => response.json())
     .then((json) => json)
     .catch((error) => { throw error; });
 };
 
-
-function createUrl(organizationId, status) {
-  const key = 'status=';
-  const page = 'page=1&';
-  let queryParams = '';
-  if (status && status.length === 1) {
-    queryParams = page.concat(key).concat(status[0]);
-  } else if (status && status.length === 2) {
-    queryParams = page.concat(key).concat(status[0]).concat(',').concat(status[1]);
-  } else if (status && status.length === 3) {
-    queryParams = page.concat(key).concat(status[0]).concat(',').concat(status[1]).concat(',').concat(status[2]);
-  }
-  return 'http://localhost:8444/organizations/'.concat(organizationId).concat('/locations?').concat(queryParams);
-}
-
-export { ApiService };
+export default ApiService;
