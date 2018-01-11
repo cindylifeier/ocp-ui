dff9824d119e23878e784223b639c1e9ad0102b0
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-10 19:40:58 -0500

    Implement function to generate query param url.
    #17170 - As a user, I can see a list of locations in the OCP, so that I know all of the locations in the system

--- app/utils/ApiService.js
+++ app/utils/ApiService.js
@@ -19,7 +19,15 @@ ApiService.getLocationsByIdAndStatus = function (organizationId, status) {
 
 function createUrl(organizationId, status) {
   const key = 'status=';
-  const queryParams = status.length() === 2 ? key.concat(status[0]).concat('&').key.concat(status[1]) : key.concat(status[0]);
+  const page = 'page=1&';
+  let queryParams = '';
+  if (status && status.length === 1) {
+    queryParams = page.concat(key).concat(status[0]);
+  } else if (status && status.length === 2) {
+    queryParams = page.concat(key).concat(status[0]).concat(',').concat(status[1]);
+  } else if (status && status.length === 3) {
+    queryParams = page.concat(key).concat(status[0]).concat(',').concat(status[1]).concat(',').concat(status[2]);
+  }
   return 'http://localhost:8444/organizations/'.concat(organizationId).concat('/locations?').concat(queryParams);
 }
 
