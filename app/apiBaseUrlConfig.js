ae48eb9372696141b846efaed49bd02cd6d35f61
JiahaoLi <Jiahao.Li@feisystems.com>
2018-01-12 16:43:57 -0500

    Fix incorrect api base url
    #17176:As a user, I can see a list of practitioners in the OCP, so that I know all of the practitioners in the system.

--- app/apiBaseUrlConfig.js
+++ app/apiBaseUrlConfig.js
@@ -2,7 +2,7 @@ function getApiBaseUrl() {
   let apiBaseUrl = 'http://localhost:8446/ocp-fis';
 
   if (process.env.NODE_ENV === 'production') {
-    apiBaseUrl = 'http://http://20.0.2.87:8446/ocp-fis';
+    apiBaseUrl = 'http://20.0.2.87:8446/ocp-fis';
   }
 
   return apiBaseUrl;
