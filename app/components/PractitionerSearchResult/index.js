044ad3a0a616b99a4e895b46c420147a9da1d8a3
JiahaoLi <Jiahao.Li@feisystems.com>
2018-01-12 11:55:45 -0500

    Implement presentational component to show searching practitioner result
    #17176:As a user, I can see a list of practitioners in the OCP, so that I know all of the practitioners in the system.

---
+++ app/components/PractitionerSearchResult/index.js
@@ -0,0 +1,81 @@
+/**
+ *
+ * PractitionerSearchResult
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import uniqueId from 'lodash/uniqueId';
+import RefreshIndicatorLoading from '../RefreshIndicatorLoading';
+import styles from './PractitionerSearchResult.css';
+
+// import styled from 'styled-components';
+function PractitionerSearchResult({ loading, error, searchResult }) {
+  if (loading) {
+    return <RefreshIndicatorLoading />;
+  }
+
+  if (error !== false) {
+    return (<p>No match search result.</p>);
+  }
+
+  if (searchResult !== false) {
+    return (
+      <div className={styles.table}>
+        <div className={styles.rowGridContainer}>
+          <div className={styles.cellGridHeaderItem}>FirstName</div>
+          <div className={styles.cellGridHeaderItem}>LastName</div>
+          <div className={styles.cellGridHeaderItem}>Status</div>
+          <div className={styles.cellGridHeaderItem}>Telecoms</div>
+          <div className={styles.cellGridHeaderItem}>Address</div>
+        </div>
+        {displayPractitionerSearchResult(searchResult)}
+      </div>
+    );
+  }
+
+  return (<div />);
+}
+
+function displayPractitionerSearchResult(practitioners) {
+  return (
+    practitioners && practitioners.map((practitioner) => (
+      <div key={uniqueId()} className={styles.rowGridContainer}>
+        <div className={styles.cellGridItem}>{practitioner.name[0].firstName ? practitioner.name[0].firstName : ''}</div>
+        <div className={styles.cellGridItem}>{practitioner.name[0].lastName ? practitioner.name[0].lastName : ''}</div>
+        <div className={styles.cellGridItem}>{practitioner.active ? 'Active' : 'Inactive'}</div>
+        <div className={styles.cellGridItem}>{getTelecom(practitioner)}</div>
+        <div className={styles.cellGridItem}>{getAddress(practitioner)}</div>
+      </div>
+    )));
+}
+
+// Todo: Refactor to make reuseable
+function getTelecom(practitioner) {
+  const telecoms = practitioner.telecoms;
+  let value;
+  for (let i = 0; i < telecoms.length; i += 1) {
+    if (telecoms[i].system === 'PHONE') {
+      value = telecoms[i].value;
+    }
+  }
+  return value;
+}
+
+function getAddress(practitioner) {
+  const address = practitioner.address[0];
+  let value;
+  if (address != null) {
+    value = `${address.line1} ${address.line2} ${address.city} ${address.stateCode} ${address.postalCode} ${address.countryCode}`;
+  }
+  return value;
+}
+
+PractitionerSearchResult.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.any,
+  searchResult: PropTypes.any,
+};
+
+export default PractitionerSearchResult;
