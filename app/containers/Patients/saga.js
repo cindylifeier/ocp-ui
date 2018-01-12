7c69f500a3d8433c1948beabba2a3187078b3306
CindyLiFei <cindy.li@feisystems.com>
2018-01-12 16:42:15 -0500

    Make api base url configurable
    # 17177 - As a user, I can see a list of patients in the OCP, so that I know all of the patients in the system

--- app/containers/Patients/saga.js
+++ app/containers/Patients/saga.js
@@ -1,9 +1,11 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import { LOAD_PATIENT_SEARCH_RESULT } from './constants';
 import { searchPatientsError, searchPatientsSuccess } from './actions';
+import getApiBaseUrl from '../../apiBaseUrlConfig';
 
 export function* loadSearchResult({ searchTerms, searchType, includeInactive }) {
-  const requestURL = `http://localhost:8446/ocp-fis/patients/search?value=${searchTerms}&showInactive=${includeInactive}&type=${searchType}`;
+  const apiBaseURL = getApiBaseUrl();
+  const requestURL = `${apiBaseURL}/patients/search?value=${searchTerms}&showInactive=${includeInactive}&type=${searchType}`;
 
   try {
     const searchPatientResult = yield fetch(requestURL).then((resp) => resp.json());
