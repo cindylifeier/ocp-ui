b111ae70fbb1ac30345f6e02402bbbd694fd3efa
JiahaoLi <Jiahao.Li@feisystems.com>
2018-01-12 11:32:09 -0500

    Configure saga to manage searching practitioners
    #17176:As a user, I can see a list of practitioners in the OCP, so that I know all of the practitioners in the system.

--- app/containers/Practitioners/saga.js
+++ app/containers/Practitioners/saga.js
@@ -1,6 +1,21 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 
-// Individual exports for testing
-export default function* defaultSaga() {
-  // See example in containers/HomePage/saga.js
+import { searchPractitionersError, searchPractitionersSuccess } from './actions';
+import { LOAD_PRACTITIONER_SEARCH_RESULT } from './constants';
+import request from '../../utils/request';
+
+export function* loadSearchResult({ searchTerms, searchType, includeInactive }) {
+  const query = `searchType=${searchType}&searchValue=${searchTerms}&showInactive=${includeInactive}`;
+  const requestURL = `http://localhost:8444/practitioners/search?${query}`;
+
+  try {
+    const searchPractitionerResult = yield call(request, requestURL);
+    yield put(searchPractitionersSuccess(searchPractitionerResult, searchTerms));
+  } catch (error) {
+    yield put(searchPractitionersError(error));
+  }
+}
+
+export default function* watchFetchPractitioners() {
+  yield takeLatest(LOAD_PRACTITIONER_SEARCH_RESULT, loadSearchResult);
 }
