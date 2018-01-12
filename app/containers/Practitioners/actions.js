533a1d8b8c138e4c09bee45cc4b3ba125d8aaaae
JiahaoLi <Jiahao.Li@feisystems.com>
2018-01-12 11:26:18 -0500

    Add search practitioner actions
    #17176:As a user, I can see a list of practitioners in the OCP, so that I know all of the practitioners in the system.

--- app/containers/Practitioners/actions.js
+++ app/containers/Practitioners/actions.js
@@ -4,12 +4,28 @@
  *
  */
 
-import {
-  DEFAULT_ACTION,
-} from './constants';
+import { LOAD_PRACTITIONER_SEARCH_RESULT, SEARCH_PRACTITIONERS_ERROR, SEARCH_PRACTITIONERS_SUCCESS } from './constants';
 
-export function defaultAction() {
+export function loadPractitionerSearchResult(searchTerms, searchType, includeInactive) {
   return {
-    type: DEFAULT_ACTION,
+    type: LOAD_PRACTITIONER_SEARCH_RESULT,
+    searchTerms,
+    searchType,
+    includeInactive,
+  };
+}
+
+export function searchPractitionersSuccess(searchResult, searchTerms) {
+  return {
+    type: SEARCH_PRACTITIONERS_SUCCESS,
+    searchResult,
+    searchTerms,
+  };
+}
+
+export function searchPractitionersError(error) {
+  return {
+    type: SEARCH_PRACTITIONERS_ERROR,
+    error,
   };
 }
--- app/containers/Practitioners/constants.js
+++ app/containers/Practitioners/constants.js
@@ -4,4 +4,6 @@
  *
  */
 
-export const DEFAULT_ACTION = 'app/Practitioners/DEFAULT_ACTION';
+export const LOAD_PRACTITIONER_SEARCH_RESULT = 'app/Practitioners/LOAD_PRACTITIONER_SEARCH_RESULT';
+export const SEARCH_PRACTITIONERS_SUCCESS = 'app/Practitioners/SEARCH_PRACTITIONERS_SUCCESS';
+export const SEARCH_PRACTITIONERS_ERROR = 'app/Practitioners/SEARCH_PRACTITIONERS_ERROR';
