7d6818906115abf97beec76c4a6f287a072cc42a
CindyLiFei <cindy.li@feisystems.com>
2018-01-12 15:41:04 -0500

    Update server url to ocp-ui-api
    # 17177 - As a user, I can see a list of patients in the OCP, so that I know all of the patients in the system

--- app/containers/Patients/saga.js
+++ app/containers/Patients/saga.js
@@ -3,7 +3,7 @@ import { LOAD_PATIENT_SEARCH_RESULT } from './constants';
 import { searchPatientsError, searchPatientsSuccess } from './actions';
 
 export function* loadSearchResult({ searchTerms, searchType, includeInactive }) {
-  const requestURL = `http://localhost:8444/patients/search?value=${searchTerms}&showInactive=${includeInactive}&type=${searchType}`;
+  const requestURL = `http://localhost:8446/ocp-fis/patients/search?value=${searchTerms}&showInactive=${includeInactive}&type=${searchType}`;
 
   try {
     const searchPatientResult = yield fetch(requestURL).then((resp) => resp.json());
