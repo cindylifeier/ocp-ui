26f65b5c2d8c29c82346b187bf9bf16651292a5f
JiahaoLi <Jiahao.Li@feisystems.com>
2018-01-12 12:00:21 -0500

    Define reducer to handle searching practitioners
    #17176:As a user, I can see a list of practitioners in the OCP, so that I know all of the practitioners in the system.

--- app/containers/Practitioners/reducer.js
+++ app/containers/Practitioners/reducer.js
@@ -6,17 +6,36 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
+  LOAD_PRACTITIONER_SEARCH_RESULT, SEARCH_PRACTITIONERS_ERROR,
+  SEARCH_PRACTITIONERS_SUCCESS,
 } from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  loading: false,
+  error: false,
+  searchPractitioners: {
+    result: false,
+  },
+});
 
 function practitionersReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
+    case LOAD_PRACTITIONER_SEARCH_RESULT:
+      return state
+        .set('loading', true)
+        .set('error', false)
+        .setIn(['searchPractitioners', 'result'], false);
+    case SEARCH_PRACTITIONERS_SUCCESS:
+      return state
+        .setIn(['searchPractitioners', 'result'], action.searchResult)
+        .set('loading', false);
+    case SEARCH_PRACTITIONERS_ERROR:
+      return state
+        .set('error', action.error)
+        .set('loading', false);
+    default: {
       return state;
-    default:
-      return state;
+    }
   }
 }
 
