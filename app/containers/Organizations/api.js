8fac72b9e811ed828564826dd7b87d4314274aed
JiahaoLi <Jiahao.Li@feisystems.com>
2018-01-17 11:58:40 -0500

    Refactor to use watcher pattern on managing organizations saga
    #17176:As a user, I can see a list of practitioners in the OCP, so that I know all of the practitioners in the system.

--- app/containers/Organizations/api.js
+++ app/containers/Organizations/api.js
@@ -1,18 +1,4 @@
-import queryString from '../../utils/queryString';
-import getApiBaseUrl from '../../apiBaseUrlConfig';
-import { DEFAULT_PAGE_SIZE } from './constants';
-
-const baseApiUrl = getApiBaseUrl();
-
-export default function getOrganizations(searchValue, showInactive, searchType, page) {
-  const size = DEFAULT_PAGE_SIZE;
-  const params = queryString({ searchValue, showInactive, searchType, size, page });
-  return fetch(`${baseApiUrl}/organizations/search${params}`)
-    .then((resp) => resp.json())
-    .then(mapToFrontendOrganizationList);
-}
-
-function mapToFrontendOrganizationList(resp) {
+export function mapToFrontendOrganizationList(resp) {
   const mappReponse = resp;
   const organizationElements = (resp.elements || []).map(mapToFrontendOrganization);
   mappReponse.elements = organizationElements;
