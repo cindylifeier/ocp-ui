a7fd259e3608d98cc4ff837f650c03c893eba324
JiahaoLi <Jiahao.Li@feisystems.com>
2018-01-17 21:29:59 -0500

    Implement api service for practitioners container component
    #17176:As a user, I can see a list of practitioners in the OCP, so that I know all of the practitioners in the system.

--- app/containers/Practitioners/saga.js
+++ app/containers/Practitioners/saga.js
@@ -1,17 +1,12 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 
 import { searchPractitionersError, searchPractitionersSuccess } from './actions';
-import { DEFAULT_PAGE_SIZE, LOAD_PRACTITIONER_SEARCH_RESULT } from './constants';
-import request from '../../utils/request';
-import getApiBaseUrl from '../../apiBaseUrlConfig';
+import { LOAD_PRACTITIONER_SEARCH_RESULT } from './constants';
+import searchPractitioners from './api';
 
 export function* fetchSearchResult({ searchTerms, searchType, includeInactive, currentPage }) {
-  const query = `searchType=${searchType}&searchValue=${searchTerms}&showInactive=${includeInactive}&page=${currentPage}&size=${DEFAULT_PAGE_SIZE}`;
-  const apiBaseURL = getApiBaseUrl();
-  const requestURL = `${apiBaseURL}/practitioners/search?${query}`;
-
   try {
-    const searchPractitionerResult = yield call(request, requestURL);
+    const searchPractitionerResult = yield call(searchPractitioners, searchTerms, searchType, includeInactive, currentPage);
     yield put(searchPractitionersSuccess(searchPractitionerResult, searchTerms, searchType, includeInactive));
   } catch (error) {
     yield put(searchPractitionersError(error));
