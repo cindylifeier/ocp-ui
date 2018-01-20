88ae02ff127e429a7ae4130daf4d9701de10361f
Teng Luo <Teng.Luo@feisystems.com>
2018-01-19 22:28:19 -0500

    Fixed position header and footer
    #17157: As a user, I can create an organization, so that organizations can exist in the OCP

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -8,11 +8,11 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import AppBar from 'material-ui/AppBar';
 import messages from './messages';
-
+import styles from './styles.css';
 function Footer() {
   const year = new Date().getFullYear();
   return (
-    <div>
+    <div className={styles.footer}>
       <AppBar
         title={<FormattedMessage {...messages.header} values={{ year }} />}
         showMenuIconButton={false}
