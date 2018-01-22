4cabcffbf8e6c9be1e05f8792fd2cf9d6307794a
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-22 11:33:54 -0500

    Fix typo in identifier system constant and rearrange code
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

--- app/utils/LookupService.js
+++ app/utils/LookupService.js
@@ -2,7 +2,7 @@ import { select } from 'redux-saga/effects';
 import {
   ADDRESSTYPE,
   ADDRESSUSE,
-  IDENTIFIERSYSTEMS,
+  IDENTIFIERSYSTEM,
   LOCATIONSTATUS,
   LOCATIONTYPE,
   TELECOMSYSTEM,
@@ -35,29 +35,36 @@ function* isLookupTypeInStore(selectedTypes, verifyingType, makeSelectorType) {
 
 export function* getLookupTypesNotInStore(action) {
   const lookupTypesNotInStore = [];
-  if (!(yield isLookupTypeInStore(action.lookupTypes, USPSSTATES, makeSelectUspsStates)) && action.lookupTypes.includes(USPSSTATES)) {
+  if (!(yield isLookupTypeInStore(action.lookupTypes, USPSSTATES, makeSelectUspsStates))
+    && action.lookupTypes.includes(USPSSTATES)) {
     lookupTypesNotInStore.push(USPSSTATES);
   }
   if (!(yield isLookupTypeInStore(action.lookupTypes, LOCATIONSTATUS, makeSelectLocationStatuses))
     && action.lookupTypes.includes(LOCATIONSTATUS)) {
     lookupTypesNotInStore.push(LOCATIONSTATUS);
   }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, LOCATIONTYPE, makeSelectLocationTypes)) && action.lookupTypes.includes(LOCATIONTYPE)) {
+  if (!(yield isLookupTypeInStore(action.lookupTypes, LOCATIONTYPE, makeSelectLocationTypes))
+    && action.lookupTypes.includes(LOCATIONTYPE)) {
     lookupTypesNotInStore.push(LOCATIONTYPE);
   }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, ADDRESSTYPE, makeSelectAddressTypes)) && action.lookupTypes.includes(ADDRESSTYPE)) {
+  if (!(yield isLookupTypeInStore(action.lookupTypes, ADDRESSTYPE, makeSelectAddressTypes))
+    && action.lookupTypes.includes(ADDRESSTYPE)) {
     lookupTypesNotInStore.push(ADDRESSTYPE);
   }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, ADDRESSUSE, makeSelectAddressUses)) && action.lookupTypes.includes(ADDRESSUSE)) {
+  if (!(yield isLookupTypeInStore(action.lookupTypes, ADDRESSUSE, makeSelectAddressUses))
+    && action.lookupTypes.includes(ADDRESSUSE)) {
     lookupTypesNotInStore.push(ADDRESSUSE);
   }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, IDENTIFIERSYSTEMS, makeSelectIdentifierSystems)) && action.lookupTypes.includes(IDENTIFIERSYSTEMS)) {
-    lookupTypesNotInStore.push(IDENTIFIERSYSTEMS);
+  if (!(yield isLookupTypeInStore(action.lookupTypes, IDENTIFIERSYSTEM, makeSelectIdentifierSystems))
+    && action.lookupTypes.includes(IDENTIFIERSYSTEM)) {
+    lookupTypesNotInStore.push(IDENTIFIERSYSTEM);
   }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, TELECOMSYSTEM, makeSelectTelecomSystems)) && action.lookupTypes.includes(TELECOMSYSTEM)) {
+  if (!(yield isLookupTypeInStore(action.lookupTypes, TELECOMSYSTEM, makeSelectTelecomSystems))
+    && action.lookupTypes.includes(TELECOMSYSTEM)) {
     lookupTypesNotInStore.push(TELECOMSYSTEM);
   }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, TELECOMUSE, makeSelectTelecomUses)) && action.lookupTypes.includes(TELECOMUSE)) {
+  if (!(yield isLookupTypeInStore(action.lookupTypes, TELECOMUSE, makeSelectTelecomUses))
+    && action.lookupTypes.includes(TELECOMUSE)) {
     lookupTypesNotInStore.push(TELECOMUSE);
   }
 
