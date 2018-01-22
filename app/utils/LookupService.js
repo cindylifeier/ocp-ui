134ecc0d458458ba320ea49b9c522ee2cc2125ca
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-21 22:07:11 -0500

    Implement a generalise lookup service
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

---
+++ app/utils/LookupService.js
@@ -0,0 +1,72 @@
+import { select } from 'redux-saga/effects';
+import {
+  ADDRESSTYPE,
+  ADDRESSUSE,
+  IDENTIFIERSYSTEMS,
+  LOCATIONSTATUS,
+  LOCATIONTYPE,
+  TELECOMSYSTEM,
+  TELECOMUSE,
+  USPSSTATES,
+} from '../containers/App/constants';
+import {
+  makeSelectAddressTypes,
+  makeSelectAddressUses,
+  makeSelectIdentifierSystems,
+  makeSelectLocationStatuses,
+  makeSelectLocationTypes,
+  makeSelectTelecomSystems,
+  makeSelectTelecomUses,
+  makeSelectUspsStates,
+} from '../containers/App/selectors';
+import getApiBaseUrl from '../apiBaseUrlConfig';
+import request from './request';
+
+
+function* isLookupTypeInStore(selectedTypes, verifyingType, makeSelectorType) {
+  if (selectedTypes.includes(verifyingType)) {
+    const dataFromStore = yield select(makeSelectorType());
+    if (dataFromStore && dataFromStore.length > 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export function* getLookupTypesNotInStore(action) {
+  const lookupTypesNotInStore = [];
+  if (!(yield isLookupTypeInStore(action.lookupTypes, USPSSTATES, makeSelectUspsStates)) && action.lookupTypes.includes(USPSSTATES)) {
+    lookupTypesNotInStore.push(USPSSTATES);
+  }
+  if (!(yield isLookupTypeInStore(action.lookupTypes, LOCATIONSTATUS, makeSelectLocationStatuses))
+    && action.lookupTypes.includes(LOCATIONSTATUS)) {
+    lookupTypesNotInStore.push(LOCATIONSTATUS);
+  }
+  if (!(yield isLookupTypeInStore(action.lookupTypes, LOCATIONTYPE, makeSelectLocationTypes)) && action.lookupTypes.includes(LOCATIONTYPE)) {
+    lookupTypesNotInStore.push(LOCATIONTYPE);
+  }
+  if (!(yield isLookupTypeInStore(action.lookupTypes, ADDRESSTYPE, makeSelectAddressTypes)) && action.lookupTypes.includes(ADDRESSTYPE)) {
+    lookupTypesNotInStore.push(ADDRESSTYPE);
+  }
+  if (!(yield isLookupTypeInStore(action.lookupTypes, ADDRESSUSE, makeSelectAddressUses)) && action.lookupTypes.includes(ADDRESSUSE)) {
+    lookupTypesNotInStore.push(ADDRESSUSE);
+  }
+  if (!(yield isLookupTypeInStore(action.lookupTypes, IDENTIFIERSYSTEMS, makeSelectIdentifierSystems)) && action.lookupTypes.includes(IDENTIFIERSYSTEMS)) {
+    lookupTypesNotInStore.push(IDENTIFIERSYSTEMS);
+  }
+  if (!(yield isLookupTypeInStore(action.lookupTypes, TELECOMSYSTEM, makeSelectTelecomSystems)) && action.lookupTypes.includes(TELECOMSYSTEM)) {
+    lookupTypesNotInStore.push(TELECOMSYSTEM);
+  }
+  if (!(yield isLookupTypeInStore(action.lookupTypes, TELECOMUSE, makeSelectTelecomUses)) && action.lookupTypes.includes(TELECOMUSE)) {
+    lookupTypesNotInStore.push(TELECOMUSE);
+  }
+
+  return lookupTypesNotInStore;
+}
+
+export function getLookups(lookupTypes) {
+  const apiBaseURL = getApiBaseUrl();
+  const lookupKeyList = lookupTypes.join();
+  const requestURL = `${apiBaseURL}/lookups?lookUpTypeList=${lookupKeyList}`;
+  return request(requestURL);
+}
