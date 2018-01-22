481c6b5d6e81f4c6da84cc2ca09c1228257355d0
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-21 22:08:27 -0500

    Delete initial implementation
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

--- app/containers/LocationCreateEdit/api.js
+++ app/containers/LocationCreateEdit/api.js
@@ -1,10 +1,1 @@
-import getApiBaseUrl from '../../apiBaseUrlConfig';
-import request from '../../utils/request';
 
-const apiBaseURL = getApiBaseUrl();
-
-export default function getLookups(lookupTypes) {
-  const lookupKeyList = lookupTypes.join();
-  const requestURL = `${apiBaseURL}/lookups?lookUpTypeList=${lookupKeyList}`;
-  return request(requestURL);
-}
