ff95d1bc917407a6b22eab9d885767267769e276
JiahaoLi <Jiahao.Li@feisystems.com>
2018-01-23 14:09:14 -0500

    Remove unnecessary initial values for create practitioner form
    #17174:As a user, I can enroll a new practitioner, so that practitioners can exist in the OCP

--- app/components/ManagePractitioner/index.js
+++ app/components/ManagePractitioner/index.js
@@ -13,23 +13,6 @@ import ManagePractitionerForm from './ManagePractitionerForm';
 import messages from './messages';
 import { TEXT_MIN_LENGTH } from '../../containers/ManagePractitionerPage/constants';
 
-const initialValues = {
-  firstName: '',
-  middleName: '',
-  lastName: '',
-  roleType: '',
-  identifierType: '',
-  identifierValue: '',
-  address1: '',
-  address2: '',
-  city: '',
-  state: '',
-  postalCode: '',
-  country: '',
-  email: '',
-  phone: '',
-};
-
 function ManagePractitioner(props) {
   const minimumLength = TEXT_MIN_LENGTH;
   const postalCodePattern = new RegExp('^\\d{5}(?:[-\\s]\\d{4})?$');
@@ -37,7 +20,6 @@ function ManagePractitioner(props) {
   return (
     <div>
       <Formik
-        initialValues={initialValues}
         onSubmit={(values, actions) => {
           onSave(values);
           actions.setSubmitting(false);
