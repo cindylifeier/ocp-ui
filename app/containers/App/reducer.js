385dbef9039f43feadeb6280e0ce302646e2bd30
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-22 19:21:44 -0500

    Update reducer to use the same lookup types as in selector
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -13,41 +13,42 @@
 import { fromJS } from 'immutable';
 
 import {
-  GET_LOCATION_LOOKUPS,
+  GET_LOOKUPS,
   GET_LOOKUPS_SUCCESS,
-  GET_LOOKUPS_ERROR,
+  GET_LOOKUPS_ERROR, USPSSTATES, LOCATIONTYPE, LOCATIONSTATUS, ADDRESSTYPE, ADDRESSUSE, IDENTIFIERSYSTEM, TELECOMSYSTEM,
+  TELECOMUSE,
 } from './constants';
 
 // The initial state of the App
 const initialState = fromJS({
   loading: false,
   error: false,
-  uspsStates: [],
-  locationTypes: [],
-  locationStatuses: [],
-  addressTypes: [],
-  addressUses: [],
-  identifierSystems: [],
-  telecomSystems: [],
-  telecomUses: [],
+  USPSSTATES: [],
+  LOCATIONTYPE: [],
+  LOCATIONSTATUS: [],
+  ADDRESSTYPE: [],
+  ADDRESSUSE: [],
+  IDENTIFIERSYSTEM: [],
+  TELECOMSYSTEM: [],
+  TELECOMUSE: [],
 });
 
 function appReducer(state = initialState, action) {
   switch (action.type) {
-    case GET_LOCATION_LOOKUPS:
+    case GET_LOOKUPS:
       return state
         .set('loading', true)
         .set('error', false);
     case GET_LOOKUPS_SUCCESS:
       return state
-        .set('uspsStates', fromJS((action.lookups && action.lookups.uspsStates) || state.get('uspsStates')))
-        .set('addressTypes', fromJS((action.lookups && action.lookups.addressTypes) || state.get('addressTypes')))
-        .set('addressUses', fromJS((action.lookups && action.lookups.addressUses) || state.get('addressUses')))
-        .set('identifierSystems', fromJS((action.lookups && action.lookups.identifierSystems) || state.get('identifierSystems')))
-        .set('telecomSystems', fromJS((action.lookups && action.lookups.telecomSystems) || state.get('telecomSystems')))
-        .set('telecomUses', fromJS((action.lookups && action.lookups.telecomUses) || state.get('telecomUses')))
-        .set('locationStatuses', fromJS((action.lookups && action.lookups.locationStatuses) || state.get('locationStatuses')))
-        .set('locationTypes', fromJS((action.lookups && action.lookups.locationTypes) || state.get('locationTypes')))
+        .set(USPSSTATES, fromJS((action.lookups && action.lookups.uspsStates) || state.get(USPSSTATES)))
+        .set(ADDRESSTYPE, fromJS((action.lookups && action.lookups.addressTypes) || state.get(ADDRESSTYPE)))
+        .set(ADDRESSUSE, fromJS((action.lookups && action.lookups.addressUses) || state.get(ADDRESSUSE)))
+        .set(IDENTIFIERSYSTEM, fromJS((action.lookups && action.lookups.identifierSystems) || state.get(IDENTIFIERSYSTEM)))
+        .set(TELECOMSYSTEM, fromJS((action.lookups && action.lookups.telecomSystems) || state.get(TELECOMSYSTEM)))
+        .set(TELECOMUSE, fromJS((action.lookups && action.lookups.telecomUses) || state.get(TELECOMUSE)))
+        .set(LOCATIONSTATUS, fromJS((action.lookups && action.lookups.locationStatuses) || state.get(LOCATIONSTATUS)))
+        .set(LOCATIONTYPE, fromJS((action.lookups && action.lookups.locationTypes) || state.get(LOCATIONTYPE)))
         .set('loading', false);
     case GET_LOOKUPS_ERROR:
       return state
