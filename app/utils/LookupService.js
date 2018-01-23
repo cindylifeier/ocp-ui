302938c32e8ee10db3579e3ea6b4710d36618dff
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-22 19:14:47 -0500

    Refactore lookups service to iterate over lookup types in action to determine if there are in store.
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

--- app/utils/LookupService.js
+++ app/utils/LookupService.js
@@ -1,77 +1,30 @@
 import { select } from 'redux-saga/effects';
-import {
-  ADDRESSTYPE,
-  ADDRESSUSE,
-  IDENTIFIERSYSTEM,
-  LOCATIONSTATUS,
-  LOCATIONTYPE,
-  TELECOMSYSTEM,
-  TELECOMUSE,
-  USPSSTATES,
-} from '../containers/App/constants';
-import {
-  makeSelectAddressTypes,
-  makeSelectAddressUses,
-  makeSelectIdentifierSystems,
-  makeSelectLocationStatuses,
-  makeSelectLocationTypes,
-  makeSelectTelecomSystems,
-  makeSelectTelecomUses,
-  makeSelectUspsStates,
-} from '../containers/App/selectors';
+import { makeSelectLookups } from '../containers/App/selectors';
 import getApiBaseUrl from '../apiBaseUrlConfig';
 import request from './request';
 
-
-function* isLookupTypeInStore(selectedTypes, verifyingType, makeSelectorType) {
-  if (selectedTypes.includes(verifyingType)) {
-    const dataFromStore = yield select(makeSelectorType());
-    if (dataFromStore && dataFromStore.length > 0) {
-      return true;
-    }
+function* isLookupTypeInStore(lookupType) {
+  const dataFromStore = yield select(makeSelectLookups(lookupType));
+  if (dataFromStore && dataFromStore.length > 0) {
+    return true;
   }
   return false;
 }
 
 export function* getLookupTypesNotInStore(action) {
   const lookupTypesNotInStore = [];
-  if (!(yield isLookupTypeInStore(action.lookupTypes, USPSSTATES, makeSelectUspsStates))
-    && action.lookupTypes.includes(USPSSTATES)) {
-    lookupTypesNotInStore.push(USPSSTATES);
+  let i = 0;
+  while (i < action.lookupTypes.length) {
+    const lookupType = action.lookupTypes[i];
+    if (!(yield isLookupTypeInStore(lookupType))) {
+      lookupTypesNotInStore.push(lookupType);
+    }
+    i += 1;
   }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, LOCATIONSTATUS, makeSelectLocationStatuses))
-    && action.lookupTypes.includes(LOCATIONSTATUS)) {
-    lookupTypesNotInStore.push(LOCATIONSTATUS);
-  }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, LOCATIONTYPE, makeSelectLocationTypes))
-    && action.lookupTypes.includes(LOCATIONTYPE)) {
-    lookupTypesNotInStore.push(LOCATIONTYPE);
-  }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, ADDRESSTYPE, makeSelectAddressTypes))
-    && action.lookupTypes.includes(ADDRESSTYPE)) {
-    lookupTypesNotInStore.push(ADDRESSTYPE);
-  }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, ADDRESSUSE, makeSelectAddressUses))
-    && action.lookupTypes.includes(ADDRESSUSE)) {
-    lookupTypesNotInStore.push(ADDRESSUSE);
-  }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, IDENTIFIERSYSTEM, makeSelectIdentifierSystems))
-    && action.lookupTypes.includes(IDENTIFIERSYSTEM)) {
-    lookupTypesNotInStore.push(IDENTIFIERSYSTEM);
-  }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, TELECOMSYSTEM, makeSelectTelecomSystems))
-    && action.lookupTypes.includes(TELECOMSYSTEM)) {
-    lookupTypesNotInStore.push(TELECOMSYSTEM);
-  }
-  if (!(yield isLookupTypeInStore(action.lookupTypes, TELECOMUSE, makeSelectTelecomUses))
-    && action.lookupTypes.includes(TELECOMUSE)) {
-    lookupTypesNotInStore.push(TELECOMUSE);
-  }
-
   return lookupTypesNotInStore;
 }
 
-export function getLookups(lookupTypes) {
+export function fetchLookups(lookupTypes) {
   const apiBaseURL = getApiBaseUrl();
   const lookupKeyList = lookupTypes.join();
   const requestURL = `${apiBaseURL}/lookups?lookUpTypeList=${lookupKeyList}`;
