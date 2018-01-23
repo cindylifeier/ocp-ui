81b28c808c2e6c792bf3ddb3d0ee70bcd46384ca
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-22 19:17:23 -0500

    Create a generic actions for all lookups
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

--- app/containers/App/actions.js
+++ app/containers/App/actions.js
@@ -5,29 +5,15 @@
  */
 
 import {
-  GET_LOCATION_LOOKUPS,
+  GET_LOOKUPS,
   GET_LOOKUPS_ERROR,
   GET_LOOKUPS_FROM_STORE,
-  GET_LOOKUPS_SUCCESS, GET_ORGANIZATION_LOOKUPS, GET_PATIENT_LOOKUPS,
+  GET_LOOKUPS_SUCCESS,
 } from './constants';
 
-export function getLocationLookupsAction(lookupTypes) {
+export function getLookupsAction(lookupTypes) {
   return {
-    type: GET_LOCATION_LOOKUPS,
-    lookupTypes,
-  };
-}
-
-export function getPatientLookupsAction(lookupTypes) {
-  return {
-    type: GET_PATIENT_LOOKUPS,
-    lookupTypes,
-  };
-}
-
-export function getOrganizationLookupsAction(lookupTypes) {
-  return {
-    type: GET_ORGANIZATION_LOOKUPS,
+    type: GET_LOOKUPS,
     lookupTypes,
   };
 }
