a81942b2999648159c301f008e41af14d2c37662
sadhch <sadhana.chandravanka@feisystems.com>
2018-01-23 15:47:08 -0500

    Remove host hardcoded value
    17349 - As a development team member, I can rely on development process environment and application infrastructure services for working software fast delivery

--- app/apiBaseUrlConfig.js
+++ app/apiBaseUrlConfig.js
@@ -1,11 +1,5 @@
 function getApiBaseUrl() {
-  let apiBaseUrl = 'http://localhost/ocp-ui-api/ocp-fis';
-
-  if (process.env.NODE_ENV === 'production') {
-    apiBaseUrl = 'http://20.0.2.87/ocp-ui-api/ocp-fis';
-  }
-
-  return apiBaseUrl;
+  return '/ocp-ui-api/ocp-fis';
 }
 
 export default getApiBaseUrl;
