7daa76a5ff8a9d7c1847d3db577bb9a72884c934
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-22 19:13:17 -0500

    Delete manage organization component
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

--- app/containers/ManageOrganizationPage/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { selectManageOrganizationPageDomain } from '../selectors';
-
-describe('selectManageOrganizationPageDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
