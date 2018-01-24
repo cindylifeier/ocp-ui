c64a6920f5fed26f29719b3e820c2e2a173d3e44
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-24 11:48:35 -0500

    Add i18n messages for manage location form.
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

---
+++ app/components/ManageLocation/messages.js
@@ -0,0 +1,107 @@
+/*
+ * ManageLocation Messages
+ *
+ * This contains all the text for the ManageLocation component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'ocpui.components.ManageLocation.header',
+    defaultMessage: 'This is the ManageLocation component !',
+  },
+  locationNameHintText: {
+    id: 'ocpui.components.ManageLocation.locationNameHintText',
+    defaultMessage: 'Location Name',
+  },
+  locationNameFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.locationNameFloatingLabelText',
+    defaultMessage: 'Location Name',
+  },
+  statusFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.statusFloatingLabelText',
+    defaultMessage: 'Status',
+  },
+  locationPartOfHintText: {
+    id: 'ocpui.components.ManageLocation.locationPartOfHintText',
+    defaultMessage: 'Location Part Of',
+  },
+  locationPhysicalType: {
+    id: 'ocpui.components.ManageLocation.locationPhysicalType',
+    defaultMessage: 'Location Type',
+  },
+  locationPartOfFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.locationPartOfFloatingLabelText',
+    defaultMessage: 'Location Part Of',
+  },
+  identifierSystemTypeFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.identifierSystemTypeFloatingLabelText',
+    defaultMessage: 'Identifier System',
+  },
+  addressTypesFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.addressTypesFloatingLabelText',
+    defaultMessage: 'Address Type',
+  },
+  address1HintText: {
+    id: 'ocpui.components.ManageLocation.address1HintText',
+    defaultMessage: 'Address 1',
+  },
+  address1FloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.address1FloatingLabelText',
+    defaultMessage: 'Address 1',
+  },
+  address2HintText: {
+    id: 'ocpui.components.ManageLocation.address2HintText',
+    defaultMessage: 'Address 2',
+  },
+  address2FloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.address2FloatingLabelText',
+    defaultMessage: 'Address 2',
+  },
+  addressUseFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.addressUseFloatingLabelText',
+    defaultMessage: 'Address Use',
+  },
+  cityHintText: {
+    id: 'ocpui.components.ManageLocation.cityHintText',
+    defaultMessage: 'City',
+  },
+  cityFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.cityFloatingLabelText',
+    defaultMessage: 'City',
+  },
+  statesFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.statesFloatingLabelText',
+    defaultMessage: 'State',
+  },
+  postalCodeHintText: {
+    id: 'ocpui.components.ManageLocation.postalCodeHintText',
+    defaultMessage: 'Zip Code',
+  },
+  postalCodeFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.postalCodeFloatingLabelText',
+    defaultMessage: 'Zip Code',
+  },
+  identifierValueHintText: {
+    id: 'ocpui.components.ManageLocation.identifierValueHintText',
+    defaultMessage: 'Identifier Value',
+  },
+  identifierVlueFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.identifierVlueFloatingLabelText',
+    defaultMessage: 'Identifier Value',
+  },
+  telecomSystemValueHintText: {
+    id: 'ocpui.components.ManageLocation.telecomSystemValueHintText',
+    defaultMessage: 'Telecom System',
+  },
+  telecomSystemValueFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.telecomSystemValueFloatingLabelText',
+    defaultMessage: 'Telecom System',
+  },
+  telecomSystemTypeFloatingLabelText: {
+    id: 'ocpui.components.ManageLocation.telecomSystemTypeFloatingLabelText',
+    defaultMessage: 'Telecom System Type',
+  },
+
+
+});
