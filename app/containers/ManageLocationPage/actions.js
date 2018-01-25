423b05cef30f7042a8d70dd6c42d37a5a97dfaac
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-25 09:32:29 -0500

    Implement actions to manage location creation
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

--- app/containers/ManageLocationPage/actions.js
+++ app/containers/ManageLocationPage/actions.js
@@ -5,11 +5,30 @@
  */
 
 import {
-  DEFAULT_ACTION,
+  CREATE_LOCATION_ERROR,
+  CREATE_LOCATION_SUCCESS,
+  POST_CREATE_LOCATION,
 } from './constants';
 
-export function defaultAction() {
+
+export function createLocation(location, organizationId) {
   return {
-    type: DEFAULT_ACTION,
+    type: POST_CREATE_LOCATION,
+    location,
+    organizationId,
+  };
+}
+
+export function createLocationSuccess(location) {
+  return {
+    type: CREATE_LOCATION_SUCCESS,
+    location,
+  };
+}
+
+export function createLocationError(location) {
+  return {
+    type: CREATE_LOCATION_ERROR,
+    location,
   };
 }
