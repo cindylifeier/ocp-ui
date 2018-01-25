f40c91dfc5fe8ae4e2002fdcf256f6397596488a
CindyLiFei <cindy.li@feisystems.com>
2018-01-25 10:53:18 -0500

    Map patient form data to backend patient structure
    # 17163 - As a user, I can enroll a new patient, so that patients can exist in the OCP

--- app/containers/ManagePatientPage/api.js
+++ app/containers/ManagePatientPage/api.js
@@ -7,9 +7,39 @@ export default function postPatient(patientFormData) {
   const requestURL = `${apiBaseURL}/patients/`;
   return request(requestURL, {
     method: 'POST',
-    body: JSON.stringify(patientFormData),
+    body: JSON.stringify(mapToBackendPatient(patientFormData)),
     headers: {
       'Content-Type': 'application/json',
     },
   });
 }
+
+function mapToBackendPatient(patientFormData) {
+  const {
+    firstName, lastName, birthDate, genderCode, identifierType, identifierValue, language, race,
+    ethnicity, birthSex, address1, address2, city, state, postalCode, country, telecomType, telecomValue,
+  } = patientFormData;
+
+  const identifier = [{
+    system: identifierType,
+    value: identifierValue,
+  }];
+  const name = [{
+    firstName,
+    lastName,
+  }];
+  const telecom = [{
+    system: telecomType,
+    value: telecomValue,
+  }];
+  const address = [{
+    line1: address1,
+    line2: address2,
+    city,
+    stateCode: state,
+    postalCode,
+    countryCode: country,
+  }];
+  return { identifier, name, telecom, address, birthDate, genderCode, language, race, ethnicity, birthSex };
+}
+
