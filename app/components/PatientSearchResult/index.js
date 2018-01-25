8d6e31e275eb1abda8275f34c5104981b914ffc7
Teng Luo <Teng.Luo@feisystems.com>
2018-01-25 14:54:56 -0500

    Add action button to patients table
    #17174: As a user, I can create an practitioners, so that practitioners can exist in the OCP

--- app/components/PatientSearchResult/index.js
+++ app/components/PatientSearchResult/index.js
@@ -5,10 +5,28 @@
 */
 
 import React from 'react';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import ActionList from 'material-ui/svg-icons/action/list';
+
 import uniqueId from 'lodash/uniqueId';
 import PropTypes from 'prop-types';
 import RefreshIndicatorLoading from '../RefreshIndicatorLoading';
 import styles from './styles.css';
+
+const iconStyles = {
+  iconButton: {
+    position: 'relative',
+  },
+  icon: {
+    width: '100%',
+    height: 26,
+    position: 'absolute',
+    top: '0',
+    right: '0',
+  },
+};
 
 function displayPatientSearchResult(patients) {
   return patients && patients.map((patient) => (
@@ -19,6 +37,24 @@ function displayPatientSearchResult(patients) {
       <div className={styles.cellGridItem}>{patient.genderCode}</div>
       <div className={styles.cellGridItem}>{getIdentifiers(patient.identifier)}</div>
       <div className={styles.cellGridItem}>{patient.active ? 'active' : 'inactive' }</div>
+      <div>
+        <IconMenu
+          iconButtonElement={
+            (<IconButton
+              className={styles.iconButton}
+              iconStyle={iconStyles.icon}
+              style={iconStyles.iconButton}
+            >
+              <ActionList />
+            </IconButton>)
+          }
+          anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+          targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        >
+          <MenuItem className={styles.menuItem} primaryText="Edit" />
+          <MenuItem className={styles.menuItem} primaryText="Remove" />
+        </IconMenu>
+      </div>
     </div>
   ));
 }
@@ -58,13 +94,14 @@ function PatientSearchResult({ loading, error, searchResult }) {
   if (searchResult !== false && searchResult.elements !== null && searchResult.elements.length !== 0) {
     return (
       <div className={styles.table}>
-        <div className={styles.rowGridContainer}>
+        <div className={styles.rowGridHeaderContainer}>
           <div className={styles.cellGridHeaderItem}>First Name</div>
           <div className={styles.cellGridHeaderItem}>Last Name</div>
           <div className={styles.cellGridHeaderItem}>DOB</div>
           <div className={styles.cellGridHeaderItem}>Gender</div>
           <div className={styles.cellGridHeaderItem}>Identifier</div>
           <div className={styles.cellGridHeaderItem}>Status</div>
+          <div></div>
         </div>
         {displayPatientSearchResult(searchResult.elements)}
       </div>
