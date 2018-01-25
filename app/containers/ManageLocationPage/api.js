7b40a200b95383324fcc5efeb522efc704dc6e34
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-25 09:33:49 -0500

    Implement function to call api to create location
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

---
+++ app/containers/ManageLocationPage/api.js
@@ -0,0 +1,37 @@
+import request from '../../utils/request';
+import getApiBaseUrl from '../../apiBaseUrlConfig';
+
+const apiBaseUrl = getApiBaseUrl();
+
+export default function createLocation(location, organizationId) {
+  const url = `${apiBaseUrl}/organizations/${organizationId}/location`;
+  console.log(JSON.stringify(mapToBffLocation(location)));
+  return request(url, {
+    method: 'POST',
+    body: JSON.stringify(mapToBffLocation(location)),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
+function mapToBffLocation(rawlocation) {
+  const location = {};
+  location.name = rawlocation.name;
+  location.managingLocationLogicalId = '';
+  location.status = '';
+  location.physicalType = rawlocation.physicalType;
+  const { identifierSystem, identifierValue } = rawlocation;
+  location.identifiers = [{
+    system: identifierSystem,
+    value: identifierValue,
+    oid: '',
+    priority: '',
+    display: '',
+  }];
+  const { telecomSystem, telecomUse, telecomSystemValue } = rawlocation;
+  location.telecoms = [{ system: telecomSystem, use: telecomUse, value: telecomSystemValue }];
+  const { line1, line2, city, stateCode, postalCode, use } = rawlocation;
+  location.address = { line1, line2, city, stateCode, postalCode, use, countryCode: 'US' };
+  return location;
+}
