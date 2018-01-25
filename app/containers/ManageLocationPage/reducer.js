00a51d95539e1d863fc594ab880f32f3e2bdfbd7
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-25 11:54:22 -0500

    Update state in reducer with error
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

--- app/containers/ManageLocationPage/reducer.js
+++ app/containers/ManageLocationPage/reducer.js
@@ -6,15 +6,18 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
+  CREATE_LOCATION_ERROR, CREATE_LOCATION_SUCCESS,
 } from './constants';
 
 const initialState = fromJS({});
 
 function manageLocationPageReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case CREATE_LOCATION_SUCCESS:
+      return state
+        .set('error', false);
+    case CREATE_LOCATION_ERROR:
+      return state.set('error', action.error);
     default:
       return state;
   }
