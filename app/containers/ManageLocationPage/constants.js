ac35faedc009a85057a49a07e1b18e3003868c8e
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-25 09:33:07 -0500

    Add constants to manage location
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

--- app/containers/ManageLocationPage/constants.js
+++ app/containers/ManageLocationPage/constants.js
@@ -8,10 +8,12 @@
  *  ManageLocationPage action types
  *  @type {string}
  */
-export const DEFAULT_ACTION = 'ocpui/ManageLocationPage/DEFAULT_ACTION';
-
+export const POST_CREATE_LOCATION = 'ocpui/ManageLocation/POST_CREATE_LOCATION';
+export const CREATE_LOCATION_SUCCESS = 'ocpui/ManageLocation/CREATE_LOCATION_SUCCESS';
+export const CREATE_LOCATION_ERROR = 'ocpui/ManageLocation/CREATE_LOCATION_ERROR';
 
 /**
 *  Other constants
 *
 */
+export const TEXT_MIN_LENGTH = 3;
