9a9dd4c8775e108d09cfd50f9878a70b6361d0f5
CindyLiFei <cindy.li@feisystems.com>
2018-01-26 00:32:01 -0500

    Add notification to create patient
    # 17163 - As a user, I can enroll a new patient, so that patients can exist in the OCP

--- app/containers/ManagePatientPage/saga.js
+++ app/containers/ManagePatientPage/saga.js
@@ -2,17 +2,22 @@
 
 // Individual exports for testing
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { push } from 'react-router-redux';
+import { goBack } from 'react-router-redux';
 import { SAVE_PATIENT } from './constants';
 import { savePatientError } from './actions';
 import postPatient from './api';
+import { showNotification } from '../Notification/actions';
 
 
-export function* savePatientWorker({ patientFormData }) {
+export function* savePatientWorker(action) {
   try {
-    yield call(postPatient, patientFormData);
-    yield put(push('/home'));
+    yield call(postPatient, action.patientFormData);
+    yield put(showNotification('Successfully created the patient.'));
+    yield call(action.handleSubmitting);
+    yield put(goBack());
   } catch (error) {
+    yield put(showNotification('Failed to create the patient.'));
+    yield call(action.handleSubmitting);
     yield put(savePatientError(error));
   }
 }
