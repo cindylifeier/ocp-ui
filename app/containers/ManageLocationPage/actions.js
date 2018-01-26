244d1cf48e807942684948b2b8c610a78c3ead2b
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-26 11:19:32 -0500

    Add actions in relation to getting location
    #17158 - As a user, I can create an location, so that locations can exist in the OCP

--- app/containers/ManageLocationPage/actions.js
+++ app/containers/ManageLocationPage/actions.js
@@ -6,8 +6,8 @@
 
 import {
   CREATE_LOCATION_ERROR,
-  CREATE_LOCATION_SUCCESS,
-  POST_CREATE_LOCATION,
+  CREATE_LOCATION_SUCCESS, GET_LOCATION, GET_LOCATION_ERROR, GET_LOCATION_SUCCESS,
+  POST_CREATE_LOCATION, POST_UPDATE_LOCATION,
 } from './constants';
 
 
@@ -16,6 +16,21 @@ export function createLocation(location, organizationId) {
     type: POST_CREATE_LOCATION,
     location,
     organizationId,
+  };
+}
+
+export function updateLocation(location, organizationId) {
+  return {
+    type: POST_UPDATE_LOCATION,
+    location,
+    organizationId,
+  };
+}
+
+export function getLocation(locationId) {
+  return {
+    type: GET_LOCATION,
+    locationId,
   };
 }
 
@@ -30,5 +45,19 @@ export function createLocationError(error) {
   return {
     type: CREATE_LOCATION_ERROR,
     error,
+  };
+}
+
+export function getLocationError(error) {
+  return {
+    type: GET_LOCATION_ERROR,
+    error,
+  };
+}
+
+export function getLocationSuccess(location) {
+  return {
+    type: GET_LOCATION_SUCCESS,
+    location,
   };
 }
