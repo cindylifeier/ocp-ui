a920f441b36c5ea853a806cb27c5715e47ac5683
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-26 23:22:50 -0500

    Implement update location logic in manage location page container.
    17158:As a user, I can create an location, so that locations can exist in the OCP

--- app/containers/ManageLocationPage/index.js
+++ app/containers/ManageLocationPage/index.js
@@ -8,11 +8,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
+import find from 'lodash/find';
+import merge from 'lodash/merge';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
-import { makeSelectLocation, makeSelectSaveLocationError } from './selectors';
+import { makeSelectSaveLocationError } from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import { getLookupsAction } from '../App/actions';
@@ -26,37 +28,30 @@ import {
   makeSelectUspsStates, makeSelectAddressUses, makeSelectTelecomUses, makeSelectLocationIdentifierSystems,
 } from '../App/selectors';
 import ManageLocation from '../../components/ManageLocation/index';
-import { createLocation, getLocation, updateLocation } from './actions';
-import { makeSelectOrganization } from '../Locations/selectors';
+import { createLocation, updateLocation } from './actions';
+import { makeSelectLocations, makeSelectOrganization } from '../Locations/selectors';
 
 
 export class ManageLocationPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
-    this.state = {
-      locationId: 0,
-    };
     this.handleSaveLocation = this.handleSaveLocation.bind(this);
   }
   componentWillMount() {
-    const { match: { params } } = this.props;
-    if (params.id) {
-      const locationId = params.id;
-      this.setState({ locationId });
-      // get Location by id
-      this.props.getLocation(locationId);
-    }
     this.props.getLookups();
   }
   handleSaveLocation(location) {
-    // console.log(location);
-    if (this.state.locationId && location) {
+    const logicalId = this.props.match.params.id;
+    if (logicalId && location) {
+      merge(location, { logicalId });
       this.props.updateLocation(location, this.props.organization.id);
     } else {
       this.props.createLocation(location, this.props.organization.id);
     }
   }
   render() {
+    const logicalId = this.props.match.params.id;
+    const location = find(this.props.locations, { logicalId });
     const {
       error,
       uspsStates,
@@ -66,7 +61,6 @@ export class ManageLocationPage extends React.PureComponent { // eslint-disable-
       telecomUses,
       addressUses,
       identifierSystems,
-      location,
       organization } = this.props;
     const localProps = {
       error,
@@ -100,7 +94,6 @@ export class ManageLocationPage extends React.PureComponent { // eslint-disable-
 ManageLocationPage.propTypes = {
   match: PropTypes.object,
   getLookups: PropTypes.func.isRequired,
-  getLocation: PropTypes.func.isRequired,
   createLocation: PropTypes.func.isRequired,
   updateLocation: PropTypes.func.isRequired,
   uspsStates: PropTypes.array,
@@ -108,7 +101,7 @@ ManageLocationPage.propTypes = {
   locationStatuses: PropTypes.array,
   telecomSystems: PropTypes.array,
   telecomUses: PropTypes.array,
-  location: PropTypes.object,
+  locations: PropTypes.array,
   addressUses: PropTypes.array,
   organization: PropTypes.object,
   identifierSystems: PropTypes.array,
@@ -128,7 +121,7 @@ const mapStateToProps = createStructuredSelector({
   addressUses: makeSelectAddressUses(),
   identifierSystems: makeSelectLocationIdentifierSystems(),
   error: makeSelectSaveLocationError(),
-  location: makeSelectLocation(),
+  locations: makeSelectLocations(),
 });
 
 function mapDispatchToProps(dispatch) {
@@ -136,7 +129,6 @@ function mapDispatchToProps(dispatch) {
     getLookups: () => dispatch(getLookupsAction([USPSSTATES, LOCATIONSTATUS, LOCATIONPHYSICALTYPE, ADDRESSUSE, TELECOMSYSTEM, TELECOMUSE, LOCATIONIDENTIFIERSYSTEM])),
     createLocation: (location, organizationId) => dispatch(createLocation(location, organizationId)),
     updateLocation: (location, organizationId) => dispatch(updateLocation(location, organizationId)),
-    getLocation: (locationId) => dispatch(getLocation(locationId)),
   };
 }
 
