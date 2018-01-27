e6b2975c77ff2569f8617cba43f45a8a9af397ee
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-26 23:20:01 -0500

    Implement manage location presentation component update
    17158:As a user, I can create an location, so that locations can exist in the OCP

--- app/components/ManageLocation/index.js
+++ app/components/ManageLocation/index.js
@@ -6,26 +6,30 @@
 
 import React from 'react';
 
+import { isUndefined } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import { Formik } from 'formik';
 import PropTypes from 'prop-types';
+import isEmpty from 'lodash/isEmpty';
+import merge from 'lodash/merge';
+import identity from 'lodash/identity';
+import pickBy from 'lodash/pickBy';
 import yup from 'yup';
 import messages from './messages';
 import styles from './styles.css';
 import ManageLocationForm from './ManageLocationForm';
 import { TEXT_MIN_LENGTH } from '../../containers/ManageLocationPage/constants';
+import { setEmptyStringWhenUndefined } from '../ManagePractitioner/index';
+
 
 function ManageLocation(props) {
   const minimumLength = TEXT_MIN_LENGTH;
   const postalCodePattern = new RegExp('^\\d{5}(?:[-\\s]\\d{4})?$');
   const { onSave } = props;
-  // const name = location.name;
-
-  const initialValues = { };
   return (
     <div className={styles.root} >
       <Formik
-        initialValues={initialValues}
+        initialValues={setFormData(props.location)}
         onSubmit={(values, actions) => {
           onSave(values);
           actions.setSubmitting(false);
@@ -64,7 +68,71 @@ function ManageLocation(props) {
 
 ManageLocation.propTypes = {
   onSave: PropTypes.func.isRequired,
-  // location: PropTypes.object,
+  location: PropTypes.object,
 };
 
 export default ManageLocation;
+
+
+function setFormData(location) {
+  let formData = null;
+  if (!isEmpty(location)) {
+    formData = merge(
+      mapLocationToFiledObject(location, 'name'),
+      mapLocationToFiledObject(location, 'status'),
+      mapLocationToFiledObject(location, 'physicalType'),
+      mapLocationToFiledObject(location, 'managingLocationLogicalId'),
+      mapLocationToAddressFields(location),
+      mapLocationToIdentifierFields(location),
+      mapLocationToTelecomFields(location));
+  }
+  return pickBy(formData, identity);
+}
+
+function mapLocationToFiledObject(location, fieldName) {
+  const fieldObject = {};
+  if (!isUndefined(location[fieldName])) {
+    fieldObject[fieldName] = setEmptyStringWhenUndefined(location[fieldName]);
+  }
+  return fieldObject;
+}
+
+function mapLocationToAddressFields(location) {
+  let fieldObject = {};
+  if (!isUndefined(location.address)) {
+    fieldObject = {
+      line1: setEmptyStringWhenUndefined(location.address.line1),
+      line2: setEmptyStringWhenUndefined(location.address.line2),
+      city: setEmptyStringWhenUndefined(location.address.city),
+      stateCode: setEmptyStringWhenUndefined(location.address.stateCode),
+      postalCode: setEmptyStringWhenUndefined(location.address.postalCode),
+      countryCode: setEmptyStringWhenUndefined(location.address.countryCode),
+      use: setEmptyStringWhenUndefined(location.address.use),
+    };
+  }
+  return fieldObject;
+}
+
+
+function mapLocationToIdentifierFields(location) {
+  let fieldObject = {};
+  if (location.identifiers && location.identifiers.length > 0) {
+    fieldObject = {
+      identifierSystem: setEmptyStringWhenUndefined(location.identifiers[0].system),
+      identifierValue: setEmptyStringWhenUndefined(location.identifiers[0].value),
+    };
+  }
+  return fieldObject;
+}
+
+function mapLocationToTelecomFields(location) {
+  let fieldObject = {};
+  if (location.telecoms && location.telecoms.length > 0) {
+    fieldObject = {
+      telecomSystem: setEmptyStringWhenUndefined(location.telecoms[0].system),
+      telecomSystemValue: setEmptyStringWhenUndefined(location.telecoms[0].value),
+      telecomUse: setEmptyStringWhenUndefined(location.telecoms[0].use),
+    };
+  }
+  return fieldObject;
+}
