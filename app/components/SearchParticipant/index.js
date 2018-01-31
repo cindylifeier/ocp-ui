e26a3883f0d290d30803267dea48b0c1a57cd365
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-31 13:08:35 -0500

    Start implementing app practitioner form.
    17182 - As a user, I can create a care teams for the patient with practitioner.

--- app/components/SearchParticipant/index.js
+++ app/components/SearchParticipant/index.js
@@ -5,21 +5,65 @@
 */
 
 import React from 'react';
-// import styled from 'styled-components';
-
+import yup from 'yup';
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Formik } from 'formik';
+import { teal500, white } from 'material-ui/styles/colors';
+import styles from './styles.css';
 import messages from './messages';
 
-function SearchParticipant() {
+function SearchParticipant(props) {
+  const { onSearch } = props;
   return (
-    <div>
-      <FormattedMessage {...messages.header} />
+    <div className={styles.root} >
+      <Formik
+        initialValues={{}}
+        onSubmit={(values, actions) => {
+          onSearch(values);
+          actions.setSubmitting(false);
+        }}
+        validationSchema={yup.object().shape({
+          // name: yup.string()
+          //   .required((<FormattedMessage {...messages.validation.required} />))
+          //   .min(minimumLength, (
+          //     <FormattedMessage {...messages.validation.minLength} values={{ minimumLength }} />)),
+          // identifierValue: yup.string()
+          //   .required((<FormattedMessage {...messages.validation.required} />))
+          //   .min(minimumLength, (
+          //     <FormattedMessage {...messages.validation.minLength} values={{ minimumLength }} />)),
+        })}
+        render={() => (
+          <form>
+            <div className={styles.gridContainer}>
+              <div className={styles.gridItem}>
+                <div className={styles.buttonGroup}>
+                  <RaisedButton
+                    backgroundColor={teal500}
+                    labelColor={white}
+                    label={<FormattedMessage {...messages.addParticipantBtnLabel} />}
+                    primary
+                    // disabled={!dirty || isSubmitting || !isValid}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={styles.gridContainer}>
+              <div className={styles.gridItem}>
+                <p> Participant Table goes here!!</p>
+              </div>
+            </div>
+          </form>
+          )
+        }
+      />
     </div>
   );
 }
 
 SearchParticipant.propTypes = {
-
+  onSearch: PropTypes.func.isRequired,
 };
 
 export default SearchParticipant;
