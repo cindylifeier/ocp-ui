e3f6766a6cef8e0ed2a4acf51ff449bf8e51c282
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-31 13:07:47 -0500

    Add pass on search function
    17182 - As a user, I can create a care teams for the patient with practitioner.

--- app/components/ManageCareTeam/index.js
+++ app/components/ManageCareTeam/index.js
@@ -5,21 +5,23 @@
 */
 
 import React from 'react';
-// import styled from 'styled-components';
-
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import SearchParticipant from '../SearchParticipant/index';
 
-function ManageCareTeam() {
+function ManageCareTeam(props) {
+  const { onSearch } = props;
   return (
     <div>
       <FormattedMessage {...messages.header} />
+      <SearchParticipant onSearch={onSearch}></SearchParticipant>
     </div>
   );
 }
 
 ManageCareTeam.propTypes = {
-
+  onSearch: PropTypes.func.isRequired,
 };
 
 export default ManageCareTeam;
