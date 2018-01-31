951fe6df07c0f029a493cdb58cd028634bd0fee1
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-01-31 15:58:49 -0500

    Add constant for search participant
    17182 - As a user, I can create a care teams for the patient with practitioner.

---
+++ app/components/SearchParticipant/constants.js
@@ -0,0 +1,4 @@
+
+export const fieldStyle = { width: '15vw' };
+export const floatingLabelStyle = { fontFamily: 'Roboto, sans-serif' };
+export const iconButtonStyle = { top: '26px', height: '30px' };
