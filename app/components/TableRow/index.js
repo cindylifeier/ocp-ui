5be88332830925d065f68291a9ae251ffb7a22a2
burcakulug <burcak.ulug@feisystems.com>
2018-02-01 11:18:17 -0500

    Create a reusable table structure based on css grid
    #17183:As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

---
+++ app/components/TableRow/index.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * TableRow
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './styles.css';
+
+// import styled from 'styled-components';
+
+function TableRow({ children }) {
+  return (
+    <div
+      className={styles.rowGridContainer}
+      style={{ gridTemplateColumns: `repeat(${React.Children.count(children)}, 1fr) 0.5fr` }}
+    >
+      {children}
+    </div>
+  );
+}
+
+TableRow.propTypes = {
+  children: PropTypes.node,
+};
+
+export default TableRow;
