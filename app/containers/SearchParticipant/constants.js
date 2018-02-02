9a48da90ca2b63b8f4be734c59e347d630462f39
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-02-02 16:20:30 -0500

    Add constant to be used in search participant action.
    17182 - As a user, I can create a care teams for the patient with practitioner.

---
+++ app/containers/SearchParticipant/constants.js
@@ -0,0 +1,24 @@
+/*
+ *
+ * SearchParticipant constants
+ *
+ */
+
+/**
+ *  SearchParticipant action types
+ *  @type {string}
+ */
+export const DEFAULT_ACTION = 'ocpui/SearchParticipant/DEFAULT_ACTION';
+
+export const fieldStyle = { width: '15vw' };
+export const floatingLabelStyle = { fontFamily: 'Roboto, sans-serif' };
+export const iconButtonStyle = { top: '26px', height: '30px' };
+
+export const SEARCH_PARTICIPANT = 'ocpui/ManageCareTeamPage/GET_SEARCH_PARTICIPANT';
+export const SEARCH_PARTICIPANT_SUCCESS = 'ocpui/ManageCareTeamPage/SEARCH_PARTICIPANT_SUCCESS';
+export const SEARCH_PARTICIPANT_ERROR = 'ocpui/ManageCareTeamPage/SEARCH_PARTICIPANT_ERROR';
+
+/**
+*  Other constants
+*
+*/
