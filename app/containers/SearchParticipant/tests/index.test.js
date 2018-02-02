f23eba370342d19896423b6aae311a3fa6e8097a
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-02-02 16:17:56 -0500

    Generate unit test code.
    17182 - As a user, I can create a care teams for the patient with practitioner.

--- app/containers/SearchParticipant/tests/index.test.js
+++ app/containers/SearchParticipant/tests/index.test.js
@@ -1,7 +1,7 @@
 // import React from 'react';
 // import { shallow } from 'enzyme';
 
-// import SearchParticipant from '../index';
+// import { SearchParticipant } from '../index';
 
 describe('<SearchParticipant />', () => {
   it('Expect to have unit tests specified', () => {
---
+++ app/containers/SearchParticipant/tests/reducer.test.js
@@ -0,0 +1,9 @@
+
+import { fromJS } from 'immutable';
+import searchParticipantReducer from '../reducer';
+
+describe('searchParticipantReducer', () => {
+  it('returns the initial state', () => {
+    expect(searchParticipantReducer(undefined, {})).toEqual(fromJS({}));
+  });
+});
