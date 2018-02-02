4dee0fd986fabf1c6dd1d1b49e9d83e2eb6680a1
JiahaoLi <Jiahao.Li@feisystems.com>
2018-02-02 15:26:23 -0500

    Implement api service to create care team
    17182: As a user, I can create a care teams for the patient with practitioner.

--- app/containers/ManageCareTeamPage/api.js
+++ app/containers/ManageCareTeamPage/api.js
@@ -1,9 +1,41 @@
 import isEmpty from 'lodash/isEmpty';
 import find from 'lodash/find';
+import request from '../../utils/request';
+import getApiBaseUrl from '../../apiBaseUrlConfig';
+
+const apiBaseURL = getApiBaseUrl();
 
 export function getPatientById(patients, patientId) {
   if (!isEmpty(patients)) {
     return find(patients, { id: patientId });
   }
   return null;
+}
+
+export function createCareTeam(careTeamFormData) {
+  const requestURL = `${apiBaseURL}/careteams`;
+  return request(requestURL, {
+    method: 'POST',
+    body: JSON.stringify(mapToBffCareTeam(careTeamFormData)),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
+function mapToBffCareTeam(careTeamData) {
+  const {
+    careTeamName, category, status, startDate, endDate,
+  } = careTeamData;
+
+  // Todo: Replace with formData later
+  const participants = [{
+    roleCode: '112247003',
+    memberId: '700551',
+    memberFirstName: 'Participant',
+    memberLastName: 'Test',
+    memberType: 'practitioner',
+  }];
+
+  return { name: careTeamName, categoryCode: category, statusCode: status, startDate, endDate, participants };
 }
