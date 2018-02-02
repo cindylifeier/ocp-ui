3c3db7395411b3ff899bc62234570b559843cc2c
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-02-02 16:14:05 -0500

    Delete search participant presentation component.
    17182 - As a user, I can create a care teams for the patient with practitioner.

--- app/components/SearchParticipant/constants.js
+++
@@ -1,4 +0,0 @@
-
-export const fieldStyle = { width: '15vw' };
-export const floatingLabelStyle = { fontFamily: 'Roboto, sans-serif' };
-export const iconButtonStyle = { top: '26px', height: '30px' };
