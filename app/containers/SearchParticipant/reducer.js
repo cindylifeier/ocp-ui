74bc876a599192d42fa5bec273f2127d912a05b3
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-02-02 19:15:56 -0500

    Set search result to the state.
    17182 - As a user, I can create a care teams for the patient with practitioner.

--- app/containers/SearchParticipant/reducer.js
+++ app/containers/SearchParticipant/reducer.js
@@ -6,14 +6,19 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
+  SEARCH_PARTICIPANT_ERROR,
+  SEARCH_PARTICIPANT_SUCCESS,
 } from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  searchParticipantResult: [],
+});
 
 function searchParticipantReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
+    case SEARCH_PARTICIPANT_SUCCESS:
+      return state.set('searchParticipantResult', fromJS((action.searchParticipantResults && action.searchParticipantResults.elements) || []));
+    case SEARCH_PARTICIPANT_ERROR:
       return state;
     default:
       return state;
