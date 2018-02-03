11dc3a322ef775a994ad3ae782386643308de6cb
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-02-02 19:04:13 -0500

    Add search constant
    17182 - As a user, I can create a care teams for the patient with practitioner.

--- app/containers/SearchParticipant/constants.js
+++ app/containers/SearchParticipant/constants.js
@@ -14,9 +14,9 @@ export const fieldStyle = { width: '15vw' };
 export const floatingLabelStyle = { fontFamily: 'Roboto, sans-serif' };
 export const iconButtonStyle = { top: '26px', height: '30px' };
 
-export const SEARCH_PARTICIPANT = 'ocpui/ManageCareTeamPage/GET_SEARCH_PARTICIPANT';
-export const SEARCH_PARTICIPANT_SUCCESS = 'ocpui/ManageCareTeamPage/SEARCH_PARTICIPANT_SUCCESS';
-export const SEARCH_PARTICIPANT_ERROR = 'ocpui/ManageCareTeamPage/SEARCH_PARTICIPANT_ERROR';
+export const SEARCH_PARTICIPANT = 'ocpui/SearchParticipant/SEARCH_PARTICIPANT';
+export const SEARCH_PARTICIPANT_SUCCESS = 'ocpui/SearchParticipant/SEARCH_PARTICIPANT_SUCCESS';
+export const SEARCH_PARTICIPANT_ERROR = 'ocpui/SearchParticipant/SEARCH_PARTICIPANT_ERROR';
 
 /**
 *  Other constants
