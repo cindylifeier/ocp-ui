7789b3d05b0d0147b89b737c00d1e35e1b5d619c
CindyLiFei <cindy.li@feisystems.com>
2018-02-05 16:33:42 -0500

    Add unit test for healthcare services actions
    # 17532 - As a user, I can see a list of Health Care Services related to an organization in the OCP, so that I know all of the Health Care Services in the system.

--- app/containers/HealthcareServices/tests/actions.test.js
+++ app/containers/HealthcareServices/tests/actions.test.js
@@ -1,18 +1,36 @@
 
 import {
-  defaultAction,
-} from '../actions';
-import {
-  DEFAULT_ACTION,
+  GET_ACTIVE_HEALTHCARE_SERVICES, GET_FILTERED_HEALTHCARE_SERVICES,
+  INITIALIZE_HEALTHCARE_SERVICES,
 } from '../constants';
+import {
+  getActiveHealthcareServices, getFilteredHealthcareServices,
+  initializeHealthcareServices,
+} from '../actions';
 
 describe('HealthcareServices actions', () => {
-  describe('Default Action', () => {
-    it('has a type of DEFAULT_ACTION', () => {
+  describe('Initialize healthcare services Action', () => {
+    it('has a type of INITIALIZE_HEALTHCARE_SERVICES', () => {
       const expected = {
-        type: DEFAULT_ACTION,
+        type: INITIALIZE_HEALTHCARE_SERVICES,
       };
-      expect(defaultAction()).toEqual(expected);
+      expect(initializeHealthcareServices()).toEqual(expected);
+    });
+  });
+  describe('Get Active Healthcare Services', () => {
+    it('has a type of GET_ACTIVE_HEALTHCARE_SERVICES', () => {
+      const expected = {
+        type: GET_ACTIVE_HEALTHCARE_SERVICES,
+      };
+      expect(getActiveHealthcareServices('2', 'Organization', 1)).toEqual(expected);
+    });
+  });
+  describe('Get filtered Healthcare Services', () => {
+    it('has a type of GET_FILTERED_HEALTHCARE_SERVICES', () => {
+      const expected = {
+        type: GET_FILTERED_HEALTHCARE_SERVICES,
+      };
+      expect(getFilteredHealthcareServices(1, true)).toEqual(expected);
     });
   });
 });
