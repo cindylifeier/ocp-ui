a3de4408c6fce999877def7a9a04c6e61837f201
burcakulug <burcak.ulug@feisystems.com>
2018-02-05 15:11:12 -0500

    Replace mock data with actual call in CareTeams api.js
    #17183:As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

--- app/containers/CareTeams/api.js
+++ app/containers/CareTeams/api.js
@@ -1,89 +1,9 @@
 import queryString from '../../utils/queryString';
 import getApiBaseUrl from '../../apiBaseUrlConfig';
 import { DEFAULT_PAGE_SIZE, DEFAULT_START_PAGE_NUMBER } from '../App/constants';
+import request from '../../utils/request';
 
 const baseApiUrl = getApiBaseUrl();
-
-const mockCareTeamsPage = {
-  size: 2,
-  totalNumberOfPages: 4,
-  currentPage: 1,
-  currentPageSize: 2,
-  hasNextPage: true,
-  hasPreviousPage: false,
-  firstPage: true,
-  lastPage: false,
-  totalElements: 3,
-  hasElements: true,
-  elements: [{
-    id: '111111',
-    name: 'My Care Team One',
-    statusCode: 'active',
-    statusDisplay: 'Active',
-    reasonCodes: [{
-      code: '109006',
-      display: 'Anxiety disorder of childhood OR adolescence',
-    }, {
-      code: '122003',
-      display: 'Choroidal hemorrhage',
-    },
-    ],
-    categoryCode: 'encounter',
-    categoryDisplay: 'Encounter',
-    subjectId: '11111',
-    subjectFirstName: 'Adam',
-    subjectLastName: 'Smith',
-    startDate: '01/01/2017',
-    endDate: '02/01/2017',
-    participants: [{
-      roleCode: '112247003',
-      roleDisplay: 'Medical doctor',
-      memberId: '700551',
-      memberFirstName: 'Gregory', // if not organization
-      memberLastName: 'House', // if not organization
-      memberType: 'practitioner', // can be one of `patient`, `practitioner`, `organization` or `relatedPerson`
-      onBehalfOfId: '143191',
-      onBehalfOfName: 'Great Cross Hospital',
-
-    }, {
-      roleCode: '394765007',
-      roleDisplay: 'Prison practice',
-      memberId: '650858',
-      memberName: 'Great Cross Hospital', // if organization
-      memberType: 'organization', // can be one of `patient`, `practitioner`, `organization` or `relatedPerson`
-      onBehalfOfId: '143191',
-      onBehalfOfName: 'Great Cross Hospital',
-    },
-    ],
-  }, {
-    id: '222222',
-    name: 'My Care Team Two',
-    statusCode: 'active',
-    statusDisplay: 'Active',
-    reasonCodes: [{
-      code: '134006',
-      display: 'Decreased hair growth',
-    },
-    ],
-    categoryCode: 'condition',
-    categoryDisplay: 'Condition',
-    subjectId: '11111',
-    subjectFirstName: 'Adam',
-    subjectLastName: 'Smith',
-    startDate: '04/01/2017',
-    endDate: '08/01/2017',
-    participants: [{
-      roleCode: '113157001',
-      roleDisplay: 'Grand-mother',
-      memberId: '121212',
-      memberFirstName: 'Granny', // if not organization
-      memberLastName: 'Smith', // if not organization
-      memberType: 'relatedPerson', // can be one of `patient`, `practitioner`, `organization` or `relatedPerson`
-    },
-    ],
-  },
-  ],
-};
 
 export default function getCareTeams(query, statusList) {
   const { pageNumber = DEFAULT_START_PAGE_NUMBER, pageSize = DEFAULT_PAGE_SIZE } = query;
@@ -95,8 +15,5 @@ export default function getCareTeams(query, statusList) {
   };
   const params = queryString(q);
   const requestURL = `${baseApiUrl}/careTeams/search${params}`;
-  // TODO: remove once the backend is implemented
-  console.log(`stubbing call for ${requestURL}`);
-  // return request(requestURL);
-  return new Promise((resolve) => setTimeout(() => resolve(mockCareTeamsPage), 2000));
+  return request(requestURL);
 }
