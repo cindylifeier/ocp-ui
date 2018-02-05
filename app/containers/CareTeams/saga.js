da8b0bc16bfbed9831f6442419e591695c38dc39
burcakulug <burcak.ulug@feisystems.com>
2018-02-05 15:43:52 -0500

    Improve error handling and notifications in CareTeams saga
    #17183:As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

--- app/containers/CareTeams/saga.js
+++ app/containers/CareTeams/saga.js
@@ -4,13 +4,28 @@ import { getCareTeamsError, getCareTeamsSuccess } from './actions';
 import getCareTeamsApi from './api';
 import { showNotification } from '../Notification/actions';
 
+function getErrorMessage(err) {
+  let errorMessage = '';
+  if (err && err.message === 'Failed to fetch') {
+    errorMessage = 'Failed to retrieve patient\'s care teams. Server is offline.';
+  } else if (err && err.response && err.response.status === 404) {
+    errorMessage = 'The patient does not have any care teams.';
+  } else if (err && err.response && err.response.status === 500) {
+    errorMessage = 'Failed to retrieve patient\'s care teams. Unknown server error.';
+  } else {
+    errorMessage = 'Failed to retrieve patient\'s care teams. Unknown error.';
+  }
+  return errorMessage;
+}
+
 export function* getCareTeams({ query, statusList }) {
   try {
     const careTeamsPage = yield call(getCareTeamsApi, query, [DEFAULT_CARE_TEAM_STATUS_CODE, ...statusList]);
     yield put(getCareTeamsSuccess(careTeamsPage));
   } catch (err) {
+    const errMsg = getErrorMessage(err);
     yield put(getCareTeamsError(err));
-    yield put(showNotification('Failed to retrieve patient\'s care teams, please try again.'));
+    yield put(showNotification(errMsg));
   }
 }
 
