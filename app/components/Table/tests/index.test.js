a669a815d4b8bf8e3511e7579b9bafbf0344003d
burcakulug <burcak.ulug@feisystems.com>
2018-02-05 17:02:27 -0500

    Add unit test for Table component
    #17183:As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

--- app/components/Table/tests/index.test.js
+++ app/components/Table/tests/index.test.js
@@ -1,10 +1,28 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import Table from '../index';
+import Table from '../index';
+import TableHeaderColumn from '../../TableHeaderColumn';
+import TableHeader from '../../TableHeader';
+import TableRow from '../../TableRow';
+import TableRowColumn from '../../TableRowColumn';
 
 describe('<Table />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('shoud match snapshot', () => {
+    // Act
+    const renderedComponent = shallow(
+      (<Table>
+        <TableHeader>
+          <TableHeaderColumn>Col</TableHeaderColumn>
+        </TableHeader>
+        <TableRow>
+          <TableRowColumn>Row</TableRowColumn>
+        </TableRow>
+      </Table>),
+    );
+
+    // Assert
+    expect(renderedComponent).toMatchSnapshot();
   });
-});
+})
+;
