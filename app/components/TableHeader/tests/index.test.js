b1b9dcb583cb3e4252e9f78c0bdb4992c473dd19
burcakulug <burcak.ulug@feisystems.com>
2018-02-06 09:33:03 -0500

    Add unit test for TableHeader
    #17183:As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

--- app/components/TableHeader/tests/index.test.js
+++ app/components/TableHeader/tests/index.test.js
@@ -1,10 +1,19 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import TableHeader from '../index';
+import TableHeader from '../index';
+import TableHeaderColumn from '../../TableHeaderColumn';
 
 describe('<TableHeader />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('shoud match snapshot', () => {
+    // Act
+    const renderedComponent = shallow(
+      (<TableHeader>
+        <TableHeaderColumn>Col</TableHeaderColumn>
+      </TableHeader>),
+    );
+
+    // Assert
+    expect(renderedComponent).toMatchSnapshot();
   });
 });
