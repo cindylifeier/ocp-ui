ff1a688a41b91e1092d692978c611a68efbbe09f
Shruti Rao <shruti.rao@feisystems.com>
2018-02-06 15:12:28 -0500

    Refactor container name
    #17531:As a user, I can assign a Health Care Service for a location, so that Health Care Service can exist in the OCP location from its parent organization

--- app/containers/AssignHealthCareServiceToLocationPage/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import assignHealthCareServiceToLocationPageReducer from '../reducer';
-
-describe('assignHealthCareServiceToLocationPageReducer', () => {
-  it('returns the initial state', () => {
-    expect(assignHealthCareServiceToLocationPageReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
