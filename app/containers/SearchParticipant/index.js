c70f5c02dfe9fe420bad000d7ace729903643e3f
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-02-07 17:26:26 -0500

    Implement start and end datepicker field and also participant role dropdown in the search result row.
    17182 - As a user, I can create a care teams for the patient with practitioner.

--- app/containers/SearchParticipant/index.js
+++ app/containers/SearchParticipant/index.js
@@ -18,7 +18,7 @@ import Dialog from 'material-ui/Dialog';
 import MenuItem from 'material-ui/MenuItem';
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
-import { IconButton, RaisedButton, teal500, white } from 'material-ui';
+import { DatePicker, IconButton, RaisedButton, teal500, white } from 'material-ui';
 import ActionSearch from 'material-ui/svg-icons/action/search';
 import reducer from './reducer';
 import saga from './saga';
@@ -26,7 +26,7 @@ import saga from './saga';
 import styles from './styles.css';
 import messages from './messages';
 import { fieldStyle, floatingLabelStyle, iconButtonStyle } from './constants';
-import { makeSelectParticipantTypes } from '../App/selectors';
+import { makeSelectParticipantRoles, makeSelectParticipantTypes } from '../App/selectors';
 import TextField from '../../components/TextField';
 import SelectField from '../../components/SelectField';
 import { addParticipants, getSearchParticipant, initializeSearchParticipant } from './actions';
@@ -36,7 +36,8 @@ import TableHeaderColumn from '../../components/TableHeaderColumn';
 import TableRow from '../../components/TableRow';
 import TableRowColumn from '../../components/TableRowColumn';
 import TableHeader from '../../components/TableHeader';
-import { getParticipantName } from '../../utils/CareTeamUtils';
+import { DATE_PICKER_MODE, getParticipantName } from '../../utils/CareTeamUtils';
+
 
 export class SearchParticipant extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -48,43 +49,127 @@ export class SearchParticipant extends React.PureComponent { // eslint-disable-l
     this.addParticipant = this.addParticipant.bind(this);
     this.handleDialogClose = this.handleDialogClose.bind(this);
   }
+
   componentWillMount() {
     this.props.initializeSearchParticipant();
   }
+
   addParticipant(participant) {
     this.handleDialogClose();
     const selected = [];
     selected.push(participant);
     this.props.addParticipants(selected);
   }
+
   handleDialogClose() {
     this.setState({ open: false });
     this.props.handleClose();
   }
+
   handleSearch(values) {
     const { name, member } = values;
     this.props.searchParticipant(name, member);
   }
+
+  createSearchResultHeader() {
+    return (<Table>
+      <TableHeader>
+        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderName} />}</TableHeaderColumn>
+        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderRole} />}</TableHeaderColumn>
+        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderPeriod} />}</TableHeaderColumn>
+        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderAction} />}</TableHeaderColumn>
+      </TableHeader>
+    </Table>);
+  }
+
   createSearchResultRows() {
+    const today = new Date();
+    const participantRoles = this.props.participantRoles;
     return this.props.searchParticipantResult.map((participant) => (
-      <TableRow key={uniqueId()}>
-        <TableRowColumn> { getParticipantName(participant) } </TableRowColumn>
-        <TableRowColumn></TableRowColumn>
-        <TableRowColumn></TableRowColumn>
-        <TableRowColumn>
-          <RaisedButton
-            backgroundColor={teal500}
-            labelColor={white}
-            label={<FormattedMessage {...messages.addParticipantBtnLabel} />}
-            type="button"
-            value={participant}
-            onClick={() => this.addParticipant(participant)}
-            primary
-          />
-        </TableRowColumn>
-      </TableRow>
+      <Formik
+        key={uniqueId()}
+        initialValues={{}}
+        onSubmit={(values, actions) => {
+          this.handleSearch(values);
+          actions.setSubmitting(false);
+        }}
+        validationSchema={yup.object().shape({})}
+        render={(formikProps) => {
+          const { isSubmitting, dirty, isValid } = formikProps;
+          return (
+            <Form>
+              <Table>
+                <TableRow key={uniqueId()}>
+                  <TableRowColumn> {getParticipantName(participant)} </TableRowColumn>
+                  <TableRowColumn>
+                    <SelectField
+                      name="role"
+                      fullWidth
+                      floatingLabelText={<FormattedMessage {...messages.floatingLabelText.participantRole} />}
+                    >
+                      {participantRoles && participantRoles.map((participantRole) => (
+                        <MenuItem value={participantRole.code} primaryText={participantRole.display} key={uniqueId()} />
+                      ))}
+                    </SelectField>
+                  </TableRowColumn>
+                  <TableRowColumn>
+                    <DatePicker
+                      fullWidth
+                      name="startDate"
+                      minDate={today}
+                      mode={DATE_PICKER_MODE.LANDSCAPE}
+                      hintText={<FormattedMessage {...messages.hintText.startDate} />}
+                      floatingLabelText={<FormattedMessage {...messages.floatingLabelText.startDate} />}
+                    />
+                  </TableRowColumn>
+                  <TableRowColumn>
+                    <DatePicker
+                      fullWidth
+                      name="endDate"
+                      minDate={today}
+                      mode={DATE_PICKER_MODE.LANDSCAPE}
+                      hintText={<FormattedMessage {...messages.hintText.endDate} />}
+                      floatingLabelText={<FormattedMessage {...messages.floatingLabelText.endDate} />}
+                    />
+                  </TableRowColumn>
+                  <TableRowColumn>
+                    <RaisedButton
+                      backgroundColor={teal500}
+                      labelColor={white}
+                      label={<FormattedMessage {...messages.addParticipantBtnLabel} />}
+                      type="button"
+                      value={participant}
+                      onClick={() => this.addParticipant(participant)}
+                      primary
+                      disabled={!dirty || isSubmitting || !isValid}
+                    />
+                  </TableRowColumn>
+                </TableRow>
+              </Table>
+            </Form>
+          );
+        }}
+      />
     ));
   }
+
+  createNoSearchResultTable() {
+    return (<Table>
+      <TableHeader>
+        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderName} />}</TableHeaderColumn>
+        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderRole} />}</TableHeaderColumn>
+        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderPeriod} />}</TableHeaderColumn>
+        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderAction} />}</TableHeaderColumn>
+      </TableHeader>
+      <TableRow key={uniqueId()}>
+        <TableRowColumn> {<FormattedMessage {...messages.noSearchParticipantResult} />}</TableRowColumn>
+        <TableRowColumn> </TableRowColumn>
+        <TableRowColumn> </TableRowColumn>
+        <TableRowColumn> </TableRowColumn>
+      </TableRow>
+    </Table>);
+  }
+
   render() {
     const { participantTypes, isOpen, searchParticipantResult } = this.props;
     const actionsButtons = [
@@ -130,7 +215,7 @@ export class SearchParticipant extends React.PureComponent { // eslint-disable-l
                       >
                         {participantTypes && participantTypes.map((member) =>
                           <MenuItem key={member.code} value={member.code} primaryText={member.display} />,
-                          )
+                        )
                         }
                       </SelectField>
                     </div>
@@ -144,23 +229,17 @@ export class SearchParticipant extends React.PureComponent { // eslint-disable-l
                         <ActionSearch />
                       </IconButton>
                     </div>
+                    <div className={styles.gridItem}>
+                    </div>
                   </div>
-                  { searchParticipantResult && searchParticipantResult.length > 0 &&
-                    <Table>
-                      <TableHeader>
-                        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderName} />}</TableHeaderColumn>
-                        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderRole} />}</TableHeaderColumn>
-                        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderPeriod} />}</TableHeaderColumn>
-                        <TableHeaderColumn>{<FormattedMessage {...messages.participantTableHeaderAction} />}</TableHeaderColumn>
-                      </TableHeader>
-                      { this.createSearchResultRows() }
-                    </Table>
-                  }
                 </div>
               </Form>
             );
           }}
         />
+        {searchParticipantResult && searchParticipantResult.length > 0 && this.createSearchResultHeader()}
+        {searchParticipantResult && searchParticipantResult.length > 0 && this.createSearchResultRows()}
+        {searchParticipantResult && searchParticipantResult.length === 0 && this.createNoSearchResultTable()}
       </Dialog>
     );
   }
@@ -173,6 +252,7 @@ SearchParticipant.propTypes = {
   initializeSearchParticipant: PropTypes.func.isRequired,
   addParticipants: PropTypes.func.isRequired,
   searchParticipantResult: PropTypes.array,
+  participantRoles: PropTypes.array,
   participantTypes: PropTypes.arrayOf(PropTypes.shape({
     code: PropTypes.string.isRequired,
     display: PropTypes.string.isRequired,
@@ -184,6 +264,7 @@ SearchParticipant.propTypes = {
 const mapStateToProps = createStructuredSelector({
   participantTypes: makeSelectParticipantTypes(),
   searchParticipantResult: makeSelectSearchParticipantResults(),
+  participantRoles: makeSelectParticipantRoles(),
 });
 
 function mapDispatchToProps(dispatch) {
