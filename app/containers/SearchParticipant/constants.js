6aae528878694c4e34d51c7175c23a1963f614a7
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-02-07 08:47:20 -0500

    Add remove participant constant.
    17182 - As a user, I can create a care teams for the patient with practitioner.

--- app/containers/SearchParticipant/constants.js
+++ app/containers/SearchParticipant/constants.js
@@ -19,8 +19,9 @@ export const SEARCH_PARTICIPANT_SUCCESS = 'ocpui/SearchParticipant/SEARCH_PARTIC
 export const SEARCH_PARTICIPANT_ERROR = 'ocpui/SearchParticipant/SEARCH_PARTICIPANT_ERROR';
 export const ADD_PARTICIPANT = 'ocpui/SearchParticipant/ADD_PARTICIPANT';
 export const INITIALIZE_SEARCH_PARTICIPANT = 'ocpui/SearchParticipant/INITIALIZE_SEARCH_PARTICIPANT';
-
+export const REMOVE_PARTICIPANT = 'ocpui/SearchParticipant/REMOVE_PARTICIPANT';
 /**
 *  Other constants
 *
 */
+
