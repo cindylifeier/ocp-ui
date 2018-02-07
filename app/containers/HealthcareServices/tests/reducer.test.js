1f832077148934d894b494566e50e19c467c736c
burcakulug <burcak.ulug@feisystems.com>
2018-02-07 09:57:22 -0500

    Add unit tests for HealthcareServices reducer initial state
    #17533:As a user, I can see a list of Health Care Services related to a location in the OCP, so that I know all of the Health Care Services in the system.

--- app/containers/HealthcareServices/tests/reducer.test.js
+++ app/containers/HealthcareServices/tests/reducer.test.js
@@ -1,9 +1,17 @@
-
 import { fromJS } from 'immutable';
 import healthcareServicesReducer from '../reducer';
 
 describe('healthcareServicesReducer', () => {
   it('returns the initial state', () => {
-    expect(healthcareServicesReducer(undefined, {})).toEqual(fromJS({}));
+    expect(healthcareServicesReducer(undefined, {})).toEqual(fromJS({
+      loading: false,
+      error: false,
+      data: [],
+      organization: null,
+      location: null,
+      currentPage: 0,
+      totalNumberOfPages: 0,
+      includeInactive: false,
+    }));
   });
 });
