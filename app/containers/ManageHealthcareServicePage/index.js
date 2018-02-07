80c3ac23ae27390b512c9b9df2fefb59cbaba27f
sadhch <sadhana.chandravanka@feisystems.com>
2018-02-07 18:05:14 -0500

    Fetch Look up data for Health Care Service Resource
    17159 - As a user, I can create a Health Care Service for an organization, so that Health Care Service can exist in the OCP organization

--- app/containers/ManageHealthcareServicePage/index.js
+++ app/containers/ManageHealthcareServicePage/index.js
@@ -8,42 +8,81 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
-import makeSelectManageHealthcareServicePage from './selectors';
+import { makeSelectHealthcareServiceCategories,
+        makeSelectHealthcareServiceTypes,
+        makeSelectHealthcareServiceReferralMethods,
+        makeSelectHealthcareServiceSpecialities,
+        makeSelectTelecomSystems,
+        makeSelectTelecomUses } from '../App/selectors';
+import { getLookupsAction } from '../App/actions';
 import reducer from './reducer';
 import saga from './saga';
-import messages from './messages';
+import ManageHealthcareService from '../../components/ManageHealthcareService';
+import { HEALTHCARESERVICECATEGORY, HEALTHCARESERVICETYPE, HEALTHCARESERVICEREFERRALMETHOD, HEALTHCARESERVICESPECIALITY, TELECOMSYSTEM, TELECOMUSE } from '../App/constants';
+
 
 export class ManageHealthcareServicePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  componentWillMount() {
+    this.props.getLookups();
+  }
+
   render() {
+    const {
+      healthCareServiceCategories,
+      healthCareServiceTypes,
+      healthcareServiceReferralMethods,
+      healthcareServiceSpecialities,
+      telecomSystems,
+      telecomUses,
+      } = this.props;
+    const hcsProps = {
+      healthCareServiceCategories,
+      healthCareServiceTypes,
+      healthcareServiceReferralMethods,
+      healthcareServiceSpecialities,
+      telecomSystems,
+      telecomUses,
+    };
     return (
       <div>
         <Helmet>
           <title>ManageHealthcareServicePage</title>
           <meta name="description" content="Description of ManageHealthcareServicePage" />
         </Helmet>
-        <FormattedMessage {...messages.header} />
+        <ManageHealthcareService {...hcsProps} />
       </div>
     );
   }
 }
 
 ManageHealthcareServicePage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  getLookups: PropTypes.func.isRequired,
+  healthCareServiceCategories: PropTypes.array,
+  healthCareServiceTypes: PropTypes.array,
+  healthcareServiceReferralMethods: PropTypes.array,
+  healthcareServiceSpecialities: PropTypes.array,
+  telecomSystems: PropTypes.array,
+  telecomUses: PropTypes.array,
 };
 
 const mapStateToProps = createStructuredSelector({
-  managehealthcareservicepage: makeSelectManageHealthcareServicePage(),
+  healthCareServiceCategories: makeSelectHealthcareServiceCategories(),
+  healthCareServiceTypes: makeSelectHealthcareServiceTypes(),
+  healthcareServiceReferralMethods: makeSelectHealthcareServiceReferralMethods(),
+  healthcareServiceSpecialities: makeSelectHealthcareServiceSpecialities(),
+  telecomSystems: makeSelectTelecomSystems(),
+  telecomUses: makeSelectTelecomUses(),
 });
 
 function mapDispatchToProps(dispatch) {
   return {
-    dispatch,
+    getLookups: () => dispatch(getLookupsAction([HEALTHCARESERVICECATEGORY, HEALTHCARESERVICETYPE, HEALTHCARESERVICEREFERRALMETHOD, HEALTHCARESERVICESPECIALITY, TELECOMSYSTEM, TELECOMUSE])),
+
   };
 }
 
