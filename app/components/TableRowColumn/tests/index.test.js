bbcf800669d82aeaa60bd9179b564664c434a36f
burcakulug <burcak.ulug@feisystems.com>
2018-02-08 15:53:31 -0500

    Add enzyme adapter configuration and update snapshots in TableRowColumn
    #17183:As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

--- app/components/TableRowColumn/tests/index.test.js
+++ app/components/TableRowColumn/tests/index.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 
 import TableRowColumn from '../index';
+
+configure({ adapter: new Adapter() });
 
 describe('<TableRowColumn />', () => {
   it('should match snapshot', () => {
