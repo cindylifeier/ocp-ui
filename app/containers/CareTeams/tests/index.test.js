f31c18e2d96ff13da995f826266f11d2c0fefcb2
burcakulug <burcak.ulug@feisystems.com>
2018-02-08 16:08:38 -0500

    Add unit tests for CareTeams container
    #17183:As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

--- app/containers/CareTeams/tests/index.test.js
+++ app/containers/CareTeams/tests/index.test.js
@@ -1,10 +1,188 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 
-// import { CareTeams } from '../index';
+import { CareTeams } from '../index';
+
+configure({ adapter: new Adapter() });
 
 describe('<CareTeams />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  describe('Snapshots testing', () => {
+    it('should match snapshot', () => {
+      // Arrange
+      const initializeLookups = jest.fn();
+      const getCareTeams = jest.fn();
+      const query = { a1: 'a1', a2: 'a2' };
+      const patientName = 'patientName';
+      const statusList = ['active', 'inactive'];
+      const loading = false;
+      const elements = [{
+        id: '1',
+        name: 'a',
+        subjectId: 'subjectId1',
+        subjectFirstName: 'subjectFirstName1',
+        subjectLastName: 'subjectLastName1',
+      }, {
+        id: '2',
+        name: 'b',
+        subjectId: 'subjectId2',
+        subjectFirstName: 'subjectFirstName2',
+        subjectLastName: 'subjectLastName2',
+      }];
+      const currentPage = 10;
+      const totalNumberOfPages = 100;
+      const data = {
+        elements,
+        currentPage,
+        totalNumberOfPages,
+      };
+      const careTeams = {
+        loading,
+        data,
+        query,
+        patientName,
+        statusList,
+      };
+      const careTeamStatuses = [{
+        code: 'active',
+        display: 'Active',
+      }, {
+        code: 'inactive',
+        display: 'Inactive',
+      }, {
+        code: 'suspended',
+        display: 'suspended',
+      }];
+      const props = {
+        getCareTeams,
+        initializeLookups,
+        careTeams,
+        careTeamStatuses,
+      };
+
+      // Act
+      const renderedComponent = shallow(<CareTeams {...props} />);
+
+      // Assert
+      expect(renderedComponent).toMatchSnapshot();
+    });
+  });
+
+  it('should contain patient name', () => {
+    // Arrange
+    const initializeLookups = jest.fn();
+    const getCareTeams = jest.fn();
+    const query = { a1: 'a1', a2: 'a2' };
+    const patientName = 'patientName';
+    const statusList = ['active', 'inactive'];
+    const loading = false;
+    const elements = [{
+      id: '1',
+      name: 'a',
+      subjectId: 'subjectId1',
+      subjectFirstName: 'subjectFirstName1',
+      subjectLastName: 'subjectLastName1',
+    }, {
+      id: '2',
+      name: 'b',
+      subjectId: 'subjectId2',
+      subjectFirstName: 'subjectFirstName2',
+      subjectLastName: 'subjectLastName2',
+    }];
+    const currentPage = 10;
+    const totalNumberOfPages = 100;
+    const data = {
+      elements,
+      currentPage,
+      totalNumberOfPages,
+    };
+    const careTeams = {
+      loading,
+      data,
+      query,
+      patientName,
+      statusList,
+    };
+    const careTeamStatuses = [{
+      code: 'active',
+      display: 'Active',
+    }, {
+      code: 'inactive',
+      display: 'Inactive',
+    }, {
+      code: 'suspended',
+      display: 'suspended',
+    }];
+    const props = {
+      getCareTeams,
+      initializeLookups,
+      careTeams,
+      careTeamStatuses,
+    };
+
+    // Act
+    const renderedComponent = shallow(<CareTeams {...props} />);
+
+    // Assert
+    expect(renderedComponent.contains(patientName)).toBe(true);
+  });
+
+  it('should call initializeLookups', () => {
+    // Arrange
+    const initializeLookups = jest.fn();
+    const getCareTeams = jest.fn();
+    const query = { a1: 'a1', a2: 'a2' };
+    const patientName = 'patientName';
+    const statusList = ['active', 'inactive'];
+    const loading = false;
+    const elements = [{
+      id: '1',
+      name: 'a',
+      subjectId: 'subjectId1',
+      subjectFirstName: 'subjectFirstName1',
+      subjectLastName: 'subjectLastName1',
+    }, {
+      id: '2',
+      name: 'b',
+      subjectId: 'subjectId2',
+      subjectFirstName: 'subjectFirstName2',
+      subjectLastName: 'subjectLastName2',
+    }];
+    const currentPage = 10;
+    const totalNumberOfPages = 100;
+    const data = {
+      elements,
+      currentPage,
+      totalNumberOfPages,
+    };
+    const careTeams = {
+      loading,
+      data,
+      query,
+      patientName,
+      statusList,
+    };
+    const careTeamStatuses = [{
+      code: 'active',
+      display: 'Active',
+    }, {
+      code: 'inactive',
+      display: 'Inactive',
+    }, {
+      code: 'suspended',
+      display: 'suspended',
+    }];
+    const props = {
+      getCareTeams,
+      initializeLookups,
+      careTeams,
+      careTeamStatuses,
+    };
+
+    // Act
+    shallow(<CareTeams {...props} />);
+
+    // Assert
+    expect(initializeLookups).toHaveBeenCalledTimes(1);
   });
 });
