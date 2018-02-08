5ae9372b83fba690af758002161642365a9385af
JiahaoLi <Jiahao.Li@feisystems.com>
2018-02-08 13:21:34 -0500

    Implement to set initial selected participants for searchParticipant component
    17182: As a user, I can create a care teams for the patient with practitioner.

--- app/utils/CareTeamUtils.js
+++ app/utils/CareTeamUtils.js
@@ -1,10 +1,11 @@
 import isEmpty from 'lodash/isEmpty';
 import { EMPTY_STRING } from '../containers/App/constants';
+import Util from './Util';
 
 const ORGANIZATION_TYPE = 'organization';
 
 export const getParticipantName = (participant) => {
-  if (participant && participant.member && participant.member.type === ORGANIZATION_TYPE) {
+  if (participant && participant.member && Util.equalsIgnoreCase(participant.member.type, ORGANIZATION_TYPE)) {
     return participant.member.name;
   }
   const firstName = isEmpty(participant.member.firstName) ? EMPTY_STRING : participant.member.firstName;
