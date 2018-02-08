56d9abbc37d7e496628bd18bce00ce1a0d014f57
burcakulug <burcak.ulug@feisystems.com>
2018-02-08 15:50:30 -0500

    Add enzyme adapter configuration and update snapshots in TableHeader
    #17183:As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

--- app/components/TableHeader/tests/index.test.js
+++ app/components/TableHeader/tests/index.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 
 import TableHeader from '../index';
 import TableHeaderColumn from '../../TableHeaderColumn';
+
+configure({ adapter: new Adapter() });
 
 describe('<TableHeader />', () => {
   it('should match snapshot', () => {
