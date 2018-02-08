4939629b504a775ea1925e95746b4308ef721bea
sadhch <sadhana.chandravanka@feisystems.com>
2018-02-08 01:13:09 -0500

    Add Validation for the required fields
    17159 - As a user, I can create a Health Care Service for an organization, so that Health Care Service can exist in the OCP organization

--- app/components/ManageHealthcareService/index.js
+++ app/components/ManageHealthcareService/index.js
@@ -6,17 +6,36 @@
 
 import React from 'react';
 import { Formik } from 'formik';
+import yup from 'yup';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 import ManageHealthcareServiceForm from './ManageHealthcareServiceForm';
+import messages from '../ManagePractitioner/messages';
+import { TEXT_MIN_LENGTH } from '../../containers/ManagePractitionerPage/constants';
 
 function ManageHealthcareService(props) {
   const { onSave } = props;
+  const minimumLength = TEXT_MIN_LENGTH;
   return (
     <div>
       <Formik
         onSubmit={(values, actions) => {
           onSave(values, actions);
         }}
+        validationSchema={yup.object().shape({
+          name: yup.string()
+            .required((<FormattedMessage {...messages.validation.required} />))
+            .min(minimumLength, (
+              <FormattedMessage {...messages.validation.minLength} values={{ minimumLength }} />)),
+          programName: yup.string()
+            .required((<FormattedMessage {...messages.validation.required} />))
+            .min(minimumLength, (
+              <FormattedMessage {...messages.validation.minLength} values={{ minimumLength }} />)),
+          type: yup.string()
+            .required((<FormattedMessage {...messages.validation.required} />)),
+          category: yup.string()
+            .required((<FormattedMessage {...messages.validation.required} />)),
+        })}
         render={(formikProps) => <ManageHealthcareServiceForm {...formikProps} {...props} />}
       />
     </div>
