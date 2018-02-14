3b36ccc333d393d2e4fa40cab2201dc3e93e90fd
sadhch <sadhana.chandravanka@feisystems.com>
2018-02-14 13:13:08 -0500

    Add Initial container component of react pure component type for Activity Definition Resource
    17899 - As a user, I can create activity definition at organization level

---
+++ app/containers/ManageActivityDefinitionPage/messages.js
@@ -0,0 +1,13 @@
+/*
+ * ManageActivityDefinitionPage Messages
+ *
+ * This contains all the text for the ManageActivityDefinitionPage component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  createHeader: {
+    id: 'ocpui.containers.ManageActivityDefinitionPage.createHeader',
+    defaultMessage: 'Create Activity Definition',
+  },
+});
---
+++ app/containers/ManageActivityDefinitionPage/tests/selectors.test.js
@@ -0,0 +1,8 @@
+// import { fromJS } from 'immutable';
+// import { selectManageActivityDefinitionPageDomain } from '../selectors';
+
+describe('selectManageActivityDefinitionPageDomain', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
