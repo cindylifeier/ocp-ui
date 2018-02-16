b91da59d1e6180cde144ed54208e13d985d0f3cc
JiahaoLi <Jiahao.Li@feisystems.com>
2018-02-16 00:13:15 -0500

    Implement initial login presentational component
    17894: As a user, I can login OCP and get authorized, so that I can access OCP resources

---
+++ app/components/Login/index.js
@@ -0,0 +1,98 @@
+/**
+ *
+ * Login
+ *
+ */
+
+import React from 'react';
+import { Card } from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Form, Formik } from 'formik';
+import PropTypes from 'prop-types';
+import { teal500, white } from 'material-ui/styles/colors';
+
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+import ocpLogo from '../../images/omnibus-care-plan-logo.png';
+import styles from './styles.css';
+
+const inlineStyles = {
+  divider: {
+    backgroundColor: teal500,
+    height: '3px',
+  },
+  card: {
+    borderWidth: '1px',
+    backgroundColor: 'rgba(242, 242, 242, 1)',
+  },
+};
+
+function Login(props) {
+  const { onLogin } = props;
+
+  return (
+    <div>
+      <div>
+        <img className={styles.logo} src={ocpLogo} alt="ocp logo" />
+      </div>
+      <Divider style={inlineStyles.divider} />
+      <Card style={inlineStyles.card} className={styles.loginCard}>
+        <div className={styles.title}>
+          <FormattedMessage {...messages.title} />
+        </div>
+        <Formik
+          onSubmit={(values) => {
+            onLogin(values);
+          }}
+          render={(loginFormProps) => {
+            const { isSubmitting, dirty, isValid } = loginFormProps;
+            return (
+              <Form>
+                <div className={styles.gridContainer}>
+                  <div className={`${styles.gridItem} ${styles.username}`}>
+                    <TextField
+                      name="username"
+                      hintText={<FormattedMessage {...messages.hintText.username} />}
+                      floatingLabelText={<FormattedMessage {...messages.floatingLabelText.username} />}
+                      fullWidth
+                    />
+                  </div>
+                  <div className={`${styles.gridItem} ${styles.password}`}>
+                    <TextField
+                      name="password"
+                      hintText={<FormattedMessage {...messages.hintText.password} />}
+                      floatingLabelText={<FormattedMessage {...messages.floatingLabelText.password} />}
+                      fullWidth
+                    />
+                  </div>
+                  <div className={`${styles.gridItem}`}>
+                    <FormattedMessage {...messages.forgotLink} />
+                  </div>
+                  <div className={`${styles.gridItem} ${styles.button}`}>
+                    <RaisedButton
+                      type="submit"
+                      backgroundColor={teal500}
+                      labelColor={white}
+                      fullWidth
+                      label={<FormattedMessage {...messages.loginButton} />}
+                      disabled={!dirty || isSubmitting || !isValid}
+                    />
+                  </div>
+                </div>
+              </Form>
+            );
+          }}
+        >
+        </Formik>
+      </Card>
+    </div>
+  );
+}
+
+Login.propTypes = {
+  onLogin: PropTypes.func.isRequired,
+};
+
+export default Login;
