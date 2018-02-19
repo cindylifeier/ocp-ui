0bf4cd0c1fe78256eac212db4f3cc417400d9174
burcakulug <burcak.ulug@feisystems.com>
2018-02-19 15:32:48 -0500

    Refactor TableHeaderStyleGrid unit tests
    #17891:[Enabler] As a developer, I can use styled components to compose UI, so that the UI has consistent look and feel

--- app/components/TableHeader/tests/TableHeaderStyledGrid.test.js
+++ app/components/TableHeader/tests/TableHeaderStyledGrid.test.js
@@ -8,45 +8,69 @@ import TableHeaderStyledGrid from '../TableHeaderStyledGrid';
 configure({ adapter: new Adapter() });
 
 describe('<TableHeaderStyledGrid />', () => {
-  it('should have children', () => {
-    // Arrange
-    const children = (<span>test</span>);
+  describe('structural tests', () => {
+    it('should have children', () => {
+      // Arrange
+      const children = (<span>test</span>);
 
-    // Act
-    const renderedComponent = shallow(<TableHeaderStyledGrid>{children}</TableHeaderStyledGrid>);
+      // Act
+      const renderedComponent = shallow(<TableHeaderStyledGrid>{children}</TableHeaderStyledGrid>);
 
-    // Assert
-    expect(renderedComponent.contains(children)).toEqual(true);
+      // Assert
+      expect(renderedComponent.contains(children)).toEqual(true);
+    });
   });
 
-  it('should be grid', () => {
-    // Arrange
-    const children = (<span>test</span>);
+  describe('style tests', () => {
+    it('should be grid', () => {
+      // Arrange
+      const children = (<span>test</span>);
 
-    // Act
-    const renderedComponent = shallow(<TableHeaderStyledGrid>{children}</TableHeaderStyledGrid>);
+      // Act
+      const renderedComponent = shallow(<TableHeaderStyledGrid>{children}</TableHeaderStyledGrid>);
 
-    // Assert
-    expect(renderedComponent).toHaveStyleRule('display', 'grid');
-  });
-
-  it('should be have styles', () => {
-    // Arrange
-    const children = (<span>test</span>);
-
-    // Act
-    const renderedComponent = shallow(<TableHeaderStyledGrid>{children}</TableHeaderStyledGrid>);
-
-    // Assert
-    expect(renderedComponent).toHaveStyleRule('border-bottom', '1px outset rgb(51, 51, 51)');
-    expect(renderedComponent).toHaveStyleRule('background-color', '#f2f2f2', {
-      modifier: ':first-child',
+      // Assert
+      expect(renderedComponent).toHaveStyleRule('display', 'grid');
     });
-    expect(renderedComponent).toHaveStyleRule('font-size', '20px', {
-      modifier: ':first-child',
+
+    it('should have border-bottom', () => {
+      // Arrange
+      const children = (<span>test</span>);
+
+      // Act
+      const renderedComponent = shallow(<TableHeaderStyledGrid>{children}</TableHeaderStyledGrid>);
+
+      // Assert
+      expect(renderedComponent).toHaveStyleRule('border-bottom', '1px outset rgb(51, 51, 51)');
     });
-    expect(renderedComponent).toHaveStyleRule('background-color', '#f2f2f2', {
-      modifier: ':nth-child(odd)',
+
+    it('should have first-child styles', () => {
+      // Arrange
+      const children = (<span>test</span>);
+
+      // Act
+      const renderedComponent = shallow(<TableHeaderStyledGrid>{children}</TableHeaderStyledGrid>);
+
+      // Assert
+      expect(renderedComponent).toHaveStyleRule('background-color', '#f2f2f2', {
+        modifier: ':first-child',
+      });
+      expect(renderedComponent).toHaveStyleRule('font-size', '20px', {
+        modifier: ':first-child',
+      });
+    });
+
+    it('should have nth-child styles', () => {
+      // Arrange
+      const children = (<span>test</span>);
+
+      // Act
+      const renderedComponent = shallow(<TableHeaderStyledGrid>{children}</TableHeaderStyledGrid>);
+
+      // Assert
+      expect(renderedComponent).toHaveStyleRule('background-color', '#f2f2f2', {
+        modifier: ':nth-child(odd)',
+      });
     });
   });
 });
