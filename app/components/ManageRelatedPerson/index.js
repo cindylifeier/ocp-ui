d6352b8aff1dcf229d7e7d7258c68d190d9db7bc
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-02-19 09:22:30 -0500

    Start implementing manage related person presentation component.
    17550 - As a user, I would like to add related person to patient .

--- app/components/ManageRelatedPerson/index.js
+++ app/components/ManageRelatedPerson/index.js
@@ -5,21 +5,69 @@
 */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 // import styled from 'styled-components';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+// import { FormattedMessage } from 'react-intl';
+// import messages from './messages';
+// import ManagePatientForm from '../ManagePatient/ManagePatientForm';
+import ManageRelatedPersonForm from './ManageRelatedPersonForm';
+// import { TEXT_MIN_LENGTH } from '../../containers/ManageRelatedPersonPage/constants';
 
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
-function ManageRelatedPerson() {
+function ManageRelatedPerson(props) {
+  // const minimumLength = TEXT_MIN_LENGTH;
+  // const postalCodePattern = new RegExp('^\\d{5}(?:[-\\s]\\d{4})?$');
+  const { onSave, uspsStates, patientIdentifierSystems, administrativeGenders, telecomSystems, relationshipTypes } = props;
+  const lookUpFormData = {
+    uspsStates,
+    patientIdentifierSystems,
+    administrativeGenders,
+    telecomSystems,
+    relationshipTypes,
+  };
   return (
     <div>
-      <FormattedMessage {...messages.header} />
+      <Formik
+        initialValues={{}}
+        onSubmit={(values, actions) => {
+          onSave(values, actions);
+        }}
+        validationSchema={yup.object().shape({
+          // lastName: yup.string()
+          //   .required((<FormattedMessage {...messages.validation.required} />))
+          //   .min(minimumLength, (
+          //     <FormattedMessage {...messages.validation.minLength} values={{ minimumLength }} />)),
+          // firstName: yup.string()
+          //   .required((<FormattedMessage {...messages.validation.required} />))
+          //   .min(minimumLength, (
+          //     <FormattedMessage {...messages.validation.minLength} values={{ minimumLength }} />)),
+          // genderCode: yup.string()
+          //   .required((<FormattedMessage {...messages.validation.required} />)),
+          // birthDate: yup.date()
+          //   .required((<FormattedMessage {...messages.validation.required} />)),
+          // identifierType: yup.string()
+          //   .required((<FormattedMessage {...messages.validation.required} />)),
+          // identifierValue: yup.string()
+          //   .required((<FormattedMessage {...messages.validation.required} />)),
+          // postalCode: yup.string()
+          //   .matches(postalCodePattern, (<FormattedMessage {...messages.validation.postalCode} />)),
+        })}
+        render={(formikProps) => <ManageRelatedPersonForm {...formikProps} {...lookUpFormData} />}
+      />
     </div>
   );
 }
 
 ManageRelatedPerson.propTypes = {
-
+  onSave: PropTypes.func.isRequired,
+  uspsStates: PropTypes.array.isRequired,
+  patientIdentifierSystems: PropTypes.array.isRequired,
+  administrativeGenders: PropTypes.array.isRequired,
+  telecomSystems: PropTypes.array.isRequired,
+  relationshipTypes: PropTypes.array.isRequired,
+  // patient: PropTypes.object,
 };
 
 export default ManageRelatedPerson;
