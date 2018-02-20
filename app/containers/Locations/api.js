a091cf2fd4acd03718fe45e9affb2ceab6c3dfc9
JiahaoLi <Jiahao.Li@feisystems.com>
2018-02-20 15:49:01 -0500

    Refactor to get locations api base url via endpoint service
    17894: As a user, I can login OCP and get authorized, so that I can access OCP resources

--- app/containers/Locations/api.js
+++ app/containers/Locations/api.js
@@ -4,21 +4,21 @@
  *
  */
 
-import getApiBaseUrl from '../../apiBaseUrlConfig';
 import { DEFAULT_PAGE_SIZE } from '../App/constants';
-import { requestWithJWT } from '../../utils/request';
+import request from '../../utils/request';
+import { BASE_ORGANIZATIONS_API_URL, getEndpoint } from '../../utils/endpointService';
 
-const apiBaseUrl = getApiBaseUrl();
+const baseEndpoint = getEndpoint(BASE_ORGANIZATIONS_API_URL);
 
 export default function searchLocationsByIdAndStatus(organizationId, status, currentPage) {
   const url = createUrl(organizationId, status, currentPage);
-  return requestWithJWT(url);
+  return request(url, baseEndpoint.isSecured);
 }
 
 function createUrl(organizationId, status, currentPage) {
   const initialParams = `pageNumber=${currentPage}&pageSize=${DEFAULT_PAGE_SIZE}&statusList=active,`;
   let queryParams = '';
-  const baseUrl = `${apiBaseUrl}/organizations/${organizationId}`.concat('/locations?');
+  const baseUrl = `${baseEndpoint.url}/${organizationId}`.concat('/locations?');
   if (status && status.length === 0) {
     queryParams = initialParams;
   } else if (status && status.length === 1) {
