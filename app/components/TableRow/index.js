6f86d7fd19732656721d097ef010d81a23880fba
burcakulug <burcak.ulug@feisystems.com>
2018-02-20 17:19:06 -0500

    Enhance TableRow component to accept additional properties
    #17891:[Enabler] As a developer, I can use styled components to compose UI, so that the UI has consistent look and feel

--- app/components/TableRow/index.js
+++ app/components/TableRow/index.js
@@ -6,11 +6,18 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import pickBy from 'lodash/pickBy';
+
 import TableRowStyledGrid from './TableRowStyledGrid';
 
-function TableRow({ children }) {
+function TableRow({ children, columns, onClick, onKeyPress, role, tabIndex }) {
+  const additionalProps = pickBy({ onClick, onKeyPress, role, tabIndex });
   return (
-    <TableRowStyledGrid gap="5px" columns={`repeat(${React.Children.count(children)}, 1fr)`}>
+    <TableRowStyledGrid
+      gap="5px"
+      columns={columns || `repeat(${React.Children.count(children)}, 1fr)`}
+      {...additionalProps}
+    >
       {children}
     </TableRowStyledGrid>
   );
@@ -18,6 +25,11 @@ function TableRow({ children }) {
 
 TableRow.propTypes = {
   children: PropTypes.node,
+  columns: PropTypes.string,
+  onClick: PropTypes.func,
+  onKeyPress: PropTypes.func,
+  role: PropTypes.string,
+  tabIndex: PropTypes.string,
 };
 
 export default TableRow;
