e2ecce9b75d6cbd8736050103577bd0ae904e92a
Shruti Rao <shruti.rao@feisystems.com>
2018-02-20 16:33:12 -0500

    Add APIs to edit and GET healthcareService
    #17905:As a user, I can Edit a Health Care Service at organization level, so that is changed at organization level

--- app/containers/ManageHealthcareServicePage/api.js
+++ app/containers/ManageHealthcareServicePage/api.js
@@ -1,3 +1,5 @@
+import isEmpty from 'lodash/isEmpty';
+import find from 'lodash/find';
 import request from '../../utils/request';
 import getApiBaseUrl from '../../apiBaseUrlConfig';
 
@@ -11,8 +13,30 @@ export function createHealthcareService(healthcareServiceFormData, organizationI
     headers: {
       'Content-Type': 'application/json',
     },
-
   });
+}
+
+export function editHealthcareService(healthcareServiceFormData, organizationId) {
+  const url = `${apiBaseUrl}/organization/${organizationId}/healthcare-service/${healthcareServiceFormData.logicalId}`;
+  return request(url, {
+    method: 'PUT',
+    body: JSON.stringify(mapToBffHealthcareService(healthcareServiceFormData)),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
+export function getHealthcareServiceByIdFromStore(healthcareServices, logicalId) {
+  if (!isEmpty(healthcareServices)) {
+    return find(healthcareServices, { logicalId });
+  }
+  return null;
+}
+
+export function getHealthcareServiceById(logicalId) {
+  const requestURL = `${apiBaseUrl}/healthcare-service/${logicalId}`;
+  return request(requestURL);
 }
 
 function mapToBffHealthcareService(healthcareServiceFormData) {
