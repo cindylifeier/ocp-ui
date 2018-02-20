8c07e305863cb1bf457236923586b6bb156f4e35
burcakulug <burcak.ulug@feisystems.com>
2018-02-20 17:21:39 -0500

    Refactor OrganizationTable to utilize TableRow
    #17891:[Enabler] As a developer, I can use styled components to compose UI, so that the UI has consistent look and feel

--- app/components/OrganizationTable/index.js
+++ app/components/OrganizationTable/index.js
@@ -7,30 +7,80 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
 import isEmpty from 'lodash/isEmpty';
+
 import messages from './messages';
 import Table from '../Table';
 import TableHeader from '../TableHeader';
 import TableHeaderColumn from '../TableHeaderColumn';
-import OrganizationTableRow from '../OrganizationTableRow';
+import TableRow from '../TableRow';
+import TableRowColumn from '../TableRowColumn';
+import { ENTER_KEY } from '../../containers/App/constants';
+import StyledMenuItem from '../StyledMenuItem';
+import NavigationStyledIconMenu from '../StyledIconMenu/NavigationStyledIconMenu';
+
+const tableColumns = 'repeat(5, 1fr) 50px';
 
 function OrganizationTable({ organizations, onRowClick }) {
   return (
     <Table>
-      <TableHeader columns="repeat(5, 1fr) 50px">
+      <TableHeader columns={tableColumns}>
         <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderOrganization} /></TableHeaderColumn>
         <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderAddress} /></TableHeaderColumn>
         <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderTelephone} /></TableHeaderColumn>
         <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderId} /></TableHeaderColumn>
         <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderStatus} /></TableHeaderColumn>
       </TableHeader>
-      {!isEmpty(organizations) && organizations.map((org) => (
-        <OrganizationTableRow
-          key={org.id}
-          {...org}
-          onRowClick={onRowClick}
-        />
-      ))}
+      {!isEmpty(organizations) && organizations.map((org) => {
+        const { name, address, telephone, id, status } = org;
+        return (
+          <TableRow
+            columns={tableColumns}
+            key={org.id}
+            onClick={() => onRowClick && onRowClick(org)}
+            onKeyPress={(e) => {
+              if (e.key === ENTER_KEY) {
+                if (onRowClick) {
+                  onRowClick(org);
+                }
+              }
+              e.preventDefault();
+            }}
+            role="button"
+            tabIndex="0"
+          >
+            <TableRowColumn>{name}</TableRowColumn>
+            <TableRowColumn>{address}</TableRowColumn>
+            <TableRowColumn>{telephone}</TableRowColumn>
+            <TableRowColumn>{id}</TableRowColumn>
+            <TableRowColumn>{status}</TableRowColumn>
+            <TableRowColumn>
+              <NavigationStyledIconMenu>
+                <StyledMenuItem
+                  primaryText={<FormattedMessage {...messages.edit} />}
+                  containerElement={<Link to={`/ocp-ui/manage-organization/${id}`} />}
+                />
+                <StyledMenuItem
+                  primaryText={<FormattedMessage {...messages.addLocation} />}
+                  containerElement={<Link to={'/ocp-ui/manage-location'} />}
+                />
+                <StyledMenuItem
+                  primaryText={<FormattedMessage {...messages.addHealthCareService} />}
+                  containerElement={<Link to={'/ocp-ui/manage-health-care-service'} />}
+                />
+                <StyledMenuItem
+                  primaryText={<FormattedMessage {...messages.addActivityDefinition} />}
+                  containerElement={<Link to={'/ocp-ui/manage-activity-definition'} />}
+                />
+                <StyledMenuItem
+                  primaryText={<FormattedMessage {...messages.remove} />}
+                />
+              </NavigationStyledIconMenu>
+            </TableRowColumn>
+          </TableRow>
+        );
+      })}
     </Table>
   );
 }
--- app/components/OrganizationTable/messages.js
+++ app/components/OrganizationTable/messages.js
@@ -26,4 +26,24 @@ export default defineMessages({
     id: 'ocpui.components.OrganizationTable.TableColumnHeader.Status',
     defaultMessage: 'Status',
   },
+  edit: {
+    id: 'ocpui.components.OrganizationTable.edit',
+    defaultMessage: 'Edit',
+  },
+  addLocation: {
+    id: 'ocpui.components.OrganizationTable.addLocation',
+    defaultMessage: 'Add Location',
+  },
+  addHealthCareService: {
+    id: 'ocpui.components.OrganizationTable.addHealthCareService',
+    defaultMessage: 'Add Health Care Service',
+  },
+  addActivityDefinition: {
+    id: 'ocpui.components.OrganizationTable.addActivityDefinition',
+    defaultMessage: 'Add Activity Definition',
+  },
+  remove: {
+    id: 'ocpui.components.OrganizationTable.remove',
+    defaultMessage: 'Remove',
+  },
 });
