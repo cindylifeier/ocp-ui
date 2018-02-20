374ea3cb987ddbd146da1d25b6e3febf52926277
Shruti Rao <shruti.rao@feisystems.com>
2018-02-20 16:09:59 -0500

    Add PUT and GET related actions
    #17905:As a user, I can Edit a Health Care Service at organization level, so that is changed at organization level

--- app/containers/ManageHealthcareServicePage/actions.js
+++ app/containers/ManageHealthcareServicePage/actions.js
@@ -5,7 +5,9 @@
  */
 
 import {
-  POST_HEALTHCARE_SERVICE, POST_HEALTHCARE_SERVICE_ERROR, POST_HEALTHCARE_SERVICE_SUCCESS,
+  GET_HEALTHCARE_SERVICE, GET_HEALTHCARE_SERVICE_ERROR, GET_HEALTHCARE_SERVICE_SUCCESS,
+  POST_HEALTHCARE_SERVICE, POST_HEALTHCARE_SERVICE_ERROR, POST_HEALTHCARE_SERVICE_SUCCESS, PUT_HEALTHCARE_SERVICE,
+  PUT_HEALTHCARE_SERVICE_ERROR, PUT_HEALTHCARE_SERVICE_SUCCESS,
 } from './constants';
 
 
@@ -28,6 +30,51 @@ export function createHealthcareServiceError(error) {
 export function createHealthcareServiceSuccess(respone) {
   return {
     type: POST_HEALTHCARE_SERVICE_SUCCESS,
+    respone,
+  };
+}
+
+export function editHealthcareService(healthcareServiceFormData, handleSubmitting) {
+  return {
+    type: PUT_HEALTHCARE_SERVICE,
+    healthcareServiceFormData,
+    handleSubmitting,
+  };
+}
+
+export function editHealthcareServiceError(error) {
+  return {
+    type: PUT_HEALTHCARE_SERVICE_ERROR,
+    error,
+  };
+}
+
+
+export function editHealthcareServiceSuccess(respone) {
+  return {
+    type: PUT_HEALTHCARE_SERVICE_SUCCESS,
+    respone,
+  };
+}
+
+export function getHealthcareServiceById(logicalId) {
+  return {
+    type: GET_HEALTHCARE_SERVICE,
+    logicalId,
+  };
+}
+
+export function getHealthcareServiceByIdError(error) {
+  return {
+    type: GET_HEALTHCARE_SERVICE_ERROR,
+    error,
+  };
+}
+
+
+export function getHealthcareServiceByIdSuccess(respone) {
+  return {
+    type: GET_HEALTHCARE_SERVICE_SUCCESS,
     respone,
   };
 }
--- app/containers/ManageHealthcareServicePage/constants.js
+++ app/containers/ManageHealthcareServicePage/constants.js
@@ -12,3 +12,11 @@ export const POST_HEALTHCARE_SERVICE_SUCCESS = 'ocpui/ManageHealthcareServicePag
 export const POST_HEALTHCARE_SERVICE_ERROR = 'ocpui/ManageHealthcareServicePage/POST_HEALTHCARE_SERVICE_ERROR';
 export const POST_HEALTHCARE_SERVICE = 'ocpui/ManageHealthcareServicePage/POST_HEALTHCARE_SERVICE ';
 
+export const PUT_HEALTHCARE_SERVICE_SUCCESS = 'ocpui/ManageHealthcareServicePage/PUT_HEALTHCARE_SERVICE_SUCCESS';
+export const PUT_HEALTHCARE_SERVICE_ERROR = 'ocpui/ManageHealthcareServicePage/PUT_HEALTHCARE_SERVICE_ERROR';
+export const PUT_HEALTHCARE_SERVICE = 'ocpui/ManageHealthcareServicePage/PUT_HEALTHCARE_SERVICE ';
+
+export const GET_HEALTHCARE_SERVICE_SUCCESS = 'ocpui/ManageHealthcareServicePage/GET_HEALTHCARE_SERVICE_SUCCESS';
+export const GET_HEALTHCARE_SERVICE_ERROR = 'ocpui/ManageHealthcareServicePage/GET_HEALTHCARE_SERVICE_ERROR';
+export const GET_HEALTHCARE_SERVICE = 'ocpui/ManageHealthcareServicePage/GET_HEALTHCARE_SERVICE ';
+
