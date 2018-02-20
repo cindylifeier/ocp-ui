f2efc0aba307c11767548155088b5245f53c69f2
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-02-20 11:49:40 -0500

    Implement related person actions
    17550 - As a user, I would like to add related person to patient .

--- app/containers/ManageRelatedPersonPage/actions.js
+++ app/containers/ManageRelatedPersonPage/actions.js
@@ -5,11 +5,45 @@
  */
 
 import {
-  DEFAULT_ACTION,
+  POST_RELATED_PERSON, POST_RELATED_PERSON_ERROR, PUT_RELATED_PERSON,
+  PUT_RELATED_PERSON_ERROR,
+  PUT_RELATED_PERSON_SUCCESS,
 } from './constants';
 
-export function defaultAction() {
+export function createRelatedPerson(relatedPerson) {
   return {
-    type: DEFAULT_ACTION,
+    type: POST_RELATED_PERSON,
+    relatedPerson,
   };
 }
+
+export function createRelatedPersonError(error) {
+  return {
+    type: POST_RELATED_PERSON_ERROR,
+    error,
+  };
+}
+
+export function updateRelatedPerson(relatedPerson) {
+  return {
+    type: PUT_RELATED_PERSON,
+    relatedPerson,
+  };
+}
+
+
+export function updateRelatedPersonSuccess(response) {
+  return {
+    type: PUT_RELATED_PERSON_SUCCESS,
+    response,
+  };
+}
+
+
+export function updateRelatedPersonError(error) {
+  return {
+    type: PUT_RELATED_PERSON_ERROR,
+    error,
+  };
+}
+
--- app/containers/ManageRelatedPersonPage/constants.js
+++ app/containers/ManageRelatedPersonPage/constants.js
@@ -8,8 +8,15 @@
  *  ManageRelatedPersonPage action types
  *  @type {string}
  */
-export const DEFAULT_ACTION = 'ocpui/ManageRelatedPersonPage/DEFAULT_ACTION';
+export const POST_RELATED_PERSON = 'ocpui/ManageRelatedPerson/POST_RELATED_PERSON';
+export const POST_RELATED_PERSON_SUCCESS = 'ocpui/ManageRelatedPerson/POST_RELATED_PERSON_SUCCESS';
+export const POST_RELATED_PERSON_ERROR = 'ocpui/ManageRelatedPerson/POST_RELATED_PERSON_ERROR';
 
+export const PUT_RELATED_PERSON = 'ocpui/ManageRelatedPerson/PUT_RELATED_PERSON';
+export const PUT_RELATED_PERSON_SUCCESS = 'ocpui/ManageRelatedPerson/PUT_RELATED_PERSON_SUCCESS';
+export const PUT_RELATED_PERSON_ERROR = 'ocpui/ManageRelatedPerson/PUT_RELATED_PERSON_ERROR';
+
+export const SAVE_RELATED_PERSON_ERROR = 'ocpui/ManageRelatedPerson/SAVE_RELATED_PERSON_ERROR';
 
 /**
 *  Other constants
