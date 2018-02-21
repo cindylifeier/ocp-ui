f78d559ee96ab234ece8bdf8f9f8e6a296bf593d
CindyLiFei <cindy.li@feisystems.com>
2018-02-21 15:24:45 -0500

    Implement task list after select patient
    #17902 : As a user I will see the list of all tasks for a patient

--- app/containers/Tasks/messages.js
+++ app/containers/Tasks/messages.js
@@ -8,6 +8,14 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   header: {
     id: 'ocpui.containers.Tasks.header',
-    defaultMessage: 'This is Tasks container !',
+    defaultMessage: 'Tasks',
+  },
+  patientNotSelected: {
+    id: 'ocpui.containers.Tasks.patientNotSelected',
+    defaultMessage: 'No tasks loaded. Please select a patient to view his/her tasks.',
+  },
+  noTasksFound: {
+    id: 'ocpui.containers.Tasks.noTasksFound',
+    defaultMessage: 'No tasks found.',
   },
 });
