dee3184d65f40fe99040cb5d6e235173215fafbf
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-02-21 08:31:12 -0500

    Start implementing initalize related persons component and getting the list of related person.
    17553 - As a user, I can list all of the related persons to a patient.

--- app/containers/RelatedPersons/index.js
+++ app/containers/RelatedPersons/index.js
@@ -17,19 +17,36 @@ import makeSelectRelatedPersons from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
+import { getRelatedPersons, initializeRelatedPersons } from './actions';
+import RelatedPersonTable from '../../components/RelatedPersonTable';
 
 export class RelatedPersons extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.handlePageClick = this.handlePageClick.bind(this);
+  }
+  componentDidMount() {
+    this.props.initializeRelatedPersons();
+  }
+
+  handlePageClick() {
+    // const { query, patientName, statusList } = this.props.relatedPersons;
+    // this.props.getCareTeams({ ...query, pageNumber: page }, patientName, statusList);
+  }
+
   render() {
     return (
       <div>
         <FormattedMessage {...messages.header} />
+        <RelatedPersonTable></RelatedPersonTable>
       </div>
     );
   }
 }
 
 RelatedPersons.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  // dispatch: PropTypes.func.isRequired,
+  initializeRelatedPersons: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
@@ -38,7 +55,8 @@ const mapStateToProps = createStructuredSelector({
 
 function mapDispatchToProps(dispatch) {
   return {
-    dispatch,
+    getRelatedPersons: (query, patientName, statusList) => dispatch(getRelatedPersons(query, patientName, statusList)),
+    initializeRelatedPersons: () => dispatch(initializeRelatedPersons()),
   };
 }
 
