3dd67ff86de42afdd724cf1d4f04e6dc21c33d7f
burcakulug <burcak.ulug@feisystems.com>
2018-02-22 11:10:26 -0500

    Utilize Table components in PatientSearchResult and remove css file
    #17891:[Enabler] As a developer, I can use styled components to compose UI, so that the UI has consistent look and feel

--- app/components/PatientSearchResult/messages.js
+++ app/components/PatientSearchResult/messages.js
@@ -11,7 +11,51 @@ export default defineMessages({
     defaultMessage: 'This is the PatientSearchResult component !',
   },
   addTask: {
-    id: 'ocpui.components.OrganizationsTableRow.addTask',
+    id: 'ocpui.components.PatientSearchResult.addTask',
     defaultMessage: 'Add Task',
+  },
+  addCareTeam: {
+    id: 'ocpui.components.PatientSearchResult.addCareTeam',
+    defaultMessage: 'Add Care Team',
+  },
+  edit: {
+    id: 'ocpui.components.PatientSearchResult.edit',
+    defaultMessage: 'Edit',
+  },
+  remove: {
+    id: 'ocpui.components.PatientSearchResult.remove',
+    defaultMessage: 'Remove',
+  },
+  active: {
+    id: 'ocpui.components.PatientSearchResult.active',
+    defaultMessage: 'Active',
+  },
+  inactive: {
+    id: 'ocpui.components.PatientSearchResult.inactive',
+    defaultMessage: 'Inactive',
+  },
+  firstName: {
+    id: 'ocpui.components.PatientSearchResult.firstName',
+    defaultMessage: 'First Name',
+  },
+  lastName: {
+    id: 'ocpui.components.PatientSearchResult.lastName',
+    defaultMessage: 'Last Name',
+  },
+  dob: {
+    id: 'ocpui.components.PatientSearchResult.dob',
+    defaultMessage: 'DOB',
+  },
+  gender: {
+    id: 'ocpui.components.PatientSearchResult.gender',
+    defaultMessage: 'Gender',
+  },
+  identifier: {
+    id: 'ocpui.components.PatientSearchResult.identifier',
+    defaultMessage: 'Identifier',
+  },
+  status: {
+    id: 'ocpui.components.PatientSearchResult.status',
+    defaultMessage: 'Status',
   },
 });
