4d04e12d8c9a5d8f5f466f64a738c89f21c15f75
burcakulug <burcak.ulug@feisystems.com>
2018-02-23 16:46:03 -0500

    Create AddArtifactForm component to use in ManageActivityDefinitionForm
    #17899:As a user, I can create activity definition at organization level

---
+++ app/components/AddArtifactForm/index.js
@@ -0,0 +1,78 @@
+/**
+ *
+ * AddArtifactForm
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { FormattedMessage } from 'react-intl';
+import { Form, Formik } from 'formik';
+import Yup from 'yup';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+
+import messages from './messages';
+import TextField from '../TextField';
+import SelectField from '../SelectField';
+
+const FormTitle = styled.div`
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #444;
+  background-color: #f2f2f2;
+`;
+
+function AddArtifactForm({ onAddArtifact, relatedArtifactTypes, callback }) {
+  return (
+    <div>
+      <FormTitle><FormattedMessage {...messages.header} /></FormTitle>
+      <Formik
+        onSubmit={(values) => {
+          onAddArtifact(values);
+          if (callback) {
+            callback();
+          }
+        }}
+        validationSchema={Yup.object().shape({
+          display: Yup.string().required('Required'),
+          type: Yup.string().required('Required'),
+        })}
+        render={({ isSubmitting, dirty, isValid }) => (
+          <Form>
+            <TextField name="display" floatingLabelText={<FormattedMessage {...messages.displayLabel} />} fullWidth />
+            <SelectField name="type" floatingLabelText={<FormattedMessage {...messages.displayLabel} />} fullWidth>
+              {relatedArtifactTypes.map(({ code, system, display }) => (
+                <MenuItem key={`${system}|${code}`} value={code} primaryText={display} />
+              ))}
+            </SelectField>
+            <RaisedButton
+              type="submit"
+              label={<FormattedMessage {...messages.saveButtonLabel} />}
+              disabled={!dirty || isSubmitting || !isValid}
+            />
+            <FlatButton
+              type="reset"
+              label={<FormattedMessage {...messages.cancelButtonLabel} />}
+              onClick={callback}
+            />
+          </Form>
+        )}
+      />
+    </div>
+  );
+}
+
+AddArtifactForm.propTypes = {
+  onAddArtifact: PropTypes.func.isRequired,
+  relatedArtifactTypes: PropTypes.array,
+  callback: PropTypes.func,
+};
+
+AddArtifactForm.defaultProps = {
+  relatedArtifactTypes: [],
+};
+
+export default AddArtifactForm;
