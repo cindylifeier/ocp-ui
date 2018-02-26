e80eb721f4aef99a1b88fe1d96138cc3c858f012
JiahaoLi <Jiahao.Li@feisystems.com>
2018-02-26 15:24:19 -0500

    Implement to add unit test for token service
    17894: As a user, I can login OCP and get authorized, so that I can access OCP resources

---
+++ app/utils/tests/tokenService.test.js
@@ -0,0 +1,96 @@
+import 'mock-local-storage';
+
+import {
+  isTokenExpired,
+  removeToken,
+  retrieveAuthStatus,
+  retrieveToken,
+  storeAuthStatus,
+  storeToken,
+} from '../tokenService';
+
+describe('tokenService.js', () => {
+  afterEach(() => {
+    sessionStorage.clear();
+    // remove callback
+    sessionStorage.itemInsertionCallback = null;
+  });
+  it('should return with correct stored token', () => {
+    // Arrange
+    const testToken = {
+      access_token: 'access_token',
+      token_type: 'token_type',
+      expires_in: 'expires_in',
+      scope: 'scope',
+      jti: 'jti',
+    };
+    storeToken(testToken);
+
+    // Act
+    const storedToken = retrieveToken();
+
+    // Assert
+    expect(storedToken).toEqual(testToken);
+  });
+
+  it('should return with correct stored auth status', () => {
+    // Arrange
+    const testAuthStatus = false;
+    storeAuthStatus(testAuthStatus);
+
+    // Act
+    const storedAuthStatus = retrieveAuthStatus();
+
+    // Assert
+    expect(storedAuthStatus).toEqual(testAuthStatus);
+  });
+
+  it('should clean secured data in session storage when call removeToken', () => {
+    // Arrange
+    const testAuthStatus = false;
+    const testToken = {
+      access_token: 'access_token',
+      token_type: 'token_type',
+      expires_in: 'expires_in',
+      scope: 'scope',
+      jti: 'jti',
+    };
+    storeAuthStatus(testAuthStatus);
+    storeToken(testToken);
+
+    // Act
+    removeToken();
+
+    // Assert
+    expect(retrieveToken()).toBeNull();
+    expect(retrieveAuthStatus()).toBeNull();
+  });
+
+  it('should return with correct token expired status', () => {
+    // Arrange
+    const testExpiredToken = {
+      access_token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImxlZ2FjeS10b2tlbi1rZXkiLCJ0eXAiOiJKV1QifQ.eyJleHAiOjE1MTk2NzU0NjF9.K5nG1g0yxDIDsVUkoqlgbAe1YJZZ5psFV1YeAOZw_-A',
+    };
+
+    storeToken(testExpiredToken);
+    const expiredToken = retrieveToken();
+
+    // Act
+    const isExpired = isTokenExpired(expiredToken);
+
+    // Assert
+    expect(isExpired).toBeTruthy();
+  });
+
+  it('should return expired status when there is an empty token', () => {
+    // Arrange
+    storeToken(null);
+    const expiredToken = retrieveToken();
+
+    // Act
+    const isExpired = isTokenExpired(expiredToken);
+
+    // Assert
+    expect(isExpired).toBeTruthy();
+  });
+});
