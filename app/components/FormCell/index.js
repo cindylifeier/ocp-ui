611916440f2939cfaf5cddd354969e1dfcea3994
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-02 16:08:17 -0500

    Implement generic form cell and form grid.
    18345 - As a user I should be able to create and edit communications with patient so I can check my notes later - Functionality Implementation

---
+++ app/components/FormCell/index.js
@@ -0,0 +1,16 @@
+/**
+*
+* ManageFormCell
+*
+*/
+
+import { Cell } from 'styled-css-grid';
+
+const FormCell = Cell.extend`
+  border-radius: 5px;
+  font-size: 100%;
+`;
+
+FormCell.propTypes = {};
+
+export default FormCell;
