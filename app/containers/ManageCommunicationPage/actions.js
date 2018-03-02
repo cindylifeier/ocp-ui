60632e44f8b9e96fcbec118cc6952dc9c7e56ee6
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-02 17:09:10 -0500

    Implement create and edit communication action
    18345 - As a user I should be able to create and edit communications with patient so I can check my notes later - Functionality Implementation

--- app/containers/ManageCommunicationPage/actions.js
+++ app/containers/ManageCommunicationPage/actions.js
@@ -5,11 +5,45 @@
  */
 
 import {
-  DEFAULT_ACTION,
+  CREATE_COMMUNICATION, CREATE_COMMUNICATION_SUCCESS, SAVE_COMMUNICATION_ERROR,
+  UPDATE_COMMUNICATION, UPDATE_COMMUNICATION_SUCCESS,
 } from './constants';
 
-export function defaultAction() {
+export function createCommunication(communication, patientId, handleSubmitting) {
   return {
-    type: DEFAULT_ACTION,
+    type: CREATE_COMMUNICATION,
+    communication,
+    patientId,
+    handleSubmitting,
+  };
+}
+
+export function createCommunicationSuccess(response) {
+  return {
+    type: CREATE_COMMUNICATION_SUCCESS,
+    response,
+  };
+}
+
+export function saveCommunicationError(error) {
+  return {
+    type: SAVE_COMMUNICATION_ERROR,
+    error,
+  };
+}
+
+export function updateCommunication(communication, patientId, handleSubmitting) {
+  return {
+    type: UPDATE_COMMUNICATION,
+    communication,
+    patientId,
+    handleSubmitting,
+  };
+}
+
+export function updateCommunicationSuccess(response) {
+  return {
+    type: UPDATE_COMMUNICATION_SUCCESS,
+    response,
   };
 }
