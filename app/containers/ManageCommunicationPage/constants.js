cafd7fca7a7306b8b3ea1f8a923fb3eb5d53aa80
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-02 17:10:53 -0500

    Add constants for create and edit communication
    18345 - As a user I should be able to create and edit communications with patient so I can check my notes later - Functionality Implementation

--- app/containers/ManageCommunicationPage/constants.js
+++ app/containers/ManageCommunicationPage/constants.js
@@ -8,9 +8,13 @@
  *  ManageCommunicationPage action types
  *  @type {string}
  */
-export const DEFAULT_ACTION = 'ocpui/ManageCommunicationPage/DEFAULT_ACTION';
 
+export const CREATE_COMMUNICATION = 'ocpui/ManageCommunicationPage/CREATE_COMMUNICATION';
+export const CREATE_COMMUNICATION_SUCCESS = 'ocpui/ManageCommunicationPage/CREATE_COMMUNICATION_SUCCESS';
+export const SAVE_COMMUNICATION_ERROR = 'ocpui/ManageCommunicationPage/SAVE_COMMUNICATION_ERROR';
 
+export const UPDATE_COMMUNICATION = 'ocpui/ManageCommunicationPage/UPDATE_COMMUNICATION';
+export const UPDATE_COMMUNICATION_SUCCESS = 'ocpui/ManageCommunicationPage/UPDATE_COMMUNICATION_SUCCESS';
 /**
 *  Other constants
 *
