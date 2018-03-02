a114e109559acd23d3949376ffca9169f4ddcbeb
burcakulug <burcak.ulug@feisystems.com>
2018-03-02 09:45:21 -0500

    Utilize InfoSection, InlineLabel and NoResultsFoundText in CareTeams
    #18130:[Enabler] As a developer, I should only use the styled component so that there is a unified styling approach in OCP

--- app/containers/CareTeams/index.js
+++ app/containers/CareTeams/index.js
@@ -16,26 +16,25 @@ import { Cell, Grid } from 'styled-css-grid';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
+import InfoSection from 'components/InfoSection';
+import InlineLabel from 'components/InlineLabel';
+import RefreshIndicatorLoading from 'components/RefreshIndicatorLoading';
+import CareTeamTable from 'components/CareTeamTable';
+import Card from 'components/Card';
+import CardHeader from 'components/CardHeader';
+import CenterAlign from 'components/Align/CenterAlign';
+import CenterAlignedUltimatePagination from 'components/CenterAlignedUltimatePagination';
+import NoResultsFoundText from 'components/NoResultsFoundText';
+import { makeSelectCareTeamStatuses } from 'containers/App/lookupSelectors';
+import { getLookupsAction } from 'containers/App/actions';
+import { CARETEAMSTATUS, DEFAULT_START_PAGE_NUMBER } from 'containers/App/constants';
 import makeSelectCareTeams from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
-import RefreshIndicatorLoading from '../../components/RefreshIndicatorLoading';
-import CareTeamTable from '../../components/CareTeamTable';
 import { getCareTeams, initializeCareTeams } from './actions';
-import { makeSelectCareTeamStatuses } from '../App/lookupSelectors';
 import { DEFAULT_CARE_TEAM_STATUS_CODE } from './constants';
-import { getLookupsAction } from '../App/actions';
-import { CARETEAMSTATUS, DEFAULT_START_PAGE_NUMBER } from '../App/constants';
-import Card from '../../components/Card';
-import CardHeader from '../../components/CardHeader';
-import CenterAlign from '../../components/Align/CenterAlign';
-import FilterSection from './FilterSection';
 import CheckboxGrid from './CheckboxGrid';
-import NoCareTeamSection from './NoCareTeamSection';
-import PatientInfoSection from './PatientInfoSection';
-import PatientLabel from './PatientLabel';
-import CenterAlignedUltimatePagination from '../../components/CenterAlignedUltimatePagination';
 
 export class CareTeams extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -68,7 +67,7 @@ export class CareTeams extends React.PureComponent { // eslint-disable-line reac
   renderFilter(careTeamStatuses, statusList) {
     const filteredCareTeamStatuses = careTeamStatuses.filter(({ code }) => DEFAULT_CARE_TEAM_STATUS_CODE !== code);
     return (
-      <FilterSection>
+      <InfoSection>
         <CheckboxGrid columns={this.calculateCheckboxColumns(filteredCareTeamStatuses)} gap="">
           <Cell><CenterAlign>Include</CenterAlign></Cell>
           {filteredCareTeamStatuses.map(({ code, display }) => (
@@ -85,7 +84,7 @@ export class CareTeams extends React.PureComponent { // eslint-disable-line reac
           ))
           }
         </CheckboxGrid>
-      </FilterSection>);
+      </InfoSection>);
   }
 
   render() {
@@ -97,12 +96,12 @@ export class CareTeams extends React.PureComponent { // eslint-disable-line reac
           <h4><FormattedMessage {...messages.patientNotSelected} /></h4> :
           <Grid columns={1} gap="">
             <Cell>
-              <PatientInfoSection>
-                <PatientLabel>
+              <InfoSection>
+                <InlineLabel>
                   <FormattedMessage {...messages.patientLabel} />&nbsp;:&nbsp;
-                </PatientLabel>
+                </InlineLabel>
                 {patientName}
-              </PatientInfoSection>
+              </InfoSection>
             </Cell>
             <Cell>
               {!isEmpty(careTeamStatuses) &&
@@ -115,8 +114,8 @@ export class CareTeams extends React.PureComponent { // eslint-disable-line reac
         {loading &&
         <RefreshIndicatorLoading />}
 
-        {!loading && !isEmpty(patientName) && isEmpty(data) &&
-        <NoCareTeamSection>No care teams found.</NoCareTeamSection>}
+        {!loading && !isEmpty(patientName) && (isEmpty(data) || isEmpty(data.elements)) &&
+        <NoResultsFoundText>No care teams found.</NoResultsFoundText>}
 
         {!isEmpty(data) && !isEmpty(data.elements) &&
         <CenterAlign>
