54cdbf006f91687a9943f35229a2ed58812c6202
Teng Luo <Teng.Luo@feisystems.com>
2018-03-05 13:24:25 -0500

    Create messages.js for CareCoordinator upcoming appointment
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

---
+++ app/containers/CareCoordinatorUpcomingAppointment/messages.js
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'ocpui.containers.CareCoordinatorUpcomingAppointment.header',
+    defaultMessage: 'Upcoming Appointments ',
+  },
+});
