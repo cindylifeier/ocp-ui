3e215e4b0a47341be8859aac0bfe32f35b73cc2d
Teng Luo <Teng.Luo@feisystems.com>
2018-03-05 14:49:14 -0500

    Create NoUpcomingAppointment styled component
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

---
+++ app/containers/CareCoordinatorUpcomingAppointment/NoUpcomingAppointment.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const NoUpcomingAppointment = styled.div`
+  margin: 20px 0;
+  font-weight: bold;
+`;
+
+NoUpcomingAppointment.propTypes = {};
+
+export default NoUpcomingAppointment;
