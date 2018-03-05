f70083d341bc1a3af0786f8fe14ca864da214d6f
Teng Luo <Teng.Luo@feisystems.com>
2018-03-05 12:16:13 -0500

    Create render for CareCoordinator upcoming appointment
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

---
+++ app/containers/CareCoordinatorUpcomingAppointment/render.js
@@ -0,0 +1,4 @@
+import renderFactory from 'utils/goldenLayout/renderFactory';
+import CareCoordinatorUpcomingAppointment from './index';
+
+export default renderFactory(CareCoordinatorUpcomingAppointment);
