d5740c9cb7ec8cc953abba8a9dc67e8d73bbd20b
Teng Luo <Teng.Luo@feisystems.com>
2018-03-05 14:44:25 -0500

    Create Care Coordinator upcoming appointment table component
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

---
+++ app/components/CareCoordinatorUpcomingAppointmentTable/index.js
@@ -0,0 +1,24 @@
+/**
+*
+* CareCoordinatorUpcomingAppointmentTable
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+class CareCoordinatorUpcomingAppointmentTable extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+CareCoordinatorUpcomingAppointmentTable.propTypes = {
+
+};
+
+export default CareCoordinatorUpcomingAppointmentTable;
---
+++ app/components/CareCoordinatorUpcomingAppointmentTable/tests/index.test.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import { configure, shallow } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-15';
+// import 'jest-styled-components';
+
+// import CareCoordinatorUpcomingAppointmentTable from '../index';
+
+// configure({ adapter: new Adapter() });
+
+describe('<CareCoordinatorUpcomingAppointmentTable />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
