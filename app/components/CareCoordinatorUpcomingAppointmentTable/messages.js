594e40790b9369b030cb0a9fe245b1ae1bede273
Teng Luo <Teng.Luo@feisystems.com>
2018-03-05 15:26:52 -0500

    Create messages.js for care coordinator upcoming appointments table component
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

---
+++ app/components/CareCoordinatorUpcomingAppointmentTable/messages.js
@@ -0,0 +1,37 @@
+/*
+ * CareTeamTable Messages
+ *
+ * This contains all the text for the CareTeamTable component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  columnHeaderDescription: {
+    id: 'ocpui.components.CareCoordinatorUpcomingAppointmentTable.columnHeaderDescription',
+    defaultMessage: 'Description',
+  },
+  columnHeaderAppointmentType: {
+    id: 'ocpui.components.CareCoordinatorUpcomingAppointmentTable.columnHeaderAppointmentType',
+    defaultMessage: 'Appointment Type',
+  },
+  columnHeaderStatus: {
+    id: 'ocpui.components.CareCoordinatorUpcomingAppointmentTable.columnHeaderStatus',
+    defaultMessage: 'Status',
+  },
+  columnHeaderStart: {
+    id: 'ocpui.components.CareCoordinatorUpcomingAppointmentTable.columnHeaderStart',
+    defaultMessage: 'Start',
+  },
+  columnHeaderEnd: {
+    id: 'ocpui.components.CareCoordinatorUpcomingAppointmentTable.columnHeaderEnd',
+    defaultMessage: 'End',
+  },
+  columnHeaderActor: {
+    id: 'ocpui.components.CareCoordinatorUpcomingAppointmentTable.columnHeaderActor',
+    defaultMessage: 'Actor',
+  },
+  columnHeaderAction: {
+    id: 'ocpui.components.CareCoordinatorUpcomingAppointmentTable.columnHeaderAction',
+    defaultMessage: 'Action',
+  },
+});
