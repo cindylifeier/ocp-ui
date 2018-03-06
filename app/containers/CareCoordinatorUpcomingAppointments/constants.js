863666b49fd9c00bcc70f7fa0fd0c2c5d26a060b
Teng Luo <Teng.Luo@feisystems.com>
2018-03-06 16:46:58 -0500

    Refactor constant name from Upcoming appointment to upcoming appointments
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

---
+++ app/containers/CareCoordinatorUpcomingAppointments/constants.js
@@ -0,0 +1,22 @@
+/*
+ *
+ * CareCoordinatorUpcomingAppointment constants
+ *
+ */
+
+/**
+ *  CareCoordinatorUpcomingAppointments action types
+ *  @type {string}
+ */
+export const DEFAULT_ACTION = 'ocpui/CareCoordinatorUpcomingAppointments/DEFAULT_ACTION';
+export const INITIALIZE_CARE_COORDINATOR_UPCOMING_APPOINTMENTS = 'ocpui/CareCoordinatorUpcomingAppointments/INITIALIZE_CARE_COORDINATOR_UPCOMING_APPOINTMENTS';
+export const GET_CARE_COORDINATOR_UPCOMING_APPOINTMENTS = 'ocpui/CareCoordinatorUpcomingAppointments/GET_CARE_COORDINATOR_UPCOMING_APPOINTMENTS';
+export const GET_CARE_COORDINATOR_UPCOMING_APPOINTMENTS_SUCCESS = 'ocpui/CareCoordinatorUpcomingAppointments/GET_CARE_COORDINATOR_UPCOMING_APPOINTMENTS_SUCCESS';
+export const GET_CARE_COORDINATOR_UPCOMING_APPOINTMENTS_ERROR = 'ocpui/CareCoordinatorUpcomingAppointments/GET_CARE_COORDINATOR_UPCOMING_APPOINTMENTS_ERROR';
+
+
+/**
+ *  Other constants
+ *
+ */
+export const PAGE_SIZE = 10;
