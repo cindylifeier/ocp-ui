866a47d866f962a2feae0931037b73d71fa2c164
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-06 09:24:45 -0500

    Implement get recipient related constants and actions
    18345 - As a user I should be able to create and edit communications with patient so I can check my notes later - Functionality Implementation

--- app/containers/SearchRecipient/actions.js
+++ app/containers/SearchRecipient/actions.js
@@ -4,12 +4,44 @@
  *
  */
 
-import {
-  DEFAULT_ACTION,
-} from './constants';
 
-export function defaultAction() {
+import {
+  ADD_RECIPIENT, GET_RECIPIENTS, GET_RECIPIENTS_ERROR,
+  GET_RECIPIENTS_SUCCESS, INITIALIZE_SEARCH_RECIPIENT_RESULT,
+} from 'containers/SearchRecipient/constants';
+
+export function getRecipients(patientId) {
   return {
-    type: DEFAULT_ACTION,
+    type: GET_RECIPIENTS,
+    patientId,
+  };
+}
+
+export function getRecipientsSuccess(recipients) {
+  return {
+    type: GET_RECIPIENTS_SUCCESS,
+    recipients,
+  };
+}
+
+
+export function getRecipientsError(error) {
+  return {
+    type: GET_RECIPIENTS_ERROR,
+    error,
+  };
+}
+
+export function addSelectedRecipients(selectedRecipients) {
+  return {
+    type: ADD_RECIPIENT,
+    selectedRecipients,
+  };
+}
+
+export function initializeSearchRecipients(initialSelectedRecipients) {
+  return {
+    type: INITIALIZE_SEARCH_RECIPIENT_RESULT,
+    initialSelectedRecipients,
   };
 }
--- app/containers/SearchRecipient/constants.js
+++ app/containers/SearchRecipient/constants.js
@@ -8,10 +8,18 @@
  *  SearchRecipient action types
  *  @type {string}
  */
-export const DEFAULT_ACTION = 'ocpui/SearchRecipient/DEFAULT_ACTION';
+export const GET_RECIPIENTS = 'ocpui/SearchRecipient/GET_RECIPIENTS';
+export const GET_RECIPIENTS_SUCCESS = 'ocpui/SearchRecipient/GET_RECIPIENTS_SUCCESS';
+export const GET_RECIPIENTS_ERROR = 'ocpui/SearchRecipient/GET_RECIPIENTS_ERROR';
 
+export const ADD_RECIPIENT = 'ocpui/SearchRecipient/ADD_RECIPIENT';
+export const INITIALIZE_SEARCH_RECIPIENT_RESULT = 'ocpui/SearchRecipient/INITIALIZE_SEARCH_RECIPIENT_RESULT';
 
 /**
 *  Other constants
 *
 */
+
+export const fieldStyle = { width: '15vw', padding: '2px' };
+export const floatingLabelStyle = { fontFamily: 'Roboto, sans-serif' };
+export const iconButtonStyle = { top: '26px', height: '30px' };
