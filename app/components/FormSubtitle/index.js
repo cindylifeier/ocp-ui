205129217457569266126239d90a43f75f1beb41
burcakulug <burcak.ulug@feisystems.com>
2018-03-06 12:02:41 -0500

    Move FormSubtitle margin default value to defaultProps
    #18130:[Enabler] As a developer, I should only use the styled component so that there is a unified styling approach in OCP

--- app/components/FormSubtitle/index.js
+++ app/components/FormSubtitle/index.js
@@ -9,7 +9,7 @@ import PropTypes from 'prop-types';
 
 const FormSubtitle = styled.div`
   padding-left: 0.5vw;
-  margin: ${(props) => props.margin ? props.margin : '2vh 1vw 0 1vw'};
+  margin: ${({ margin }) => margin};
   font-size: 1.2rem;
   font-weight: bold;
   color: #444;
@@ -18,6 +18,10 @@ const FormSubtitle = styled.div`
 
 FormSubtitle.propTypes = {
   margin: PropTypes.string,
+};
+
+FormSubtitle.defaultProps = {
+  margin: '2vh 1vw 0 1vw',
 };
 
 export default FormSubtitle;
