1978bfb03563e8959fe51f07b76c794eff7514a7
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-06 09:26:39 -0500

    Start implementing saga and reducer for getting recipients
    18345 - As a user I should be able to create and edit communications with patient so I can check my notes later - Functionality Implementation

--- app/containers/SearchRecipient/reducer.js
+++ app/containers/SearchRecipient/reducer.js
@@ -6,14 +6,30 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
-} from './constants';
+  GET_RECIPIENTS_ERROR, GET_RECIPIENTS_SUCCESS,
+  INITIALIZE_SEARCH_RECIPIENT_RESULT,
+} from 'containers/SearchRecipient/constants';
+import { INITIALIZE_SEARCH_RECIPIENT } from 'containers/ManageCommunicationPage/constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  recipients: [],
+  selectedRecipients: [],
+});
 
 function searchRecipientReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
+    case GET_RECIPIENTS_SUCCESS:
+      return state
+        // .set('recipients', fromJS((action.recipients && action.recipients.elements) || []));
+        .set('recipients', fromJS((action.recipients) || []));
+    case INITIALIZE_SEARCH_RECIPIENT_RESULT:
+      return state
+        .set('recipients', fromJS([]));
+    case INITIALIZE_SEARCH_RECIPIENT:
+      return state
+        .set('selectedRecipients', fromJS((action.initialSelectedRecipients) || []))
+        .set('recipients', fromJS([]));
+    case GET_RECIPIENTS_ERROR:
       return state;
     default:
       return state;
--- app/containers/SearchRecipient/saga.js
+++ app/containers/SearchRecipient/saga.js
@@ -1,6 +1,33 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import isEmpty from 'lodash/isEmpty';
+import { put, takeLatest, all } from 'redux-saga/effects';
+import { getRecipientsError, getRecipientsSuccess } from 'containers/SearchRecipient/actions';
+import { GET_RECIPIENTS } from 'containers/SearchRecipient/constants';
+import { showNotification } from '../Notification/actions';
 
-// Individual exports for testing
-export default function* defaultSaga() {
-  // See example in containers/HomePage/saga.js
+export function* getRecipientsSaga(action) {
+  try {
+    if (!isEmpty(action.patientId)) {
+      // const recipients = yield call(getRecipients, action.patientId);
+      const recipients = [
+        { logicalId: '01', name: 'Test1', role: 'Role1' },
+        { logicalId: '02', name: 'Test2', role: 'Role2' },
+        { logicalId: '03', name: 'Test3', role: 'Role3' },
+        { logicalId: '04', name: 'Test4', role: 'Role4' },
+      ];
+      yield put(getRecipientsSuccess(recipients));
+    }
+  } catch (error) {
+    yield put(showNotification('No participant found'));
+    yield put(getRecipientsError(error));
+  }
+}
+
+export function* watchGetRecipients() {
+  yield takeLatest(GET_RECIPIENTS, getRecipientsSaga);
+}
+
+export default function* rootSaga() {
+  yield all([
+    watchGetRecipients(),
+  ]);
 }
