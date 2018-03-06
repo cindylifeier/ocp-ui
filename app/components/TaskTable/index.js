65c780dc9d585ab8a384dfaa8671d3b8739b7c0c
burcakulug <burcak.ulug@feisystems.com>
2018-03-06 09:27:22 -0500

    Utilize NavigationStyledIconMenu and StyledMenuItem in TaskTable
    #18130:[Enabler] As a developer, I should only use the styled component so that there is a unified styling approach in OCP

--- app/components/TaskTable/index.js
+++ app/components/TaskTable/index.js
@@ -8,85 +8,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import isEmpty from 'lodash/isEmpty';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import IconButton from 'material-ui/IconButton';
-import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 
 import Table from 'components/Table';
 import TableHeader from 'components/TableHeader';
 import TableHeaderColumn from 'components/TableHeaderColumn';
 import TableRow from 'components/TableRow';
 import TableRowColumn from 'components/TableRowColumn';
+import NavigationStyledIconMenu from 'components/StyledIconMenu/NavigationStyledIconMenu';
+import StyledMenuItem from 'components/StyledMenuItem';
 import messages from './messages';
-import styles from './styles.css';
-
-const iconStyles = {
-  iconButton: {
-    position: 'relative',
-  },
-  icon: {
-    width: '100%',
-    height: 26,
-    position: 'absolute',
-    top: '0',
-    right: '0',
-  },
-};
+import { TASK_TABLE_COLUMNS } from './constants';
 
 function TaskTable({ elements, cancelTask }) {
   return (
-    <div>
-      <Table>
-        <TableHeader>
-          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderActivityType} /></TableHeaderColumn>
-          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderStatus} /></TableHeaderColumn>
-          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderPriority} /></TableHeaderColumn>
-          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderCreatedOn} /></TableHeaderColumn>
-          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderTaskPeriod} /></TableHeaderColumn>
-          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderCreatedBy} /></TableHeaderColumn>
-          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderTaskOwner} /></TableHeaderColumn>
-          <TableHeaderColumn />
-        </TableHeader>
-        {!isEmpty(elements) && elements.map(({ logicalId, definition, status, priority, authoredOn, executionPeriod, agent, owner }) => (
-          <TableRow key={logicalId}>
-            <TableRowColumn>{definition && definition.display}</TableRowColumn>
-            <TableRowColumn>{status && status.display}</TableRowColumn>
-            <TableRowColumn>{priority && priority.display}</TableRowColumn>
-            <TableRowColumn>{authoredOn}</TableRowColumn>
-            <TableRowColumn>{executionPeriod && executionPeriod.start} - {executionPeriod && executionPeriod.end} </TableRowColumn>
-            <TableRowColumn>{agent && agent.display} </TableRowColumn>
-            <TableRowColumn>{owner && owner.display} </TableRowColumn>
-            <TableRowColumn>
-              <div className={styles.iconButtonGridContainer}>
-                <div className={styles.iconButtonGridItem}>
-                  <IconMenu
-                    iconButtonElement={
-                      (<IconButton
-                        className={styles.iconButton}
-                        iconStyle={iconStyles.icon}
-                        style={iconStyles.iconButton}
-                      >
-                        <NavigationMenu />
-                      </IconButton>)
-                    }
-                    anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-                    targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-                  >
-                    <MenuItem
-                      className={styles.menuItem}
-                      primaryText="Cancel"
-                      disabled={status.code === 'cancelled'}
-                      onClick={() => cancelTask(logicalId)}
-                    />
-                  </IconMenu>
-                </div>
-              </div>
-            </TableRowColumn>
-          </TableRow>
-        ))}
-      </Table>
-    </div>
+    <Table>
+      <TableHeader columns={TASK_TABLE_COLUMNS}>
+        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderActivityType} /></TableHeaderColumn>
+        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderStatus} /></TableHeaderColumn>
+        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderPriority} /></TableHeaderColumn>
+        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderCreatedOn} /></TableHeaderColumn>
+        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderTaskPeriod} /></TableHeaderColumn>
+        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderCreatedBy} /></TableHeaderColumn>
+        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderTaskOwner} /></TableHeaderColumn>
+        <TableHeaderColumn />
+      </TableHeader>
+      {!isEmpty(elements) && elements.map(({ logicalId, definition, status, priority, authoredOn, executionPeriod, agent, owner }) => (
+        <TableRow key={logicalId} columns={TASK_TABLE_COLUMNS}>
+          <TableRowColumn>{definition && definition.display}</TableRowColumn>
+          <TableRowColumn>{status && status.display}</TableRowColumn>
+          <TableRowColumn>{priority && priority.display}</TableRowColumn>
+          <TableRowColumn>{authoredOn}</TableRowColumn>
+          <TableRowColumn>{executionPeriod && executionPeriod.start} - {executionPeriod && executionPeriod.end} </TableRowColumn>
+          <TableRowColumn>{agent && agent.display} </TableRowColumn>
+          <TableRowColumn>{owner && owner.display} </TableRowColumn>
+          <TableRowColumn>
+            <NavigationStyledIconMenu>
+              <StyledMenuItem
+                primaryText="Cancel"
+                disabled={status.code === 'cancelled'}
+                onClick={() => cancelTask(logicalId)}
+              />
+            </NavigationStyledIconMenu>
+          </TableRowColumn>
+        </TableRow>
+      ))}
+    </Table>
   );
 }
 
