ecc351ae002b1be4c84fa542407377c5de376948
burcakulug <burcak.ulug@feisystems.com>
2018-03-06 10:22:35 -0500

    Change FormSubtitle default top margin from 1vh to 2vh
    #18130:[Enabler] As a developer, I should only use the styled component so that there is a unified styling approach in OCP

--- app/components/FormSubtitle/index.js
+++ app/components/FormSubtitle/index.js
@@ -9,7 +9,7 @@ import PropTypes from 'prop-types';
 
 const FormSubtitle = styled.div`
   padding-left: 0.5vw;
-  margin: ${(props) => props.subtitleMargin ? props.subtitleMargin : '1vh 1vw 0 1vw'};
+  margin: ${(props) => props.margin ? props.margin : '2vh 1vw 0 1vw'};
   font-size: 1.2rem;
   font-weight: bold;
   color: #444;
@@ -17,7 +17,7 @@ const FormSubtitle = styled.div`
 `;
 
 FormSubtitle.propTypes = {
-  subtitleMargin: PropTypes.string,
+  margin: PropTypes.string,
 };
 
 export default FormSubtitle;
