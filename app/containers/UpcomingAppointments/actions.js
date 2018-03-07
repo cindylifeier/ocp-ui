3908e5b8a2ab983275332f7304a633d5e8d4c2e5
Teng Luo <Teng.Luo@feisystems.com>
2018-03-07 14:31:54 -0500

    Implement action api and saga for Upcoming Appointments Container
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

--- app/containers/UpcomingAppointments/actions.js
+++ app/containers/UpcomingAppointments/actions.js
@@ -5,11 +5,30 @@
  */
 
 import {
-  DEFAULT_ACTION,
+  GET_UPCOMING_APPOINTMENTS,
+  GET_UPCOMING_APPOINTMENTS_SUCCESS,
+  GET_UPCOMING_APPOINTMENTS_ERROR,
 } from './constants';
 
-export function defaultAction() {
+
+export function getUpcomingAppointments(query, patientName) {
   return {
-    type: DEFAULT_ACTION,
+    type: GET_UPCOMING_APPOINTMENTS,
+    query,
+    patientName,
+  };
+}
+
+export function getUpcomingAppointmentsSuccess(upcomingAppointmentsPage) {
+  return {
+    type: GET_UPCOMING_APPOINTMENTS_SUCCESS,
+    upcomingAppointmentsPage,
+  };
+}
+
+export function getUpcomingAppointmentsError(error) {
+  return {
+    type: GET_UPCOMING_APPOINTMENTS_ERROR,
+    error,
   };
 }
--- app/containers/UpcomingAppointments/saga.js
+++ app/containers/UpcomingAppointments/saga.js
@@ -1,6 +1,41 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { GET_UPCOMING_APPOINTMENTS } from 'containers/UpcomingAppointments/constants';
+import { getUpcomingAppointmentsSuccess, getUpcomingAppointmentsError } from './actions';
+import getUpComingAppointmentsApi from './api';
+import { showNotification } from '../Notification/actions';
 
-// Individual exports for testing
-export default function* defaultSaga() {
-  // See example in containers/HomePage/saga.js
+
+function getErrorMessage(err) {
+  let errorMessage = '';
+  if (err && err.message === 'Failed to fetch') {
+    errorMessage = 'Failed to retrieve patient\'s upcoming appointments. Server is offline.';
+  } else if (err && err.response && err.response.status === 404) {
+    errorMessage = 'Not have any upcoming appointments..';
+  } else if (err && err.response && err.response.status === 500) {
+    errorMessage = 'Failed to retrieve patient\'s upcoming appointments. Unknown server error.';
+  } else {
+    errorMessage = 'Failed to retrieve patient\'s upcoming appointments.. Unknown error.';
+  }
+  return errorMessage;
+}
+
+export function* getUpComingAppointments({ query }) {
+  try {
+    const upcomingAppointmentsPage = yield call(getUpComingAppointmentsApi, query);
+    yield put(getUpcomingAppointmentsSuccess(upcomingAppointmentsPage));
+  } catch (err) {
+    const errMsg = getErrorMessage(err);
+    yield put(getUpcomingAppointmentsError(err));
+    yield put(showNotification(errMsg));
+  }
+}
+
+export function* watchGetUpComingAppointments() {
+  yield takeLatest(GET_UPCOMING_APPOINTMENTS, getUpComingAppointments);
+}
+
+export default function* rootSaga() {
+  yield all([
+    watchGetUpComingAppointments(),
+  ]);
 }
