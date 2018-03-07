c785d67994c19e851f59921db735a1f46dc52b96
Teng Luo <Teng.Luo@feisystems.com>
2018-03-07 13:46:30 -0500

    Create constant for action type
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

--- app/containers/UpcomingAppointments/constants.js
+++ app/containers/UpcomingAppointments/constants.js
@@ -8,8 +8,9 @@
  *  UpcomingAppointments action types
  *  @type {string}
  */
-export const DEFAULT_ACTION = 'ocpui/UpcomingAppointments/DEFAULT_ACTION';
-
+export const GET_UPCOMING_APPOINTMENTS = 'ocpui/UpcomingAppointments/GET_UPCOMING_APPOINTMENTS';
+export const GET_UPCOMING_APPOINTMENTS_SUCCESS = 'ocpui/UpcomingAppointments/GET_UPCOMING_APPOINTMENTS_SUCCESS';
+export const GET_UPCOMING_APPOINTMENTS_ERROR = 'ocpui/UpcomingAppointments/GET_UPCOMING_APPOINTMENTS_ERROR';
 
 /**
 *  Other constants
