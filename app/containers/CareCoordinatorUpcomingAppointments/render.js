93cce8b93b336d9a7d2581675d7d44df7e840258
Teng Luo <Teng.Luo@feisystems.com>
2018-03-07 12:37:42 -0500

    Remove Care Coordinator upcoming appointments
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

--- app/containers/CareCoordinatorUpcomingAppointments/render.js
+++
@@ -1,4 +0,0 @@
-import renderFactory from 'utils/goldenLayout/renderFactory';
-import CareCoordinatorUpcomingAppointment from './index';
-
-export default renderFactory(CareCoordinatorUpcomingAppointment);
--- app/containers/CareCoordinatorUpcomingAppointments/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import careCoordinatorUpcomingAppointmentReducer from '../reducer';
-
-describe('careCoordinatorUpcomingAppointmentReducer', () => {
-  it('returns the initial state', () => {
-    expect(careCoordinatorUpcomingAppointmentReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
