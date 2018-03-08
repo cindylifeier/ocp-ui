826a9e229eb933537d67c5d9de4801712fcb53bd
JiahaoLi <Jiahao.Li@feisystems.com>
2018-03-07 21:17:48 -0500

    Implement styled component StyledCustomErrorText
    17826: As a user, I can enter multiple telecoms and addresses for an organization, so that the organization has complete contact information

---
+++ app/components/StyledCustomErrorText/messages.js
@@ -0,0 +1,13 @@
+/*
+ * StyledCustomErrorText Messages
+ *
+ * This contains all the text for the StyledCustomErrorText component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'ocpui.components.StyledCustomErrorText.header',
+    defaultMessage: 'This is the StyledCustomErrorText component !',
+  },
+});
