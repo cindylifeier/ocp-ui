b1ea1e5e1786017a9c5f66fa3b0dc83550106a13
Teng Luo <Teng.Luo@feisystems.com>
2018-03-08 16:04:55 -0500

    Add Container Header.
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

--- app/containers/UpcomingAppointments/index.js
+++ app/containers/UpcomingAppointments/index.js
@@ -12,7 +12,9 @@ import { compose } from 'redux';
 import { getLookupsAction } from 'containers/App/actions';
 import { makeSelectAppointmentTypes, makeSelectAppointmentStatuses } from 'containers/App/lookupSelectors';
 import isEmpty from 'lodash/isEmpty';
+import { FormattedMessage } from 'react-intl';
 import Card from 'components/Card/index';
+import CardHeader from 'components/CardHeader';
 import NoUpcomingAppointments from 'containers/UpcomingAppointments/NoUpcomingAppointments';
 import CareCoordinatorUpcomingAppointmentTable from 'components/CareCoordinatorUpcomingAppointmentTable/index';
 import { getUpcomingAppointments } from 'containers/UpcomingAppointments/actions';
@@ -24,7 +26,9 @@ import injectReducer from 'utils/injectReducer';
 import makeSelectUpcomingAppointments from './selectors';
 import reducer from './reducer';
 import saga from './saga';
+import messages from './messages';
 import { APPOINTMENT_STATUS, APPOINTMENT_TYPE } from '../App/constants';
+
 
 export class UpcomingAppointments extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -43,10 +47,11 @@ export class UpcomingAppointments extends React.PureComponent { // eslint-disabl
     return (
       <div>
         <Card>
+          <CardHeader title={<FormattedMessage {...messages.header} />} />
           {loading &&
           <RefreshIndicatorLoading />}
           {!loading && isEmpty(data) &&
-          <NoUpcomingAppointments>No Upcoming Appointments.</NoUpcomingAppointments>}
+          <NoUpcomingAppointments>{<FormattedMessage {...messages.noUpcomingAppointments} />}</NoUpcomingAppointments>}
           { !isEmpty(data) && !isEmpty(data.elements) &&
           <CenterAlign>
             <CareCoordinatorUpcomingAppointmentTable elements={data.elements} appointmentStatuses={appointmentStatuses} appointmentTypes={appointmentTypes} />
