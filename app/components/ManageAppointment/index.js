c35b2605d7306b912b901cc24eb066fb5c1d1741
Shruti Rao <shruti.rao@feisystems.com>
2018-03-08 15:43:25 -0500

    Implement ManageAppointment component
    #18171:As a User I should be able to create an appointment request with a patient so I stay informed of who I will meet

--- app/components/ManageAppointment/index.js
+++ app/components/ManageAppointment/index.js
@@ -1,25 +1,106 @@
 /**
-*
-* ManageAppointment
-*
-*/
+ *
+ * ManageAppointment
+ *
+ */
 
+import ManageAppointmentForm from 'components/ManageAppointment/ManageAppointmentFormOne';
+import { Formik } from 'formik';
+import isEmpty from 'lodash/isEmpty';
+import PropTypes from 'prop-types';
 import React from 'react';
-// import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
+import mapToPatientName from 'utils/PatientUtils';
+import Util from 'utils/Util';
+import yup from 'yup';
 import messages from './messages';
 
-function ManageAppointment() {
+function ManageAppointment(props) {
+  const {
+    selectedPatient,
+    appointment,
+    editMode,
+    appointmentStatuses,
+    appointmentTypes,
+    handleOpen,
+    onSave,
+    selectedParticipants,
+    initialSelectedParticipants,
+    removeParticipant,
+  } = props;
+  const propsFromContainer = {
+    selectedPatient,
+    appointmentStatuses,
+    appointmentTypes,
+    handleOpen,
+    selectedParticipants,
+    initialSelectedParticipants,
+    removeParticipant,
+  };
   return (
     <div>
-      <FormattedMessage {...messages.header} />
+      {selectedPatient &&
+      <div>
+        {((editMode && appointment) || !editMode) &&
+        <Formik
+          isInitialValid={editMode}
+          initialValues={setFormData(appointment)}
+          onSubmit={(values, actions) => {
+            onSave(values, actions);
+          }}
+          validationSchema={yup.object().shape({
+            patientName: yup.string()
+              .required((<FormattedMessage {...messages.validation.required} />)),
+            date: yup.date()
+              .required((<FormattedMessage {...messages.validation.required} />)),
+            status: yup.string()
+              .required((<FormattedMessage {...messages.validation.required} />)),
+            startDate: yup.date()
+              .required((<FormattedMessage {...messages.validation.required} />))
+              .min(new Date().toLocaleDateString(), (<FormattedMessage {...messages.validation.minStartDate} />)),
+          })
+          }
+          render={(formikProps) => <ManageAppointmentForm {...formikProps} {...propsFromContainer} />}
+        />
+        }
+      </div>
+      }
     </div>
   );
 }
 
 ManageAppointment.propTypes = {
-
+  handleOpen: PropTypes.func.isRequired,
+  removeParticipant: PropTypes.func.isRequired,
+  onSave: PropTypes.func.isRequired,
+  editMode: PropTypes.bool.isRequired,
+  selectedPatient: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.array.isRequired,
+  }),
+  appointment: PropTypes.any,
+  appointmentStatuses: PropTypes.array.isRequired,
+  appointmentTypes: PropTypes.array.isRequired,
+  selectedParticipants: PropTypes.array,
+  initialSelectedParticipants: PropTypes.array,
 };
 
+function setFormData(appointment, selectedPatient) {
+  let formData = null;
+  if (!isEmpty(appointment)) {
+    formData = {
+      patientName: mapToPatientName(selectedPatient),
+      description: appointment.description,
+      status: appointment.statusCode,
+      date: appointment.date,
+      startTime: appointment.startTime,
+      endDate: appointment.endTime,
+      appointmentType: appointment.appointmentType,
+    };
+  }
+  return Util.pickByIdentity(formData);
+}
+
 export default ManageAppointment;
+
