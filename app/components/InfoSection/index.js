801233504d25c8f00688f8ab518f136273ea8019
Shruti Rao <shruti.rao@feisystems.com>
2018-03-08 15:53:47 -0500

    Add InfoSection component
    #18171:As a User I should be able to create an appointment request with a patient so I stay informed of who I will meet

---
+++ app/components/InfoSection/index.js
@@ -0,0 +1,22 @@
+/**
+*
+* InfoSection
+*
+*/
+
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const InfoSection = styled.div`
+  margin: ${({ margin }) => margin};
+`;
+
+InfoSection.propTypes = {
+  margin: PropTypes.string,
+};
+
+InfoSection.defaultProps = {
+  margin: '10px 10px',
+};
+
+export default InfoSection;
