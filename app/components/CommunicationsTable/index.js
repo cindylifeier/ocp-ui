050b1da6e02c9eba0827e6c18da5e32a95474906
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-08 12:44:09 -0500

    Implement communication table
    18345 - As a user I should be able to create and edit communications with patient so I can check my notes later - Functionality Implementation

---
+++ app/components/CommunicationsTable/index.js
@@ -0,0 +1,116 @@
+/**
+*
+* CommunicationsTable
+*
+*/
+
+import React from 'react';
+
+import { FormattedMessage } from 'react-intl';
+import PropTypes from 'prop-types';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
+import Link from 'react-router-dom/es/Link';
+import { MANAGE_COMMUNICATION_URL } from 'containers/App/constants';
+import styles from './styles.css';
+import messages from './messages';
+import Table from '../Table';
+import TableHeader from '../TableHeader';
+import TableHeaderColumn from '../TableHeaderColumn';
+import TableRow from '../TableRow';
+import TableRowColumn from '../TableRowColumn';
+
+
+const iconStyles = {
+  iconButton: {
+    position: 'relative',
+  },
+  icon: {
+    width: '100%',
+    height: 26,
+    position: 'absolute',
+    top: '0',
+    right: '0',
+  },
+};
+
+
+function CommunicationsTable({ communications, selectedPatientId }) {
+  return (
+    <div >
+      <Table>
+        <TableHeader>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderCategory} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderContactMethod} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderRecipients} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderSender} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderSent} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderStatus} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderAction} /></TableHeaderColumn>
+        </TableHeader>
+        {communications && communications.map((communication) => (
+          <TableRow key={communication.logicalId}>
+            <TableRowColumn>{communication.categoryValue}</TableRowColumn>
+            <TableRowColumn>{communication.mediumVaule}</TableRowColumn>
+            <TableRowColumn> { getRecipientsList(communication.recipient)}</TableRowColumn>
+            <TableRowColumn>{communication.sender.display}</TableRowColumn>
+            <TableRowColumn>{communication.sent}</TableRowColumn>
+            <TableRowColumn>{communication.statusValue}</TableRowColumn>
+            <TableRowColumn>
+              <div className={styles.iconButtonGridContainer}>
+                <div className={styles.iconButtonGridItem}>
+                  <IconMenu
+                    iconButtonElement={
+                      (<IconButton
+                        className={styles.iconButton}
+                        iconStyle={iconStyles.icon}
+                        style={iconStyles.iconButton}
+                      >
+                        <NavigationMenu />
+                      </IconButton>)
+                    }
+                    anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                  >
+                    <MenuItem
+                      className={styles.menuItem}
+                      primaryText="Edit"
+                      containerElement={<Link
+                        to={{
+                          pathname: `${MANAGE_COMMUNICATION_URL}/${communication.logicalId}`,
+                          search: `?patientId=${selectedPatientId}`,
+                        }}
+                      />}
+                    />
+                  </IconMenu>
+                </div>
+              </div>
+            </TableRowColumn>
+          </TableRow>
+        ))
+        }
+      </Table>
+    </div>
+  );
+}
+
+CommunicationsTable.propTypes = {
+  communications: PropTypes.array.isRequired,
+  selectedPatientId: PropTypes.string.isRequired,
+};
+
+export default CommunicationsTable;
+
+function getRecipientsList(recipients) {
+  const names = [];
+  if (recipients) {
+    recipients.forEach((entry) => {
+      if (entry.display) {
+        names.push(entry.display);
+      }
+    });
+  }
+  return names.join(',');
+}
---
+++ app/components/CommunicationsTable/messages.js
@@ -0,0 +1,38 @@
+/*
+ * CommunicationsTable Messages
+ *
+ * This contains all the text for the CommunicationsTable component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  columnHeaderCategory: {
+    id: 'ocpui.components.communicationTable.columnHeaderCategory',
+    defaultMessage: 'Category',
+  },
+  columnHeaderContactMethod: {
+    id: 'ocpui.components.communicationTable.columnHeaderContactMethod',
+    defaultMessage: 'Contact Method',
+  },
+  columnHeaderRecipients: {
+    id: 'ocpui.components.communicationTable.columnHeaderRecipients',
+    defaultMessage: 'Recipients',
+  },
+  columnHeaderSender: {
+    id: 'ocpui.components.communicationTable.columnHeaderSender',
+    defaultMessage: 'Sender',
+  },
+  columnHeaderSent: {
+    id: 'ocpui.components.communicationTable.columnHeaderSent',
+    defaultMessage: 'Sent',
+  },
+  columnHeaderStatus: {
+    id: 'ocpui.components.communicationTable.columnHeaderStatus',
+    defaultMessage: 'Status',
+  },
+  columnHeaderAction: {
+    id: 'ocpui.components.communicationTable.columnHeaderAction',
+    defaultMessage: 'Action',
+  },
+
+});
