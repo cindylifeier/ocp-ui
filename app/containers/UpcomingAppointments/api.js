e1c76b897e8bf68658c8238ac9afbdfd5c98a991
Teng Luo <Teng.Luo@feisystems.com>
2018-03-09 13:29:19 -0500

    Remove unused code
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

--- app/containers/UpcomingAppointments/api.js
+++ app/containers/UpcomingAppointments/api.js
@@ -7,7 +7,6 @@ import { BASE_UPCOMING_APPOINTMENTS_API_URL, getEndpoint } from '../../utils/end
 export default function getUpcomingAppointments() {
   const query = { searchKey: SEARCH_KEY, searchValue: SEARCH_VALUE, pageNumber: DEFAULT_START_PAGE_NUMBER, pageSize: DEFAULT_PAGE_SIZE };
   const params = queryString(query);
-  console.log(params);
   const baseEndpoint = getEndpoint(BASE_UPCOMING_APPOINTMENTS_API_URL);
   const requestURL = `${baseEndpoint}/search${params}`;
   return request(requestURL);
