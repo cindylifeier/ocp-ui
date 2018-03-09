e0d8e852b3f9b014f4213dd2add3968bfb6fe57d
Teng Luo <Teng.Luo@feisystems.com>
2018-03-09 13:26:59 -0500

    Add constant for search care coordinator upcoming appointment
    #18176:As a care coordinator I will be able to see the list of all my upcoming appointments in my dashboard so I can plan my day.

--- app/containers/UpcomingAppointments/api.js
+++ app/containers/UpcomingAppointments/api.js
@@ -1,11 +1,13 @@
+import { SEARCH_KEY, SEARCH_VALUE } from 'containers/UpcomingAppointments/constants';
 import queryString from '../../utils/queryString';
 import { DEFAULT_PAGE_SIZE, DEFAULT_START_PAGE_NUMBER } from '../App/constants';
 import request from '../../utils/request';
 import { BASE_UPCOMING_APPOINTMENTS_API_URL, getEndpoint } from '../../utils/endpointService';
 
 export default function getUpcomingAppointments() {
-  const query = { pageNumber: DEFAULT_START_PAGE_NUMBER, pageSize: DEFAULT_PAGE_SIZE };
+  const query = { searchKey: SEARCH_KEY, searchValue: SEARCH_VALUE, pageNumber: DEFAULT_START_PAGE_NUMBER, pageSize: DEFAULT_PAGE_SIZE };
   const params = queryString(query);
+  console.log(params);
   const baseEndpoint = getEndpoint(BASE_UPCOMING_APPOINTMENTS_API_URL);
   const requestURL = `${baseEndpoint}/search${params}`;
   return request(requestURL);
--- app/containers/UpcomingAppointments/constants.js
+++ app/containers/UpcomingAppointments/constants.js
@@ -12,9 +12,13 @@ export const INITIALIZE_UPCOMING_APPOINTMENTS = 'ocpui/UpcomingAppointments/INIT
 export const GET_UPCOMING_APPOINTMENTS = 'ocpui/UpcomingAppointments/GET_UPCOMING_APPOINTMENTS';
 export const GET_UPCOMING_APPOINTMENTS_SUCCESS = 'ocpui/UpcomingAppointments/GET_UPCOMING_APPOINTMENTS_SUCCESS';
 export const GET_UPCOMING_APPOINTMENTS_ERROR = 'ocpui/UpcomingAppointments/GET_UPCOMING_APPOINTMENTS_ERROR';
-export const LOADING = 'loading';
-export const DATA = 'data';
+
 /**
 *  Other constants
 *
 */
+export const LOADING = 'loading';
+export const DATA = 'data';
+
+export const SEARCH_KEY = 'practitionerid';
+export const SEARCH_VALUE = '1528';
