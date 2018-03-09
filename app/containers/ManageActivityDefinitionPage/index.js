0c532afedb5e0eb3d294a14870d506b7c1b4247b
burcakulug <burcak.ulug@feisystems.com>
2018-03-09 09:54:25 -0500

    Refactor imports in ManageActivityDefinitionPage
    #18130:[Enabler] As a developer, I should only use the styled component so that there is a unified styling approach in OCP

--- app/containers/ManageActivityDefinitionPage/index.js
+++ app/containers/ManageActivityDefinitionPage/index.js
@@ -15,28 +15,30 @@ import { compose } from 'redux';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
-import { getLookupsAction } from '../App/actions';
-import reducer from './reducer';
-import saga from './saga';
-import messages from './messages';
-import styles from './styles.css';
-import ManageActivityDefinition from '../../components/ManageActivityDefinition';
+import ManageActivityDefinition from 'components/ManageActivityDefinition';
+import { getLookupsAction } from 'containers/App/actions';
 import {
   ACTION_PARTICIPANT_ROLE,
   ACTION_PARTICIPANT_TYPE,
   DEFINITION_TOPIC,
-  PUBLICATION_STATUS, RELATED_ARTIFACT_TYPE,
+  PUBLICATION_STATUS,
+  RELATED_ARTIFACT_TYPE,
   RESOURCE_TYPE,
-} from '../App/constants';
-import { makeSelectOrganization } from '../Locations/selectors';
-import { createActivityDefinition } from '../ManageActivityDefinitionPage/actions';
+} from 'containers/App/constants';
+import { makeSelectOrganization } from 'containers/Locations/selectors';
+import { createActivityDefinition } from 'containers/ManageActivityDefinitionPage/actions';
 import {
   makeSelectActionParticipantRoles,
   makeSelectActionParticipantTypes,
   makeSelectDefinitionTopics,
-  makeSelectPublicationStatuses, makeSelectRelatedArtifactTypes,
+  makeSelectPublicationStatuses,
+  makeSelectRelatedArtifactTypes,
   makeSelectResourceTypes,
-} from '../App/lookupSelectors';
+} from 'containers/App/lookupSelectors';
+import reducer from './reducer';
+import saga from './saga';
+import messages from './messages';
+import styles from './styles.css';
 
 export class ManageActivityDefinitionPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
