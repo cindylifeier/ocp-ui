82b7abd134fdeece2a751fadff2da59024089f88
Shruti Rao <shruti.rao@feisystems.com>
2018-03-11 18:01:29 -0400

    Implement mapToBackendAppointmentDuringCreate function
    #18171:As a User I should be able to create an appointment request with a patient so I stay informed of who I will meet

--- app/containers/ManageAppointmentPage/api.js
+++ app/containers/ManageAppointmentPage/api.js
@@ -1,3 +1,5 @@
+import { isEmpty } from 'lodash';
+import isUndefined from 'lodash/isUndefined';
 import { BASE_APPOINTMENTS_API_URL, getEndpoint } from 'utils/endpointService';
 import request from '../../utils/request';
 
@@ -15,7 +17,7 @@ export function saveAppointment(appointmentFormData) {
 
 export function createAppointmentApiCall(appointmentFormData) {
   const requestUrl = `${baseEndpoint}`;
-  const body = JSON.stringify(mapToBackendAppointment(appointmentFormData));
+  const body = JSON.stringify(mapToBackendAppointmentDuringCreate(appointmentFormData));
   return request(requestUrl, {
     method: 'POST',
     headers,
@@ -28,7 +30,7 @@ function updateAppointmentApiCall(appointmentFormData) {
   const requestURL = `${baseEndpoint}/${appointmentId}`;
   return request(requestURL, {
     method: 'PUT',
-    body: JSON.stringify(mapToBackendAppointment(appointmentFormData)),
+    body: JSON.stringify(mapToBackendAppointmentDuringUpdate(appointmentFormData)),
     headers: {
       'Content-Type': 'application/json',
     },
@@ -36,14 +38,69 @@ function updateAppointmentApiCall(appointmentFormData) {
 }
 
 export function determineNotificationForAppointment(appointmentFormData) {
-  let action = 'create';
+  let action = 'created';
   if (appointmentFormData.appointmentId) {
-    action = 'edit';
+    action = 'edited';
   }
   return action;
 }
 
-function mapToBackendAppointment(appointmentFormData) {
-  // TODO
+function mapToBackendAppointmentDuringCreate(appointmentFormData) {
+  const { appointmentType, date, description, startTime, endTime, participants, patientId, patientName } = appointmentFormData;
+  const appointmentDataToSubmit = {};
+  if (!isUndefined(description)) {
+    appointmentDataToSubmit.description = description;
+  }
+  if (!isUndefined(appointmentType)) {
+    appointmentDataToSubmit.typeCode = appointmentType;
+  }
+  let appointmentDateString;
+  let utcHours;
+  let utcMinutes;
+  if (!isUndefined(date)) {
+    const selectedDate = new Date(date);
+    const year = selectedDate.getFullYear();
+    // Note: 0=January, 1=February etc.
+    const month = toDoubleChars(selectedDate.getMonth() + 1);
+    const day = toDoubleChars(selectedDate.getDate());
+    appointmentDateString = `${year}-${month}-${day}`;
+  }
+  if (!isUndefined(date) && !isUndefined(startTime)) {
+    utcHours = toDoubleChars(new Date(startTime).getUTCHours());
+    utcMinutes = toDoubleChars(new Date(startTime).getUTCMinutes());
+    appointmentDataToSubmit.start = `${appointmentDateString}T${utcHours}:${utcMinutes}:00.00`;
+  }
+  if (!isUndefined(date) && !isUndefined(endTime)) {
+    utcHours = toDoubleChars(new Date(endTime).getUTCHours());
+    utcMinutes = toDoubleChars(new Date(endTime).getUTCMinutes());
+    appointmentDataToSubmit.end = `${appointmentDateString}T${utcHours}:${utcMinutes}:00.00`;
+  }
+  // appointmentDataToSubmit.creatorReference = 'Practitioner/1528';
+  appointmentDataToSubmit.creatorReference = 'Practitioner/1709';
+  appointmentDataToSubmit.creatorName = 'Lee Coordinator';
+  // Participant
+  if (!isUndefined(participants) && !isEmpty(participants)) {
+    // TODO: When adding Participants is implemented
+  }
+  const patientParticipant = [];
+  const patientReference = `Patient/${patientId}`;
+  patientParticipant.push({
+    actorReference: patientReference,
+    actorName: patientName,
+  });
+  appointmentDataToSubmit.participant = patientParticipant;
+  return appointmentDataToSubmit;
+}
+
+function mapToBackendAppointmentDuringUpdate(appointmentFormData) {
+  // TODO: When edit appointment is implemented
   return appointmentFormData;
 }
+
+function toDoubleChars(number) {
+  if (number < 10) {
+    return `0${number}`;
+  }
+  return number;
+}
+
