abb865cad85461e957ed59a5065333c847ec0819
CindyLiFei <cindy.li@feisystems.com>
2018-03-12 11:33:28 -0400

    Fix appointment display panel
    #18133 - As a user, should see my patient's immediate tasks in Upcoming Tasks panel so I can prioritize my activities

--- app/containers/UpcomingAppointments/render.js
+++ app/containers/UpcomingAppointments/render.js
@@ -1,4 +1,4 @@
-import { UpcomingAppointments } from 'containers/UpcomingAppointments/index';
+import UpcomingAppointments from 'containers/UpcomingAppointments/index';
 import renderFactory from 'utils/goldenLayout/renderFactory';
 
 export default renderFactory(UpcomingAppointments);
