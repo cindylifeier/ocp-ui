299396ce6cba20f4b20ba7fe44b837f379394c90
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-12 16:01:18 -0400

    Fix bug in getting list of communication.
    18345 - As a user I should be able to create and edit communications with patient so I can check my notes later - Functionality Implementation

--- app/containers/Communications/api.js
+++ app/containers/Communications/api.js
@@ -7,8 +7,7 @@ const baseEndpoint = getEndpoint(BASE_COMMUNICATIONS_API_URL);
 
 export function getCommunications(patientId, pageNumber) {
   const pageSize = PAGE_SIZE;
-  // FIXME: Remove hardcoded patient Id
-  const queryParams = { searchKey: 'patientId', searchValue: 102, pageNumber, pageSize };
+  const queryParams = { searchKey: 'patientId', searchValue: patientId, pageNumber, pageSize };
   const stringifiedParams = queryString.stringify(queryParams);
   const url = `${baseEndpoint}/search?${stringifiedParams}`;
   return request(url);
