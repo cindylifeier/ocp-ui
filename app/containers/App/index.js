39580acc4f64a7f5a33bd4160c54aaddbc3a4426
JiahaoLi <Jiahao.Li@feisystems.com>
2018-03-15 23:10:54 -0400

    Implement initial PractitionerWorkspacePage container component
    #18531: Create workflow role based UI workspaces - OCP Admin

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -20,6 +20,7 @@ import 'font-awesome/css/font-awesome.min.css';
 import Authentication from 'containers/Authentication';
 import WorkspaceSelectionPage from 'containers/WorkspaceSelectionPage';
 import AdminWorkspacePage from 'containers/AdminWorkspacePage';
+import PractitionerWorkspacePage from 'containers/PractitionerWorkspacePage';
 import PatientWorkspacePage from 'containers/PatientWorkspacePage';
 import HomePage from 'containers/HomePage/Loadable';
 import LoginPage from 'containers/LoginPage';
@@ -61,6 +62,7 @@ export function App() {
           <Authentication>
             <Route path="/ocp-ui/workspace-selection" component={WorkspaceSelectionPage} />
             <Route path="/ocp-ui/admin-workspace" component={AdminWorkspacePage} />
+            <Route path="/ocp-ui/practitioner-workspace" component={PractitionerWorkspacePage} />
             <Route path="/ocp-ui/patient-workspace" component={PatientWorkspacePage} />
             <Route path="/ocp-ui/home" component={HomePage} />
             <Route exact path="/ocp-ui/patients" component={PatientsPage} />
