54e01593378d7949d6f6c33768278fa7af00cc29
Teng Luo <Teng.Luo@feisystems.com>
2018-03-19 14:12:41 -0400

    Add hover style for tab
    #18441:[Demo Feedback] The headings on the Panel Tabs should be of reasonable font size and color that is contrasting to background.

--- app/components/GoldenLayout/GoldenLayoutContainer.js
+++ app/components/GoldenLayout/GoldenLayoutContainer.js
@@ -4,26 +4,46 @@ import PropTypes from 'prop-types';
 const GoldenLayoutContainer = styled.div`
   /* Set height considering app bar height */
   & .lm_root {
-    height: ${(props) => props.containerHeight ? props.containerHeight : 'calc(100vh - 75px)'};
+    height: ${(props) => props.containerHeight};
   }
 
   /* Enable scroll content */
   & .lm_content {
     overflow: auto;
-    background: #f4f4f4;
+    background: #f2f2f2;
+  }
+
+  & .lm_tab {
+    font-weight: bold;
   }
 
   & .lm_tab.lm_active {
-    background: #f4f4f4;
-    color: #33666f;
-    font-weight: bold;
-    font-size: 16px;
-    min-height: 18px;
+    color: ${(props) => props.primaryColor};
+    background: ${(props) => props.secondaryColor};
+    font-size: 18px;
+    font-family: Arial Bold, Arial, sans-serif;
+    height: 20px;
+  }
+
+  & .lm_tab.lm_active:hover,
+  .lm_tab:hover {
+    color: ${(props) => props.secondaryColor};
+    background: ${(props) => props.primaryColor};
+    font-size: 18px;
+    height: 20px;
   }
 `;
 
 GoldenLayoutContainer.propTypes = {
   containerHeight: PropTypes.string,
+  primaryColor: PropTypes.string,
+  secondaryColor: PropTypes.string,
+};
+
+GoldenLayoutContainer.defaultProps = {
+  containerHeight: 'calc(100vh - 75px)',
+  primaryColor: '#33666f',
+  secondaryColor: '#ffffff',
 };
 
 export default GoldenLayoutContainer;
