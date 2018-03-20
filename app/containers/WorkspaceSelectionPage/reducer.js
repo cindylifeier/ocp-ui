77baa613532ae9f0d218e2a64d27961756957cb8
JiahaoLi <Jiahao.Li@feisystems.com>
2018-03-19 21:40:32 -0400

    Implement reducer in workspace container component
    #18531: Create workflow role based UI workspaces - OCP Admin

--- app/containers/WorkspaceSelectionPage/reducer.js
+++ app/containers/WorkspaceSelectionPage/reducer.js
@@ -6,15 +6,41 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
-} from './constants';
+  GET_CARE_COORDINATORS_SUCCESS,
+  GET_CARE_MANAGERS_SUCCESS,
+  GET_ORGANIZATIONS_SUCCESS,
+  GET_PATIENTS_SUCCESS,
+} from 'containers/WorkspaceSelectionPage/constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  organizations: {
+    data: [],
+  },
+  careManagers: {
+    data: [],
+  },
+  careCoordinators: {
+    data: [],
+  },
+  patients: {
+    data: [],
+  },
+});
 
 function workspaceSelectionPageReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case GET_ORGANIZATIONS_SUCCESS:
+      return state
+        .setIn(['organizations', 'data'], fromJS(action.organizations.elements));
+    case GET_CARE_MANAGERS_SUCCESS:
+      return state
+        .setIn(['careManagers', 'data'], fromJS(action.careManagers.elements));
+    case GET_CARE_COORDINATORS_SUCCESS:
+      return state
+        .setIn(['careCoordinators', 'data'], fromJS(action.careCoordinators.elements));
+    case GET_PATIENTS_SUCCESS:
+      return state
+        .setIn(['patients', 'data'], fromJS(action.patients.elements));
     default:
       return state;
   }
