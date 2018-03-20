536d4c4eb00f5d656daf707c7633f504c4a5b579
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-20 16:11:01 -0400

    Remove presentation logic in search participant component
    18575 - As a User I should be able to add communication associated with Task and Appointment

--- app/containers/SearchRecipient/index.js
+++ app/containers/SearchRecipient/index.js
@@ -13,37 +13,26 @@ import { compose } from 'redux';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
-import DialogHeader from 'components/DialogHeader/index';
+import DialogHeader from 'components/DialogHeader';
 import { makeSelectRecipients, makeSelectSelectedRecipients } from 'containers/SearchRecipient/selectors';
 import {
   addSelectedRecipients,
   getRecipients,
   setSelectRecipientStatus,
 } from 'containers/SearchRecipient/actions';
-import FormGrid from 'components/FormGrid/index';
-import FormCell from 'components/FormCell/index';
-import { Cell, Grid } from 'styled-css-grid';
 import { getRoleName } from 'utils/CommunicationUtils';
-import StyledFlatButton from 'components/StyledFlatButton/index';
+import StyledFlatButton from 'components/StyledFlatButton';
 import Dialog from 'material-ui/Dialog';
 import StyledRaisedButton from 'components/StyledRaisedButton';
-import SelectField from 'components/SelectField/index';
-import MenuItem from 'material-ui/MenuItem';
-import ActionSearch from 'material-ui/svg-icons/action/search';
-import StyledIconButton from 'components/StyledIconButton/index';
-import { Form, Formik } from 'formik';
-import TextField from 'components/TextField/index';
-import { customContentStyle, floatingLabelStyle, iconButtonStyle } from 'containers/SearchRecipient/constants';
-import yup from 'yup';
+import { customContentStyle } from 'containers/SearchRecipient/constants';
+import SearchRecipientDialogContent from 'components/SearchRecipientDialogContent';
 import { getLookupsAction } from 'containers/App/actions';
 import { PARTICIPANTROLE, PARTICIPANTTYPE } from 'containers/App/constants';
 import { makeSelectParticipantTypes } from 'containers/App/lookupSelectors';
 import makeSelectSelectedPatient from 'containers/App/sharedDataSelectors';
-import RecipientsTable from 'components/RecipientsTable';
 import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
-
 
 export class SearchRecipient extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -86,6 +75,11 @@ export class SearchRecipient extends React.PureComponent { // eslint-disable-lin
 
   render() {
     const { isOpen, recipients, participantTypes, selectedRecipients } = this.props;
+    const localProps = {
+      recipients,
+      selectedRecipients,
+      participantTypes,
+    };
     const actionsButtons = [
       <StyledFlatButton
         label={<FormattedMessage {...messages.dialogCancelBtnLabel} />}
@@ -108,73 +102,12 @@ export class SearchRecipient extends React.PureComponent { // eslint-disable-lin
         <DialogHeader>
           <FormattedMessage {...messages.addRecipientDialogTitle} />
         </DialogHeader>
-        <div>
-          <Formik
-            onSubmit={(values, actions) => {
-              this.handleSearch(values);
-              actions.setSubmitting(false);
-            }}
-            validationSchema={yup.object().shape({
-            })}
-            render={(formikProps) => {
-              const { isSubmitting, dirty, isValid } = formikProps;
-              return (
-                <Form>
-                  <FormGrid columns={12}>
-                    <FormCell top={1} left={1} width={7}>
-                      <Grid columns="3fr 3fr 1fr" gap="">
-                        <Cell>
-                          <TextField
-                            name="name"
-                            fullWidth
-                            floatingLabelStyle={floatingLabelStyle}
-                            hintText={<FormattedMessage {...messages.hintText.practitionerName} />}
-                            floatingLabelText={<FormattedMessage {...messages.floatingLabelText.practitionerName} />}
-                          />
-                        </Cell>
-                        <Cell>
-                          <SelectField
-                            name="member"
-                            fullWidth
-                            floatingLabelText={<FormattedMessage {...messages.floatingLabelText.practitionerMember} />}
-                          >
-                            {participantTypes && participantTypes.map((member) =>
-                              <MenuItem key={member.code} value={member.code} primaryText={member.display} />,
-                            )}
-                          </SelectField>
-                          <StyledIconButton
-                            style={iconButtonStyle}
-                            tooltip={<FormattedMessage {...messages.searchButtonTooltip} />}
-                            type="submit"
-                            disabled={!dirty || isSubmitting || !isValid}
-                          >
-                            <ActionSearch />
-                          </StyledIconButton>
-                        </Cell>
-                        <Cell>
-                          <StyledIconButton
-                            style={iconButtonStyle}
-                            tooltip={<FormattedMessage {...messages.searchButtonTooltip} />}
-                            type="submit"
-                            disabled={isSubmitting}
-                          >
-                            <ActionSearch />
-                          </StyledIconButton>
-                        </Cell>
-                      </Grid>
-                    </FormCell>
-                  </FormGrid>
-                </Form>
-              );
-            }}
-          />
-          <RecipientsTable
-            recipients={recipients}
-            updateCheck={this.updateCheck}
-            selectedRecipients={selectedRecipients}
-            getRoleName={getRoleName}
-          />
-        </div>
+        <SearchRecipientDialogContent
+          {...localProps}
+          updateCheck={this.updateCheck}
+          handleSearch={this.handleSearch}
+          getRoleName={getRoleName}
+        />
       </Dialog>
     );
   }
