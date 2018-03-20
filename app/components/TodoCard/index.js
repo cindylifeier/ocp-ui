fbbe7a854cfdc14441deacd9c841ec53b5e34adb
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-20 08:10:44 -0400

    Implement to do card
    18134 - As a user I should be able to add items to my To Do list associated to a patient so I can follow up on them

---
+++ app/components/TodoCard/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * To Do Card
+ *
+ */
+import Card from 'components/Card';
+
+
+const TodoCard = Card.extend`
+  min-height: 0;
+  min-height: 150px;
+  padding: 4px !important;
+`;
+
+TodoCard.propTypes = {};
+
+export default TodoCard;
