775f4bafe2a42883091286a6798493a93b6a6a81
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-20 10:31:06 -0400

    Remove commented code
    18575 - As a User I should be able to add communication associated with Task and Appointment

--- app/containers/SearchRecipient/saga.js
+++ app/containers/SearchRecipient/saga.js
@@ -7,17 +7,7 @@ import { showNotification } from 'containers/Notification/actions';
 export function* getRecipientsSaga(action) {
   try {
     if (action.patientId) {
-      // console.log(action.patientId);
       const recipients = yield call(getRecipients, action.patientId, action.communicationId);
-      // TODO: Remove at before merge to master
-      // const recipients = [
-      //   { reference: 'Practitioner/13703', display: 'Monsieur Noysalt BALOIEYL', checked: true },
-      //   { reference: 'Practitioner/316533', display: 'Madame Poynula BASKOALTXNA', checked: true },
-      //   { reference: 'Practitioner/1192278', display: 'Mademoiselle Cobualla VOPHASAT', checked: false },
-      //   { reference: 'Practitioner/1187300', display: 'Monsieur Phunuppa WALDSAMOUSA', checked: true },
-      //   { reference: 'Practitioner/1154566', display: 'Madame Nuldo BASSAHOUN', checked: false },
-      //
-      // ];
       yield put(getRecipientsSuccess(recipients));
     }
   } catch (error) {
