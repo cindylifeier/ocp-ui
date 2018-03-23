6b40f558a8440e4a016d77b1d66b6fb7956bffa9
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-23 07:35:41 -0400

    Implement todo main task related actions
    18134 - As a user I should be able to add items to my To Do list associated to a patient so I can follow up on them

--- app/containers/Todos/actions.js
+++ app/containers/Todos/actions.js
@@ -3,7 +3,10 @@
  * Todos actions
  *
  */
-import { GET_TODOS, GET_TODOS_ERROR, GET_TODOS_SUCCESS } from 'containers/Todos/constants';
+import {
+  GET_TODOS, GET_TODOS_ERROR, GET_TODOS_SUCCESS, GET_TODO_MAIN_TASK, GET_TODO_MAIN_TASK_ERROR,
+  GET_TODO_MAIN_TASK_SUCCESS,
+} from 'containers/Todos/constants';
 
 export function getTodos(patientId, definition) {
   return {
@@ -24,6 +27,30 @@ export function getTodoSuccess(todos) {
 export function getTodoError(error) {
   return {
     type: GET_TODOS_ERROR,
+    error,
+  };
+}
+
+
+export function getTodoMainTask(patientId, definition) {
+  return {
+    type: GET_TODO_MAIN_TASK,
+    patientId,
+    definition,
+  };
+}
+
+export function getTodoMainTaskSuccess(todoMainTask) {
+  return {
+    type: GET_TODO_MAIN_TASK_SUCCESS,
+    todosTaskRefrence: todoMainTask,
+  };
+}
+
+
+export function getTodoMainTaskError(error) {
+  return {
+    type: GET_TODO_MAIN_TASK_ERROR,
     error,
   };
 }
