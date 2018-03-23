a04609abe3c28077724ca60213849b79898c4776
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-23 13:26:01 -0400

    Implement get status with icon
    18134 - As a user I should be able to add items to my To Do list associated to a patient so I can follow up on them

---
+++ app/components/TodoCardContent/constants.js
@@ -0,0 +1,10 @@
+
+/*
+ *
+ * TodoCard constants
+ *
+ */
+
+export const UPCOMING = 'UPCOMING';
+export const OVER_DUE = 'OVER_DUE';
+export const DUE_TODAY = 'DUE_TODAY';
--- app/components/TodoCardContent/index.js
+++ app/components/TodoCardContent/index.js
@@ -11,7 +11,9 @@ import ToDoCardCell from 'components/TodoCardContent/TodoCardCell';
 import Align from 'components/Align';
 import { Cell, Grid } from 'styled-css-grid';
 import PropTypes from 'prop-types';
-import Link from 'react-router-dom';
+import NotificationPriorityHigh from 'material-ui/svg-icons/notification/priority-high';
+import ContentFlag from 'material-ui/svg-icons/content/flag';
+import { DUE_TODAY, OVER_DUE, UPCOMING } from 'components/TodoCardContent/constants';
 
 function TodoCardContent(props) {
   const {
@@ -20,13 +22,26 @@ function TodoCardContent(props) {
     status,
     description,
     isPatientWorkspace,
-    todoMainTaskLogicalId,
+    // todoMainTaskLogicalId,
     todoLogicalId,
     taskBaseUrl,
     patientId,
   } = props;
   const dueDateStr = 'Due '.concat(dueDate);
-  const editTodoUrl = `${taskBaseUrl}/${todoLogicalId}?patientId=${patientId}isMainTask=false&mainTaskId=${todoMainTaskLogicalId}`;
+  // const editTodoUrl = `${taskBaseUrl}/${todoLogicalId}?patientId=${patientId}isMainTask=false&mainTaskId=${todoMainTaskLogicalId}`;
+  const editTodoUrl = `${taskBaseUrl}/${todoLogicalId}?patientId=${patientId}isMainTask=false`;
+
+  function getStatusWithIcon(statusStr) {
+    let statusElement = null;
+    if (statusStr === UPCOMING) {
+      statusElement = (<div><ContentFlag />{statusStr}</div>);
+    } else if (statusStr === OVER_DUE) {
+      statusElement = (<div><NotificationPriorityHigh />{statusStr}</div>);
+    } else if (statusStr === DUE_TODAY) {
+      statusElement = (<div><NotificationPriorityHigh />{statusStr}</div>);
+    }
+    return statusElement;
+  }
   return (
     <div>
       <TodoCardGrid column={12}>
@@ -50,11 +65,11 @@ function TodoCardContent(props) {
         <ToDoCardCell top={3} left={1} width={12}>
           <Grid columns="6fr 6fr" gap="">
             <Cell>
-              <span>{status}</span>
+              {getStatusWithIcon(status)}
             </Cell>
             <Cell>
               <Align variant="right">
-                <Link to={editTodoUrl}>Manage</Link>
+                <a href={editTodoUrl}>Manage</a>
               </Align>
             </Cell>
           </Grid>
@@ -71,7 +86,7 @@ TodoCardContent.propTypes = {
   patientId: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  todoMainTaskLogicalId: PropTypes.string.isRequired,
+  // todoMainTaskLogicalId: PropTypes.string.isRequired,
   todoLogicalId: PropTypes.string.isRequired,
   taskBaseUrl: PropTypes.string.isRequired,
   isPatientWorkspace: PropTypes.bool.isRequired,
