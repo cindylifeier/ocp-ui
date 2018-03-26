31b5f57d963cf03f23667b3ad49447876196c969
Shruti Rao <shruti.rao@feisystems.com>
2018-03-26 12:23:06 -0400

    Set Practitioner name in the dto when creating appointment

--- app/containers/ManageAppointmentPage/index.js
+++ app/containers/ManageAppointmentPage/index.js
@@ -64,7 +64,7 @@ export class ManageAppointmentPage extends React.PureComponent { // eslint-disab
     if (practitionerId) {
       merge(appointmentFormData, { practitionerId });
     }
-    const practitionerName = mapToPatientName(this.props.user.resource.name[0]);
+    const practitionerName = mapToPatientName(this.props.user.resource);
     if (practitionerName) {
       merge(appointmentFormData, { practitionerName });
     }
