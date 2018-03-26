4f73081cd203e23d2f3890c98966164fd1775523
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-26 12:03:21 -0400

    Fix fail unit test
    18134 - As a user I should be able to add items to my To Do list associated to a patient so I can follow up on them

--- app/components/TodoCard/index.js
+++ app/components/TodoCard/index.js
@@ -6,7 +6,6 @@
 import styled from 'styled-components';
 
 const TodoCard = styled.div`
-  min-height: 0;
   min-height: 150px;
   margin-top: 10px;
   padding: 4px !important;
--- app/components/TodoCard/tests/index.test.js
+++ app/components/TodoCard/tests/index.test.js
@@ -28,17 +28,8 @@ describe('<TodoCard />', () => {
 
     // Assert
     expect(renderedComponent).toHaveStyleRule('padding', '4px !important');
-    expect(renderedComponent).toHaveStyleRule('background-color', 'white');
     expect(renderedComponent).toHaveStyleRule('margin-top', '10px');
-    expect(renderedComponent).toHaveStyleRule('min-width', '650px');
     expect(renderedComponent).toHaveStyleRule('min-height', '150px');
-    expect(renderedComponent).toHaveStyleRule('height', '100%');
-    expect(renderedComponent).toHaveStyleRule('border-radius', '2px');
-    expect(renderedComponent).toHaveStyleRule('position', 'relative');
-    expect(renderedComponent).toHaveStyleRule('box-shadow', '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)');
-    expect(renderedComponent).toHaveStyleRule('transition', 'all 0.5s cubic-bezier(0.25, 0.8, 0.25, 1)');
-    expect(renderedComponent).toHaveStyleRule('box-shadow', '0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)', {
-      modifier: ':hover',
-    });
+    expect(renderedComponent).toHaveStyleRule('border', '1px solid rgb(204, 204, 204)');
   });
 });
