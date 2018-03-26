03810861496ad77e2a36da440713f8a0aa3de2c3
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-26 13:35:55 -0400

    Change todo card padding
    18134 - As a user I should be able to add items to my To Do list associated to a patient so I can follow up on them

--- app/components/TodoCard/index.js
+++ app/components/TodoCard/index.js
@@ -8,7 +8,7 @@ import styled from 'styled-components';
 const TodoCard = styled.div`
   min-height: 150px;
   margin-top: 10px;
-  padding: 4px !important;
+  padding-bottom: 4px !important;
   border: 1px solid rgb(204, 204, 204);
 `;
 
