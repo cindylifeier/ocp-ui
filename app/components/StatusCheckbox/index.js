10fbea9d8ce12cf7babd244576fc590208587c07
Teng Luo <Teng.Luo@feisystems.com>
2018-03-27 12:03:50 -0400

    Change the space between checkbox and label
    #18444:[Demo Feedback] Include checkboxes should be closer to the text next to it in all panels.

--- app/components/StatusCheckbox/index.js
+++ app/components/StatusCheckbox/index.js
@@ -4,20 +4,30 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import Checkbox from 'material-ui/Checkbox';
 import PropTypes from 'prop-types';
+import StyledCheckbox from 'components/StyledCheckbox';
+
+const styles = {
+  block: {
+    maxWidth: 250,
+  },
+  checkbox: {
+    marginBottom: 300,
+  },
+};
 
 function StatusCheckbox(props) {
   return (
     <span>
       <FormattedMessage {...props.messages} >
         {(msg) => (
-          <Checkbox
+          <StyledCheckbox
             label={msg}
             labelPosition="right"
             id={props.elementId}
             onCheck={props.handleCheck}
             checked={props.checked}
+            style={styles.checkbox}
           />
         )}
       </FormattedMessage>
