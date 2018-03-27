c556119cde6eb7b46b707476bb11d5069c0fe0e7
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-27 13:45:22 -0400

    Fix to do typo for ToDoCardContent
    18134 - As a user I should be able to add items to my To Do list associated to a patient so I can follow up on them

---
+++ app/components/ToDoCardContent/ToDoItemDescriptionBoxModel.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * To Do Item Description Box Model
+ *
+ */
+import styled from 'styled-components';
+
+const ToDoItemDescriptionBoxModel = styled.div`
+  background-color: rgb(238, 238, 238);
+  width: 100%;
+  border: 0 solid;
+  border-radius: 5px;
+  padding: 5px;
+  margin-top: 10px;
+`;
+
+ToDoItemDescriptionBoxModel.propTypes = {};
+
+export default ToDoItemDescriptionBoxModel;
