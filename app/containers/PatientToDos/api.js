8b9d3402f0c42983fbe17db302cdb561d81a92ba
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-28 14:51:23 -0400

    Implement api, reducer and render for patientToDos
    #18850 - Practitioner Workspace: "My Dashboard" retains previously selected patient context - To Do Panel

---
+++ app/containers/PatientToDos/api.js
@@ -0,0 +1,24 @@
+import queryString from 'query-string';
+import request from 'utils/request';
+import { BASE_TASKS_API_URL, getEndpoint } from 'utils/endpointService';
+
+const baseEndpoint = getEndpoint(BASE_TASKS_API_URL);
+
+export function getPatientToDos(patientId, practitionerId, definition) {
+  let queryParams = '';
+  if (patientId && !practitionerId) {
+    queryParams = { patientId, definition };
+  } else if (patientId && practitionerId) {
+    queryParams = { patientId, practitionerId, definition };
+  }
+  const stringifiedParams = queryString.stringify(queryParams);
+  const url = `${baseEndpoint}/subtasks?${stringifiedParams}`;
+  return request(url);
+}
+
+export function getToDoMainTask(patientId, definition) {
+  const queryParams = { patient: patientId, definition };
+  const stringifiedParams = queryString.stringify(queryParams);
+  const url = `${baseEndpoint}/task-references?${stringifiedParams}`;
+  return request(url);
+}
--- app/containers/PatientToDos/reducer.js
+++ app/containers/PatientToDos/reducer.js
@@ -6,15 +6,41 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
-} from './constants';
+ GET_PATIENT_TO_DO_MAIN_TASK_ERROR,
+  GET_PATIENT_TO_DO_MAIN_TASK_SUCCESS,
+  GET_PATIENT_TO_DOS,
+  GET_PATIENT_TO_DOS_ERROR,
+ GET_PATIENT_TO_DOS_SUCCESS,
+} from 'containers/PatientToDos/constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  data: [],
+  toDoMainTask: [],
+  loading: false,
+});
 
 function patientToDosReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case GET_PATIENT_TO_DOS:
+      return state.set('loading', true);
+    case GET_PATIENT_TO_DOS_SUCCESS:
+      return state
+        .set('error', false)
+        .set('loading', false)
+        .set('data', fromJS((action.toDos) || []));
+    case GET_PATIENT_TO_DO_MAIN_TASK_SUCCESS:
+      return state
+        .set('error', false)
+        .set('loading', false)
+        .set('toDoMainTask', fromJS((action.toDoMainTask) || []));
+    case GET_PATIENT_TO_DO_MAIN_TASK_ERROR:
+      return state
+        .set('error', true)
+        .set('loading', false);
+    case GET_PATIENT_TO_DOS_ERROR:
+      return state
+        .set('error', true)
+        .set('loading', false);
     default:
       return state;
   }
