3c38a8afb1a508e8245b445e39ddc74970056499
sadhch <sadhana.chandravanka@feisystems.com>
2018-03-29 16:04:11 -0400

    Remove the test code
    18799 - As a patient I should be able to create a general designation consent so I can let my Care team members have access to my health record

--- app/containers/ManageConsentPage/tests/selectors.test.js
+++ app/containers/ManageConsentPage/tests/selectors.test.js
@@ -5,8 +5,5 @@ describe('selectManageConsentPageDomain', () => {
   it('Expect to have unit tests specified', () => {
     // FIXME: add unit tests
     // expect(true).toEqual(false);
-    const { x, y } = [3, 2];
-    expect(x).toBe(3);
-    expect(y).toBe(3);
   });
 });
