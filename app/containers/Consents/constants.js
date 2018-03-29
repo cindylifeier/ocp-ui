13b0a16315068be0a4731fe41f114acd60b0cb04
CindyLiFei <cindy.li@feisystems.com>
2018-03-29 11:56:16 -0400

    Initialize consents containers component
    #18799 - As a patient I should be able to create a general designation consent so I can let my Care team members have access to my health record

---
+++ app/containers/Consents/constants.js
@@ -0,0 +1,17 @@
+/*
+ *
+ * Consents constants
+ *
+ */
+
+/**
+ *  Consents action types
+ *  @type {string}
+ */
+export const DEFAULT_ACTION = 'ocpui/Consents/DEFAULT_ACTION';
+
+
+/**
+*  Other constants
+*
+*/
