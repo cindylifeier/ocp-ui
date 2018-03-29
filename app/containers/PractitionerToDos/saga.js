573345ace6716fdedc0c62589549701734125f16
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-29 08:58:59 -0400

    Implement get to do saga
    #18850 - Practitioner Workspace: "My Dashboard" retains previously selected patient context - To Do Panel

--- app/containers/PractitionerToDos/saga.js
+++ app/containers/PractitionerToDos/saga.js
@@ -1,6 +1,32 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { GET_PRACTITIONER_TO_DOS } from 'containers/PractitionerToDos/constants';
+import {
+  getPractitionerToDoError,
+  getPractitionerToDoSuccess,
+} from 'containers/PractitionerToDos/actions';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { showNotification } from 'containers/Notification/actions';
+import { getPractitionerToDos } from './api';
+import messages from './messages';
 
-// Individual exports for testing
-export default function* defaultSaga() {
-  // See example in containers/HomePage/saga.js
+export function* getPractitionerToDosSaga(action) {
+  try {
+    const toDos = yield call(getPractitionerToDos, action.practitionerId, action.definition);
+    yield put(getPractitionerToDoSuccess(toDos));
+  } catch (error) {
+    yield put(showNotification(<FormattedMessage {...messages.noToDoError} />));
+    yield put(getPractitionerToDoError(error));
+  }
+}
+
+
+export function* watchGetPractitionerToDosSaga() {
+  yield takeLatest(GET_PRACTITIONER_TO_DOS, getPractitionerToDosSaga);
+}
+
+export default function* rootSaga() {
+  yield all([
+    watchGetPractitionerToDosSaga(),
+  ]);
 }
