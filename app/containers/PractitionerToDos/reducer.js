1d0491f86bdc4d385ff51394a2d3daff48187bcd
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-03-29 08:59:35 -0400

    Implement get to do reducer and api
    #18850 - Practitioner Workspace: "My Dashboard" retains previously selected patient context - To Do Panel

--- app/containers/PractitionerToDos/reducer.js
+++ app/containers/PractitionerToDos/reducer.js
@@ -6,15 +6,30 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
-} from './constants';
+  GET_PRACTITIONER_TO_DOS,
+  GET_PRACTITIONER_TO_DOS_ERROR,
+  GET_PRACTITIONER_TO_DOS_SUCCESS,
+} from 'containers/PractitionerToDos/constants';
 
-const initialState = fromJS({});
+
+const initialState = fromJS({
+  data: [],
+  loading: false,
+});
 
 function practitionerToDosReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case GET_PRACTITIONER_TO_DOS:
+      return state.set('loading', true);
+    case GET_PRACTITIONER_TO_DOS_SUCCESS:
+      return state
+        .set('error', false)
+        .set('loading', false)
+        .set('data', fromJS((action.toDos) || []));
+    case GET_PRACTITIONER_TO_DOS_ERROR:
+      return state
+        .set('error', true)
+        .set('loading', false);
     default:
       return state;
   }
