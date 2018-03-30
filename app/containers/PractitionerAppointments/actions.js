41773fbba0156ea94bb2da43964361b62a2b649b
Shruti Rao <shruti.rao@feisystems.com>
2018-03-30 15:05:20 -0400

    Add action.js
    #18849:Practitioner Workspace: "My Dashboard" retains previously selected patient context - Appointments Panel

--- app/containers/PractitionerAppointments/actions.js
+++ app/containers/PractitionerAppointments/actions.js
@@ -4,12 +4,62 @@
  *
  */
 
+
 import {
-  DEFAULT_ACTION,
+  CANCEL_PRACTITIONER_APPOINTMENT,
+  CANCEL_PRACTITIONER_APPOINTMENT_ERROR,
+  CANCEL_PRACTITIONER_APPOINTMENT_SUCCESS,
+  GET_PRACTITIONER_APPOINTMENTS,
+  GET_PRACTITIONER_APPOINTMENTS_ERROR,
+  GET_PRACTITIONER_APPOINTMENTS_SUCCESS,
+  INITIALIZE_PRACTITIONER_APPOINTMENTS,
 } from './constants';
 
-export function defaultAction() {
+export function initializePractitionerAppointments() {
   return {
-    type: DEFAULT_ACTION,
+    type: INITIALIZE_PRACTITIONER_APPOINTMENTS,
+  };
+}
+
+export function getPractitionerAppointments(query) {
+  return {
+    type: GET_PRACTITIONER_APPOINTMENTS,
+    query,
+  };
+}
+
+export function getPractitionerAppointmentsSuccess(upcomingAppointmentsPage) {
+  return {
+    type: GET_PRACTITIONER_APPOINTMENTS_SUCCESS,
+    upcomingAppointmentsPage,
+  };
+}
+
+export function getPractitionerAppointmentsError(error) {
+  return {
+    type: GET_PRACTITIONER_APPOINTMENTS_ERROR,
+    error,
+  };
+}
+
+
+export function cancelPractitionerAppointment(id) {
+  return {
+    type: CANCEL_PRACTITIONER_APPOINTMENT,
+    id,
+  };
+}
+
+export function cancelPractitionerAppointmentSuccess(id) {
+  return {
+    type: CANCEL_PRACTITIONER_APPOINTMENT_SUCCESS,
+    id,
+  };
+}
+
+export function cancelPractitionerAppointmentError(error) {
+  return {
+    type: CANCEL_PRACTITIONER_APPOINTMENT_ERROR,
+    error,
   };
 }
