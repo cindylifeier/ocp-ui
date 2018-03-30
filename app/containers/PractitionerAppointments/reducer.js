733cf86214efae18f091c50f9d930534503641eb
Shruti Rao <shruti.rao@feisystems.com>
2018-03-30 15:43:36 -0400

    Add reducer.js
    #18849:Practitioner Workspace: "My Dashboard" retains previously selected patient context - Appointments Panel

--- app/containers/PractitionerAppointments/reducer.js
+++ app/containers/PractitionerAppointments/reducer.js
@@ -4,17 +4,42 @@
  *
  */
 
-import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
-} from './constants';
+  CANCEL_PRACTITIONER_APPOINTMENT_SUCCESS,
+  DATA,
+  GET_PRACTITIONER_APPOINTMENTS,
+  GET_PRACTITIONER_APPOINTMENTS_ERROR,
+  GET_PRACTITIONER_APPOINTMENTS_SUCCESS,
+  LOADING,
+  STATUS_CODE_CANCELLED,
+} from 'containers/PractitionerAppointments/constants';
+import { fromJS } from 'immutable';
+import find from 'lodash/find';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  loading: false,
+  query: null,
+  showPastAppointments: false,
+});
 
 function practitionerAppointmentsReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case GET_PRACTITIONER_APPOINTMENTS:
+      return state
+        .set(LOADING, true)
+        .set('showPastAppointments', action.query.showPastAppointments)
+        .set(DATA, null);
+    case GET_PRACTITIONER_APPOINTMENTS_SUCCESS:
+      return state
+        .set(LOADING, false)
+        .set(DATA, fromJS(action.upcomingAppointmentsPage || {}));
+    case GET_PRACTITIONER_APPOINTMENTS_ERROR:
+      return state.set(LOADING, false);
+    case CANCEL_PRACTITIONER_APPOINTMENT_SUCCESS: {
+      const data = state.get(DATA).toJS();
+      find(data.elements, { logicalId: action.id }).statusCode = STATUS_CODE_CANCELLED;
+      return state.set(DATA, fromJS(data));
+    }
     default:
       return state;
   }
