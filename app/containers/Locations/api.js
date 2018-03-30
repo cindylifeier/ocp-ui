93ebf015c7b69d74e3ab854a9fef56eccd6534d9
Teng Luo <Teng.Luo@feisystems.com>
2018-03-30 18:55:17 -0400

    Add search location API
    #18328: Filter location and HCS search results by name

--- app/containers/Locations/api.js
+++ app/containers/Locations/api.js
@@ -16,6 +16,17 @@ export function searchLocationsByIdAndStatus(organizationId, status, currentPage
   return request(url);
 }
 
+export function searchLocations(organizationId, searchValue, includeInactive, searchType, currentPage) {
+  const params = queryString({
+    searchKey: searchType,
+    searchValue,
+    pageSize: DEFAULT_PAGE_SIZE,
+    pageNumber: currentPage,
+  });
+  const url = `${baseEndpoint}/${organizationId}/locations/${params}`;
+  return request(url);
+}
+
 function createUrl(organizationId, statusList, pageNumber) {
   const params = { pageNumber, statusList, pageSize: DEFAULT_PAGE_SIZE };
   const queryParams = queryString(params);
