2f83b7bdd4744beeb75f2982b615a54665f95bf6
Shruti Rao <shruti.rao@feisystems.com>
2018-03-30 15:21:53 -0400

    Add messages.js
    #18849:Practitioner Workspace: "My Dashboard" retains previously selected patient context - Appointments Panel

--- app/containers/PractitionerAppointments/messages.js
+++ app/containers/PractitionerAppointments/messages.js
@@ -8,6 +8,18 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   header: {
     id: 'ocpui.containers.PractitionerAppointments.header',
-    defaultMessage: 'This is PractitionerAppointments container !',
+    defaultMessage: 'Upcoming Appointments',
+  },
+  noUpcomingAppointments: {
+    id: 'ocpui.containers.PractitionerAppointments.noUpcomingAppointments',
+    defaultMessage: 'No Appointments loaded.',
+  },
+  buttonLabelCreateNew: {
+    id: 'ocpui.containers.PractitionerAppointments.buttonLabelCreateNew',
+    defaultMessage: 'Create New',
+  },
+  showPastAppointments: {
+    id: 'ocpui.containers.PractitionerAppointments.checkbox.showPastAppointments',
+    defaultMessage: 'Show Past Appointments',
   },
 });
