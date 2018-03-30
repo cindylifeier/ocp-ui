4c69243f71c7c34c20cd1cbfaa866e1b3a682558
Shruti Rao <shruti.rao@feisystems.com>
2018-03-30 14:56:45 -0400

    Add constants
    #18849:Practitioner Workspace: "My Dashboard" retains previously selected patient context - Appointments Panel

--- app/containers/PractitionerAppointments/constants.js
+++ app/containers/PractitionerAppointments/constants.js
@@ -9,9 +9,18 @@
  *  @type {string}
  */
 export const DEFAULT_ACTION = 'ocpui/PractitionerAppointments/DEFAULT_ACTION';
-
-
+export const INITIALIZE_PRACTITIONER_APPOINTMENTS = 'ocpui/PractitionerAppointments/INITIALIZE_PRACTITIONER_APPOINTMENTS';
+export const GET_PRACTITIONER_APPOINTMENTS = 'ocpui/PractitionerAppointments/GET_PRACTITIONER_APPOINTMENTS';
+export const GET_PRACTITIONER_APPOINTMENTS_SUCCESS = 'ocpui/PractitionerAppointments/GET_PRACTITIONER_APPOINTMENTS_SUCCESS';
+export const GET_PRACTITIONER_APPOINTMENTS_ERROR = 'ocpui/PractitionerAppointments/GET_PRACTITIONERAPPOINTMENTS_ERROR';
+export const CANCEL_PRACTITIONER_APPOINTMENT = 'ocpui/PractitionerAppointments/CANCEL_PRACTITIONER_APPOINTMENT';
+export const CANCEL_PRACTITIONER_APPOINTMENT_SUCCESS = 'ocpui/PractitionerAppointments/CANCEL_PRACTITIONER_APPOINTMENT_SUCCESS';
+export const CANCEL_PRACTITIONER_APPOINTMENT_ERROR = 'ocpui/PractitionerAppointments/CANCEL_PRACTITIONER_APPOINTMENT_ERROR';
 /**
-*  Other constants
-*
-*/
+ *  Other constants
+ *
+ */
+export const LOADING = 'loading';
+export const DATA = 'data';
+export const STATUS_CODE_CANCELLED = 'cancelled';
+
