f73351ad9d5909dcf67dc5532f47eed32ef1ed00
CindyLiFei <cindy.li@feisystems.com>
2018-04-02 09:53:51 -0400

    Implement consent table to list patient consent
    #18799 - As a patient I should be able to create a general designation consent so I can let my Care team members have access to my health record

---
+++ app/components/ConsentTable/index.js
@@ -0,0 +1,128 @@
+/**
+*
+* ConsentTable
+*
+*/
+import React from 'react';
+// import styled from 'styled-components';
+
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+import MenuItem from 'material-ui/MenuItem';
+import isEmpty from 'lodash/isEmpty';
+
+import RecordsRange from 'components/RecordsRange';
+import NoResultsFoundText from 'components/NoResultsFoundText';
+import CenterAlign from 'components/Align/CenterAlign';
+import CenterAlignedUltimatePagination from 'components/CenterAlignedUltimatePagination';
+import RefreshIndicatorLoading from 'components/RefreshIndicatorLoading';
+import TableHeader from 'components/TableHeader';
+import Table from 'components/Table';
+import TableHeaderColumn from 'components/TableHeaderColumn';
+import TableRow from 'components/TableRow';
+import TableRowColumn from 'components/TableRowColumn';
+import NavigationStyledIconMenu from 'components/StyledIconMenu/NavigationStyledIconMenu';
+import messages from './messages';
+
+const tableColumns = 'repeat(4, 1fr) 50px';
+
+function ConsentTable(props) {
+  const { consentData, relativeTop } = props;
+  return (
+    <div>
+      {consentData.loading && <RefreshIndicatorLoading />}
+      {(!consentData.loading && consentData.data && consentData.data.length > 0
+          ? <div>
+            <Table>
+              <TableHeader columns={tableColumns} relativeTop={relativeTop}>
+                <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderFromActor} /></TableHeaderColumn>
+                <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderToActor} /></TableHeaderColumn>
+                <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderPeriod} /></TableHeaderColumn>
+                <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderStatus} /></TableHeaderColumn>
+              </TableHeader>
+              {!isEmpty(consentData.data) && consentData.data.map((consent) => {
+                const { logicalId, fromActor, toActor, status, period } = consent;
+                return (
+                  <TableRow
+                    columns={tableColumns}
+                    key={logicalId}
+                    role="button"
+                    tabIndex="0"
+                  >
+                    <TableRowColumn>{fromActor}</TableRowColumn>
+                    <TableRowColumn>{toActor}</TableRowColumn>
+                    <TableRowColumn>{period.start}-{period.end} </TableRowColumn>
+                    <TableRowColumn>{status.display}</TableRowColumn>
+                    <TableRowColumn>
+                      <NavigationStyledIconMenu>
+                        <MenuItem
+                          primaryText={<FormattedMessage {...messages.edit} />}
+                          containerElement={<Link to={`/ocp-ui/manage-consent/${logicalId}`} />}
+                        />
+                        <MenuItem
+                          primaryText={<FormattedMessage {...messages.remove} />}
+                        />
+                      </NavigationStyledIconMenu>
+                    </TableRowColumn>
+                  </TableRow>
+                );
+              })}
+            </Table>
+            {!!consentData && !!consentData.currentPage &&
+            <div>
+              <CenterAlignedUltimatePagination
+                currentPage={consentData.currentPage}
+                totalPages={consentData.totalNumberOfPages}
+                onChange={consentData.handlePageClick}
+              />
+              <RecordsRange
+                currentPage={consentData.currentPage}
+                totalPages={consentData.totalNumberOfPages}
+                totalElements={consentData.totalElements}
+                currentPageSize={consentData.currentPageSize}
+              />
+            </div>}
+          </div> :
+          (<CenterAlign>
+            <NoResultsFoundText>No consents found.</NoResultsFoundText>
+          </CenterAlign>)
+      )}
+    </div>
+  );
+}
+
+ConsentTable.propTypes = {
+  relativeTop: PropTypes.number.isRequired,
+  consentData: PropTypes.shape({
+    loading: PropTypes.bool.isRequired,
+    currentPage: PropTypes.number.isRequired,
+    totalNumberOfPages: PropTypes.number.isRequired,
+    currentPageSize: PropTypes.number,
+    totalElements: PropTypes.number,
+    handlePageClick: PropTypes.func.isRequired,
+    data: PropTypes.arrayOf(PropTypes.shape({
+      logicalId: PropTypes.string.isRequired,
+      identifiers: PropTypes.arrayOf(PropTypes.shape({
+        system: PropTypes.string,
+        oid: PropTypes.string,
+        value: PropTypes.string,
+        priority: PropTypes.number,
+        display: PropTypes.string,
+      })),
+      status: PropTypes.arrayOf(PropTypes.shape({
+        code: PropTypes.string,
+        display: PropTypes.string,
+      })),
+      fromActor: PropTypes.array,
+      toActor: PropTypes.array,
+      period: PropTypes.shape({
+        start: PropTypes.date,
+        end: PropTypes.date,
+      }),
+    })).isRequired,
+  }),
+
+};
+
+export default ConsentTable;
---
+++ app/components/ConsentTable/messages.js
@@ -0,0 +1,37 @@
+/*
+ * ConsentTable Messages
+ *
+ * This contains all the text for the ConsentTable component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  tableColumnHeaderFromActor: {
+    id: 'ocpui.components.ConsentTable.tableColumnHeader.FromActor',
+    defaultMessage: 'From',
+  },
+  tableColumnHeaderToActor: {
+    id: 'ocpui.components.ConsentTable.TableColumnHeader.ToActor',
+    defaultMessage: 'To',
+  },
+  tableColumnHeaderPeriod: {
+    id: 'ocpui.components.ConsentTable.tableColumnHeader.Period',
+    defaultMessage: 'Period',
+  },
+  tableColumnHeaderStatus: {
+    id: 'ocpui.components.ConsentTable.TableColumnHeader.Status',
+    defaultMessage: 'Status',
+  },
+  edit: {
+    id: 'ocpui.components.ConsentTable.edit',
+    defaultMessage: 'Edit',
+  },
+  remove: {
+    id: 'ocpui.components.ConsentTable.remove',
+    defaultMessage: 'Remove',
+  },
+  noResultFound: {
+    id: 'ocpui.components.ConsentTable.noResultFound',
+    defaultMessage: 'No consents found',
+  },
+});
