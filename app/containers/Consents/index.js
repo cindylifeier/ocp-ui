8be18408e2b98517e9f5308d9818dac32afe67c0
CindyLiFei <cindy.li@feisystems.com>
2018-04-02 09:55:23 -0400

    Implement consents panel in patient workspace
    #18799 - As a patient I should be able to create a general designation consent so I can let my Care team members have access to my health record

--- app/containers/Consents/index.js
+++ app/containers/Consents/index.js
@@ -10,41 +10,129 @@ import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
+import isEqual from 'lodash/isEqual';
 
-import { MANAGE_CONSENT_URL } from 'containers/App/constants';
+import { DEFAULT_START_PAGE_NUMBER, MANAGE_CONSENT_URL } from 'containers/App/constants';
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
 import { PanelToolbar } from 'components/PanelToolbar';
+import Card from 'components/Card';
+import InfoSection from 'components/InfoSection';
+import ConsentTable from 'components/ConsentTable';
+import { makeSelectPatient } from 'containers/App/contextSelectors';
 import makeSelectConsents from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
+import { getConsents, initializeConsents } from './actions';
 
 export class Consents extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  static initalState = {
+    relativeTop: 0,
+    isShowSearchResult: false,
+    listConsents: {
+      currentPage: 1,
+    },
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...Consents.initalState,
+    };
+    this.handleListPageClick = this.handleListPageClick.bind(this);
+    this.onSize = this.onSize.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.getConsents({
+      pageNumber: DEFAULT_START_PAGE_NUMBER,
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { consent } = this.props;
+    const { consent: newConsent } = nextProps;
+    if (!isEqual(consent, newConsent)) {
+      this.props.initializeConsents([newConsent]);
+      this.setState({ ...Consents.initalState });
+    }
+  }
+
+  onSize(size) {
+    this.setState({ relativeTop: size.height });
+  }
+
+  handleListPageClick(currentPage) {
+    this.props.getConsents({ pageNumber: currentPage });
+  }
+
   render() {
+    const { consents } = this.props;
     const addNewItem = {
       labelName: <FormattedMessage {...messages.buttonLabelCreateNew} />,
       linkUrl: MANAGE_CONSENT_URL,
     };
+    const consentData = {
+      loading: consents.listConsents.loading,
+      data: consents.listConsents.data,
+      currentPage: consents.listConsents.currentPage,
+      totalNumberOfPages: consents.listConsents.totalNumberOfPages,
+      currentPageSize: consents.listConsents.currentPageSize,
+      totalElements: consents.listConsents.totalElements,
+      handlePageClick: this.handleListPageClick,
+    };
     return (
-      <div>
-        <PanelToolbar addNewItem={addNewItem} onSearch={this.handleSearch} showFilter={false} />
-      </div>
+      <Card>
+        <PanelToolbar
+          addNewItem={addNewItem}
+          showSearchIcon={false}
+          showFilterIcon={false}
+          showUploadIcon={false}
+          showSettingIcon={false}
+        />
+        <InfoSection margin="0 0 10px 0">
+          <ConsentTable
+            relativeTop={this.state.relativeTop}
+            consentData={consentData}
+          />
+        </InfoSection>
+      </Card>
     );
   }
 }
 
 Consents.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  initializeConsents: PropTypes.func.isRequired,
+  consent: PropTypes.object,
+  getConsents: PropTypes.func.isRequired,
+  consents: PropTypes.shape({
+    listConsents: PropTypes.shape({
+      loading: PropTypes.bool.isRequired,
+      currentPage: PropTypes.number.isRequired,
+      totalNumberOfPages: PropTypes.number.isRequired,
+      currentPageSize: PropTypes.number,
+      totalElements: PropTypes.number,
+      data: PropTypes.array,
+      error: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object,
+        PropTypes.bool,
+      ]),
+    }),
+  }),
 };
 
 const mapStateToProps = createStructuredSelector({
   consents: makeSelectConsents(),
+  patient: makeSelectPatient(),
 });
 
 function mapDispatchToProps(dispatch) {
   return {
-    dispatch,
+    initializeConsents: (consents) => dispatch(initializeConsents(consents)),
+    getConsents: (query) => dispatch(getConsents(query)),
   };
 }
 
--- app/containers/Consents/reducer.js
+++ app/containers/Consents/reducer.js
@@ -5,16 +5,44 @@
  */
 
 import { fromJS } from 'immutable';
-import {
-  DEFAULT_ACTION,
-} from './constants';
+import isEmpty from 'lodash/isEmpty';
 
-const initialState = fromJS({});
+import { INITIALIZE_CONSENTS, GET_CONSENTS, GET_CONSENTS_SUCCESS, GET_CONSENTS_ERROR } from './constants';
+
+const initialState = fromJS({
+  listConsents: {
+    loading: false,
+    data: [],
+    currentPage: 0,
+    totalNumberOfPages: 0,
+  },
+});
 
 function consentsReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case INITIALIZE_CONSENTS: {
+      if (!isEmpty(action.consents)) {
+        return initialState
+          .setIn(['listConsents', 'data'], fromJS(action.consents));
+      }
+      return initialState;
+    }
+    case GET_CONSENTS:
+      return state
+        .setIn(['listConsents', 'loading'], true);
+    case GET_CONSENTS_SUCCESS:
+      return state
+        .setIn(['listConsents', 'loading'], false)
+        .setIn(['listConsents', 'data'], fromJS(action.consents.elements))
+        .setIn(['listConsents', 'totalNumberOfPages'], action.consents.totalNumberOfPages)
+        .setIn(['listConsents', 'totalElements'], action.consents.totalElements)
+        .setIn(['listConsents', 'currentPageSize'], action.consents.currentPageSize)
+        .setIn(['listConsents', 'currentPage'], action.consents.currentPage);
+    case GET_CONSENTS_ERROR:
+      return state
+        .setIn(['listConsents', 'loading'], false)
+        .setIn(['listConsents', 'data'], fromJS([]))
+        .setIn(['listConsents', 'error'], action.error);
     default:
       return state;
   }
