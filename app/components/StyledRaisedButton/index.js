8f6736ec0479566cfcb6e3e2c47271c9f2ceb78b
JiahaoLi <Jiahao.Li@feisystems.com>
2018-04-09 14:26:44 -0400

    Change StyledRaisedButton to use material ui next button as base

--- app/components/StyledRaisedButton/index.js
+++ app/components/StyledRaisedButton/index.js
@@ -4,18 +4,29 @@
  *
  */
 
-import { RaisedButton } from 'material-ui';
 import styled from 'styled-components';
-import { teal500, white } from 'material-ui/styles/colors';
+import Button from 'material-ui-next/Button';
+import common from 'material-ui-next/colors/common';
+import teal from 'material-ui-next/colors/teal';
 
-const StyledRaisedButton = styled(RaisedButton).attrs({
-  backgroundColor: teal500,
-  labelColor: white,
-  style: {
-    fontSize: '12px',
-    fontWeight: 'bold',
-    width: '150px',
-  },
-})('');
+const StyledRaisedButton = styled(Button).attrs({
+  variant: 'raised',
+})`
+  && {
+    background-color: ${teal['500']};
+    color: ${common.white};
+    font-size: 13px;
+    font-weight: bold;
+    width: ${(props) => props.width || '150px'};
+  }
+
+  &&:hover {
+    background-color: ${teal['700']};
+  }
+
+  &&:disabled {
+    color: rgba(0, 0, 0, 0.3);
+  }
+`;
 
 export default StyledRaisedButton;
