92517c1fd40ac182accad4eb3c744efdb9f3e1f9
JiahaoLi <Jiahao.Li@feisystems.com>
2018-04-12 14:44:32 -0400

    Refactor StyledIconButton to use material ui next IconButton as base
    #19421: As a developer, I will introduce material-ui v1 to co-exist with material-ui v0.19.4, so that I can start to use the new UI components

--- app/components/StyledIconButton/tests/index.test.js
+++ app/components/StyledIconButton/tests/index.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { configure, mount, shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 import 'jest-styled-components';
-import { MuiThemeProvider } from 'material-ui';
 import NavigationMenu from '@material-ui/icons/Menu';
 
 import StyledIconButton from '../index';
@@ -17,54 +16,6 @@ describe('<StyledIconButton />', () => {
 
       // Assert
       expect(renderedComponent).toMatchSnapshot();
-    });
-  });
-
-  describe('style tests', () => {
-    it('should have material-ui props for styling', () => {
-      // Arrange
-      const expectedProps = {
-        style: { position: 'relative' },
-        iconStyle: {
-          width: '100%',
-          height: 26,
-          position: 'absolute',
-          top: '0',
-          right: '0',
-        },
-      };
-
-      // Act
-      const renderedComponent = shallow(<StyledIconButton><NavigationMenu /></StyledIconButton>);
-
-      // Assert
-      expect(renderedComponent.props().style).toEqual(expectedProps.style);
-      expect(renderedComponent.props().iconStyle).toEqual(expectedProps.iconStyle);
-    });
-
-    it('should have styles from styled-components for svg descendant', () => {
-      // Act
-      const renderedComponent = mount(
-        <MuiThemeProvider><StyledIconButton><NavigationMenu /></StyledIconButton></MuiThemeProvider>);
-
-      // Assert
-      expect(renderedComponent.find(StyledIconButton)).toHaveStyleRule('fill', 'rgb(51, 51, 51) !important', {
-        modifier: ' svg',
-      });
-      expect(renderedComponent.find(StyledIconButton)).toHaveStyleRule('transition', 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms', {
-        modifier: ' svg',
-      });
-    });
-
-    it('should have styles from styled-components for svg descendant in hover state', () => {
-      // Act
-      const renderedComponent = mount(
-        <MuiThemeProvider><StyledIconButton><NavigationMenu /></StyledIconButton></MuiThemeProvider>);
-
-      // Assert
-      expect(renderedComponent.find(StyledIconButton)).toHaveStyleRule('fill', '#26a69a !important', {
-        modifier: ':hover svg',
-      });
     });
   });
 });
