a43bede70943edbc68a39ff0920d15b52e9032b8
JiahaoLi <Jiahao.Li@feisystems.com>
2018-04-12 21:33:52 -0400

    Apply NavigationIconMenu in RelatedPersonTable component
    #19421: As a developer, I will introduce material-ui v1 to co-exist with material-ui v0.19.4, so that I can start to use the new UI components

--- app/components/RelatedPersonTable/index.js
+++ app/components/RelatedPersonTable/index.js
@@ -8,15 +8,13 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import uniqueId from 'lodash/uniqueId';
-import { Link } from 'react-router-dom';
-import MenuItem from 'material-ui/MenuItem';
 
 import Table from 'components/Table';
 import TableHeader from 'components/TableHeader';
 import TableHeaderColumn from 'components/TableHeaderColumn';
 import TableRow from 'components/TableRow';
 import TableRowColumn from 'components/TableRowColumn';
-import NavigationStyledIconMenu from 'components/StyledIconMenu/NavigationStyledIconMenu';
+import NavigationIconMenu from 'components/NavigationIconMenu';
 import { MANAGE_RELATED_PERSON_URL } from 'containers/App/constants';
 import messages from './messages';
 import { RELATED_PERSON_TABLE_COLUMNS } from './constants';
@@ -30,30 +28,28 @@ function RelatedPersonTable({ relatedPersons, patientId }) {
         <TableHeaderColumn><FormattedMessage {...messages.columnHeaderStatus} /></TableHeaderColumn>
         <TableHeaderColumn />
       </TableHeader>
-      {relatedPersons && relatedPersons.map((relatedPerson) => (
-        <TableRow key={uniqueId()} columns={RELATED_PERSON_TABLE_COLUMNS}>
-          <TableRowColumn>{relatedPerson.firstName} {relatedPerson.lastName}</TableRowColumn>
-          <TableRowColumn>{relatedPerson.relationshipValue}</TableRowColumn>
-          <TableRowColumn>{relatedPerson.active ?
-            <FormattedMessage {...messages.active} /> :
-            <FormattedMessage {...messages.inactive} />}
-          </TableRowColumn>
-          <TableRowColumn>
-            <NavigationStyledIconMenu>
-              <MenuItem
-                primaryText={<FormattedMessage {...messages.edit} />}
-                containerElement={<Link
-                  to={{
-                    pathname: `${MANAGE_RELATED_PERSON_URL}/${relatedPerson.relatedPersonId}`,
-                    search: `?patientId=${patientId}`,
-                  }}
-                />}
-              />
-            </NavigationStyledIconMenu>
-          </TableRowColumn>
-        </TableRow>
-      ))
-      }
+      {relatedPersons && relatedPersons.map((relatedPerson) => {
+        const menuItems = [{
+          primaryText: <FormattedMessage {...messages.edit} />,
+          linkTo: {
+            pathname: `${MANAGE_RELATED_PERSON_URL}/${relatedPerson.relatedPersonId}`,
+            search: `?patientId=${patientId}`,
+          },
+        }];
+        return (
+          <TableRow key={uniqueId()} columns={RELATED_PERSON_TABLE_COLUMNS}>
+            <TableRowColumn>{relatedPerson.firstName} {relatedPerson.lastName}</TableRowColumn>
+            <TableRowColumn>{relatedPerson.relationshipValue}</TableRowColumn>
+            <TableRowColumn>{relatedPerson.active ?
+              <FormattedMessage {...messages.active} /> :
+              <FormattedMessage {...messages.inactive} />}
+            </TableRowColumn>
+            <TableRowColumn>
+              <NavigationIconMenu menuItems={menuItems} />
+            </TableRowColumn>
+          </TableRow>
+        );
+      })}
     </Table>
   );
 }
