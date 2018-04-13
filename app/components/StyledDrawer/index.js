7ceb1241baa9b803df5e3fbcdaae8b3e9318e5bc
JiahaoLi <Jiahao.Li@feisystems.com>
2018-04-13 17:15:14 -0400

    Implement Styled component StyledDrawer
    #19420: Update the OCP UI pattern to match with the UX team design - All new UI components introduced.

---
+++ app/components/StyledDrawer/index.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * StyledDrawer
+ *
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Drawer from 'material-ui-next/Drawer';
+
+const StyledDrawer = styled(({ width, margin, ...other }) => (
+  <Drawer {...other} classes={{ paper: 'paper' }} />
+))`
+  & .paper {
+    width: ${(props) => props.width};
+    margin: ${(props) => props.margin};
+    border: 1px solid rgba(121, 121, 121, 1);
+    border-radius: 8px;
+  }
+`;
+
+StyledDrawer.propTypes = {
+  width: PropTypes.string,
+  margin: PropTypes.string,
+};
+
+StyledDrawer.defaultProps = {
+  width: '768px',
+  margin: '0px',
+};
+
+export default StyledDrawer;
