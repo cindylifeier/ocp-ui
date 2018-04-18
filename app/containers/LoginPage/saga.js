7321cb357f6154e2389b5524742a231837b30d80
CindyLiFei <cindy.li@feisystems.com>
2018-04-18 15:04:18 -0400

    Get user context by token
    #18449 - As a user I will have a Care Coordinator login so I can see the Care Coordinator view and functionality

--- app/containers/LoginPage/saga.js
+++ app/containers/LoginPage/saga.js
@@ -9,9 +9,8 @@ import { showNotification } from 'containers/Notification/actions';
 import { makeSelectLocation } from 'containers/App/selectors';
 import { setOrganization, setUser } from 'containers/App/contextActions';
 import { getLinkUrlByRole, getRoleByScope } from 'containers/App/helpers';
-import { getPractitioner } from 'containers/ManagePractitionerPage/api';
-import { getOrganization, getPatient } from 'containers/App/contextApi';
-import { getLoginErrorDetail, login } from './api';
+import { OCP_ADMIN_ROLE_CODE } from 'containers/App/constants';
+import { getLoginErrorDetail, getUserContext, login } from './api';
 import { loginError, loginSuccess } from './actions';
 import { LOGIN } from './constants';
 
@@ -19,7 +18,10 @@ function* loginSaga(loginAction) {
   try {
     const authData = yield call(login, loginAction.loginCredentials);
     const { user_id, user_name, email, scope, ext_attr } = yield call(jwt.decode, authData.access_token);
-    yield put(setUser({ user_id, user_name, email, scope, ext_attr }));
+    const roleScope = find(scope, (s) => s.startsWith('ocp.role'));
+    const userRole = yield call(getRoleByScope, roleScope);
+    yield put(setUser({ user_id, user_name, email, scope, ext_attr, role: userRole }));
+
     yield call(storeToken, authData);
     yield call(storeAuthStatus, true);
     const isAuthenticated = yield call(checkAuthenticated);
@@ -29,30 +31,18 @@ function* loginSaga(loginAction) {
     }
     yield put(loginSuccess(isAuthenticated));
     yield call(loginAction.handleSubmitting);
+
+    // Retreving user resource and organization details
+    if (userRole !== OCP_ADMIN_ROLE_CODE) {
+      const userContext = yield call(getUserContext);
+      const { resource, organization } = userContext;
+      yield put(setUser({ user_id, user_name, email, scope, ext_attr, resource, role: userRole }));
+      yield put(setOrganization(organization));
+    }
+
     // Redirect to referrer address
     const location = yield select(makeSelectLocation());
-    // get scope starts with 'ocp.role'
-    const roleScope = find(scope, (s) => s.startsWith('ocp.role'));
-    const userRole = yield call(getRoleByScope, roleScope);
     const linkUrl = yield call(getLinkUrlByRole, userRole);
-
-    // get user resource when resource id available
-    let resource;
-    if (ext_attr.resource === 'Practitioner') {
-      resource = yield call(getPractitioner, ext_attr.id);
-    }
-    if (ext_attr.resource === 'Patient') {
-      resource = yield call(getPatient, ext_attr.id);
-    }
-    yield put(setUser({ user_id, user_name, email, scope, ext_attr, resource, role: userRole }));
-
-    // get organization resource when user associate with organization
-    const organizationId = roleScope.replace(/\D+/g, '');
-    if (organizationId !== null && organizationId !== '') {
-      const organizationResource = yield call(getOrganization, organizationId);
-      yield put(setOrganization(organizationResource));
-    }
-
     const { from } = location.state || { from: { pathname: linkUrl } };
     yield put(push(from));
   } catch (error) {
