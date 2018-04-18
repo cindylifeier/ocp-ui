13733b098b8bc742bfb0400c690107353ec0ff41
Teng Luo <Teng.Luo@feisystems.com>
2018-04-18 15:06:24 -0400

    Modify AddNewItemButton
    #19527:Update the Calender Panel with Widget so there is no under construction message in it

--- app/components/PanelToolbar/AddNewItemButton.js
+++ app/components/PanelToolbar/AddNewItemButton.js
@@ -10,8 +10,14 @@ import FlatButton from 'material-ui/FlatButton';
 const AddNewItemButton = styled(FlatButton).attrs({
   labelStyle: {
     color: '#ffffff',
+    fontSize: '12px',
+    paddingLeft: '0',
   },
-})('');
+})`
+  && {
+    max-height: 32px;
+  }
+`;
 
 AddNewItemButton.propTypes = {};
 
