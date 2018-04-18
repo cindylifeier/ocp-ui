30858f381bcd1cd005bb82a9384326310f1392e9
sadhch <sadhana.chandravanka@feisystems.com>
2018-04-18 16:59:21 -0400

    pass organization id for participant search
    19417 - Fix UI issues associated with system generated Episode of Care creation on behalf of the organization when a new patient is created

--- app/containers/SearchParticipant/saga.js
+++ app/containers/SearchParticipant/saga.js
@@ -8,8 +8,8 @@ import { showNotification } from '../Notification/actions';
 
 export function* searchParticipantWorker(action) {
   try {
-    if (!isEmpty(action.name) && !isEmpty(action.member)) {
-      const participants = yield call(searchParticipant, action.name, action.member, action.patientId);
+    if (!isEmpty(action.member)) {
+      const participants = yield call(searchParticipant, action.name, action.member, action.patientId, action.organizationId);
       yield put(getSearchParticipantSuccess(participants));
     }
   } catch (error) {
