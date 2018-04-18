df489c3ec7ba858e6dc2fd5843086ce30d471140
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-04-18 08:53:07 -0400

    Use size me library to show or hide certain columns
    #19526 - Fixing all UI related issues reported during Analysise

--- app/components/TaskTable/index.js
+++ app/components/TaskTable/index.js
@@ -8,28 +8,82 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import isEmpty from 'lodash/isEmpty';
-
+import sizeMe from 'react-sizeme';
 import Table from 'components/Table';
 import TableHeader from 'components/TableHeader';
 import TableHeaderColumn from 'components/TableHeaderColumn';
 import TableRow from 'components/TableRow';
 import TableRowColumn from 'components/TableRowColumn';
 import NavigationIconMenu from 'components/NavigationIconMenu';
+import {
+  EXPANDED_TASK_TABLE_COLUMNS, minPanelSize,
+  SUMMARISED_TASK_TABLE_COLUMNS,
+  STATUS_CODE_CANCELLED,
+} from 'components/TaskTable/constants';
 import messages from './messages';
-import { STATUS_CODE_CANCELLED, TASK_TABLE_COLUMNS } from './constants';
 
-function TaskTable({ elements, cancelTask, patientId, communicationBaseUrl, taskBaseUrl, relativeTop }) {
+
+function TaskTable({ elements, cancelTask, patientId, communicationBaseUrl, taskBaseUrl, relativeTop, size }) {
+  const isExpanded = size && size.width && (Math.floor(size.width) > minPanelSize);
+
+  function createTableHeaders() {
+    return isExpanded ?
+      (
+        <TableHeader columns={EXPANDED_TASK_TABLE_COLUMNS} relativeTop={relativeTop}>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderActivityType} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderStatus} /></TableHeaderColumn>
+          <TableHeaderColumn> <FormattedMessage {...messages.columnHeaderDescription} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderCreatedOn} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderTaskPeriod} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderCreatedBy} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderTaskOwner} /></TableHeaderColumn>
+          <TableHeaderColumn></TableHeaderColumn>
+        </TableHeader>
+      )
+      :
+      (
+        <TableHeader columns={SUMMARISED_TASK_TABLE_COLUMNS} relativeTop={relativeTop}>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderActivityType} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderCreatedBy} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.columnHeaderTaskOwner} /></TableHeaderColumn>
+          <TableHeaderColumn></TableHeaderColumn>
+        </TableHeader>
+      );
+  }
+
+  function createTableRows(logicalId, definition, status, description, authoredOn, executionPeriod, agent, owner, menuItems) {
+    return isExpanded ?
+      (
+        <TableRow key={logicalId} columns={EXPANDED_TASK_TABLE_COLUMNS}>
+          <TableRowColumn>{definition && definition.display}</TableRowColumn>
+          <TableRowColumn>{status && status.display}</TableRowColumn>
+          <TableRowColumn>{description}</TableRowColumn>
+          <TableRowColumn>{authoredOn}</TableRowColumn>
+          <TableRowColumn>{executionPeriod && executionPeriod.start}
+            - {executionPeriod && executionPeriod.end} </TableRowColumn>
+          <TableRowColumn>{agent && agent.display} </TableRowColumn>
+          <TableRowColumn>{owner && owner.display} </TableRowColumn>
+          <TableRowColumn>
+            <NavigationIconMenu menuItems={menuItems} />
+          </TableRowColumn>
+        </TableRow>
+      )
+      :
+      (
+        <TableRow key={logicalId} columns={SUMMARISED_TASK_TABLE_COLUMNS}>
+          <TableRowColumn>{definition && definition.display}</TableRowColumn>
+          <TableRowColumn>{agent && agent.display} </TableRowColumn>
+          <TableRowColumn>{owner && owner.display} </TableRowColumn>
+          <TableRowColumn>
+            <NavigationIconMenu menuItems={menuItems} />
+          </TableRowColumn>
+        </TableRow>
+      );
+  }
+
   return (
     <Table>
-      <TableHeader columns={TASK_TABLE_COLUMNS} relativeTop={relativeTop}>
-        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderActivityType} /></TableHeaderColumn>
-        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderStatus} /></TableHeaderColumn>
-        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderDescription} /></TableHeaderColumn>
-        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderCreatedOn} /></TableHeaderColumn>
-        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderTaskPeriod} /></TableHeaderColumn>
-        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderCreatedBy} /></TableHeaderColumn>
-        <TableHeaderColumn><FormattedMessage {...messages.columnHeaderTaskOwner} /></TableHeaderColumn>
-      </TableHeader>
+      {createTableHeaders()}
       {!isEmpty(elements) && elements.map(({ logicalId, definition, status, description, authoredOn, executionPeriod, agent, owner }) => {
         const menuItems = [{
           primaryText: <FormattedMessage {...messages.editTask} />,
@@ -54,20 +108,7 @@ function TaskTable({ elements, cancelTask, patientId, communicationBaseUrl, task
           disabled: status.code === STATUS_CODE_CANCELLED,
           onClick: () => cancelTask(logicalId),
         }];
-        return (
-          <TableRow key={logicalId} columns={TASK_TABLE_COLUMNS}>
-            <TableRowColumn>{definition && definition.display}</TableRowColumn>
-            <TableRowColumn>{status && status.display}</TableRowColumn>
-            <TableRowColumn>{description}</TableRowColumn>
-            <TableRowColumn>{authoredOn}</TableRowColumn>
-            <TableRowColumn>{executionPeriod && executionPeriod.start} - {executionPeriod && executionPeriod.end} </TableRowColumn>
-            <TableRowColumn>{agent && agent.display} </TableRowColumn>
-            <TableRowColumn>{owner && owner.display} </TableRowColumn>
-            <TableRowColumn>
-              <NavigationIconMenu menuItems={menuItems} />
-            </TableRowColumn>
-          </TableRow>
-        );
+        return createTableRows(logicalId, definition, status, description, authoredOn, executionPeriod, agent, owner, menuItems);
       })}
     </Table>
   );
@@ -79,6 +120,7 @@ TaskTable.propTypes = {
   patientId: PropTypes.string.isRequired,
   communicationBaseUrl: PropTypes.string.isRequired,
   taskBaseUrl: PropTypes.string.isRequired,
+  size: PropTypes.object.isRequired,
   elements: PropTypes.arrayOf(PropTypes.shape({
     logicalId: PropTypes.string.isRequired,
     definition: PropTypes.shape({
@@ -109,4 +151,15 @@ TaskTable.propTypes = {
   })),
 };
 
-export default TaskTable;
+// Create the config for SizeMe
+const config = {
+  monitorWidth: true,
+  monitorHeight: false,
+  refreshRate: 250,
+};
+
+// Call SizeMe with the config to get back the HOC.
+const sizeMeHOC = sizeMe(config);
+
+
+export default sizeMeHOC(TaskTable);
