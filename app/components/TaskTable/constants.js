004e34eb35c60d40fdabb7a22b0a4810a110d507
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-04-18 08:51:11 -0400

    Set a minimum panel size constant
    #19526 - Fixing all UI related issues reported during Analysise

--- app/components/TaskTable/constants.js
+++ app/components/TaskTable/constants.js
@@ -1,2 +1,6 @@
 export const TASK_TABLE_COLUMNS = 'repeat(7, 1fr) 50px';
+export const EXPANDED_TASK_TABLE_COLUMNS = 'repeat(7, 1fr) 50px';
+export const SUMMARISED_TASK_TABLE_COLUMNS = 'repeat(3, 1fr) 50px';
 export const STATUS_CODE_CANCELLED = 'cancelled';
+
+export const minPanelSize = 700;
