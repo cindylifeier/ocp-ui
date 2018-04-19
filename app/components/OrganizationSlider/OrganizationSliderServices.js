6aefe2c7949f93235e62622448ca9168fb82b065
JiahaoLi <Jiahao.Li@feisystems.com>
2018-04-19 14:32:20 -0400

    Apply ControlledAccordion component in OrganizationSliderServices
    #19420: Update the OCP UI pattern to match with the UX team design - All new UI components introduced.

--- app/components/OrganizationSlider/OrganizationSliderServices.js
+++ app/components/OrganizationSlider/OrganizationSliderServices.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import ExpansionPanel, { ExpansionPanelSummary } from 'material-ui-next/ExpansionPanel';
-import Divider from 'material-ui-next/Divider';
 import teal from 'material-ui-next/colors/teal';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+
 import HealthcareServices from 'containers/HealthcareServices';
+import ControlledAccordion from 'components/ControlledAccordion';
 import StyledIconButton from 'components/StyledIconButton';
 import StyledTooltip from 'components/StyledTooltip';
 import StyledText from 'components/StyledText';
@@ -16,49 +14,29 @@ import FullWidthPanelDetails from './FullWidthPanelDetails';
 import messages from './messages';
 
 
-class OrganizationSliderServices extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      expansionPanelOpen: false,
-    };
-    this.handlePanelOpen = this.handlePanelOpen.bind(this);
-  }
-
-  handlePanelOpen() {
-    this.setState({ expansionPanelOpen: !this.state.expansionPanelOpen });
-  }
-
-  render() {
-    return (
-      <div>
-        <ExpansionPanel expanded={this.state.expansionPanelOpen}>
-          <ExpansionPanelSummary
-            expandIcon={
-              <StyledTooltip title={<FormattedMessage {...messages.addNew} />}>
-                <StyledIconButton component={Link} to={'/ocp-ui/manage-healthcare-service'} disableIconHover>
-                  <AddCircleIcon color={teal['500']} />
-                </StyledIconButton>
-              </StyledTooltip>
-            }
-          >
-            {this.state.expansionPanelOpen ?
-              <ExpandLessIcon onClick={this.handlePanelOpen} /> :
-              <ExpandMoreIcon onClick={this.handlePanelOpen} />
-            }
-            <StyledText whiteSpace>
-              <FormattedMessage {...messages.servicesPanel.panelSummary} />
-            </StyledText>
-          </ExpansionPanelSummary>
-          <Divider light />
-          <FullWidthPanelDetails>
-            <HealthcareServices />
-          </FullWidthPanelDetails>
-        </ExpansionPanel>
-      </div>
-    );
-  }
+function OrganizationSliderServices() {
+  return (
+    <div>
+      <ControlledAccordion
+        accordionTitle={
+          <StyledText whiteSpace>
+            <FormattedMessage {...messages.servicesPanel.panelSummary} />
+          </StyledText>
+        }
+        expandIcon={
+          <StyledTooltip title={<FormattedMessage {...messages.addNew} />}>
+            <StyledIconButton component={Link} to={'/ocp-ui/manage-healthcare-service'} disableIconHover>
+              <AddCircleIcon color={teal['500']} />
+            </StyledIconButton>
+          </StyledTooltip>
+        }
+      >
+        <FullWidthPanelDetails>
+          <HealthcareServices />
+        </FullWidthPanelDetails>
+      </ControlledAccordion>
+    </div>
+  );
 }
 
 OrganizationSliderServices.propTypes = {
