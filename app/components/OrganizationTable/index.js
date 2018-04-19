3905d787493b5d3196fc8606f3ddc8d55625f5ed
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-04-19 13:16:37 -0400

    Show/hide columns based on width
    #19526 - Fixing all UI related issues reported during Analysis

--- app/components/OrganizationTable/index.js
+++ app/components/OrganizationTable/index.js
@@ -9,6 +9,7 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import isEmpty from 'lodash/isEmpty';
 
+import sizeMeHOC from 'utils/SizeMeUtils';
 import RecordsRange from 'components/RecordsRange';
 import NoResultsFoundText from 'components/NoResultsFoundText';
 import CenterAlign from 'components/Align/CenterAlign';
@@ -21,24 +22,30 @@ import TableRow from 'components/TableRow';
 import TableRowColumn from 'components/TableRowColumn';
 import NavigationIconMenu from 'components/NavigationIconMenu';
 import messages from './messages';
+import { EXPANDED_TABLE_COLUMNS, SUMMARISED_TABLE_COLUMNS } from './constants';
 
-const tableColumns = 'repeat(5, 1fr) 50px';
 const ENTER_KEY = 'Enter';
 
 function OrganizationTable(props) {
-  const { organizationData, onRowClick, relativeTop } = props;
+  const { organizationData, onRowClick, relativeTop, isExpanded } = props;
+  const columns = isExpanded ? EXPANDED_TABLE_COLUMNS : SUMMARISED_TABLE_COLUMNS;
   return (
     <div>
       {organizationData.loading && <RefreshIndicatorLoading />}
       {(!organizationData.loading && organizationData.data && organizationData.data.length > 0
           ? <div>
             <Table>
-              <TableHeader columns={tableColumns} relativeTop={relativeTop}>
+              <TableHeader columns={columns} relativeTop={relativeTop}>
                 <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderOrganization} /></TableHeaderColumn>
+                {isExpanded &&
                 <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderAddress} /></TableHeaderColumn>
+                }
                 <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderTelecom} /></TableHeaderColumn>
+                {isExpanded &&
                 <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderId} /></TableHeaderColumn>
+                }
                 <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderStatus} /></TableHeaderColumn>
+                <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderAction} /></TableHeaderColumn>
               </TableHeader>
               {!isEmpty(organizationData.data) && organizationData.data.map((organization) => {
                 const flattenOrganization = props.flattenOrganizationData(organization);
@@ -61,7 +68,7 @@ function OrganizationTable(props) {
                 }];
                 return (
                   <TableRow
-                    columns={tableColumns}
+                    columns={columns}
                     key={logicalId}
                     onClick={() => onRowClick && onRowClick(organization)}
                     onKeyPress={(e) => {
@@ -76,9 +83,13 @@ function OrganizationTable(props) {
                     tabIndex="0"
                   >
                     <TableRowColumn>{name}</TableRowColumn>
+                    {isExpanded &&
                     <TableRowColumn>{addresses}</TableRowColumn>
+                    }
                     <TableRowColumn>{telecoms}</TableRowColumn>
+                    {isExpanded &&
                     <TableRowColumn>{identifiers}</TableRowColumn>
+                    }
                     <TableRowColumn>
                       {active ?
                         <FormattedMessage {...messages.active} /> :
@@ -153,6 +164,7 @@ OrganizationTable.propTypes = {
   }),
   onRowClick: PropTypes.func,
   flattenOrganizationData: PropTypes.func,
+  isExpanded: PropTypes.bool.isRequired,
 };
 
-export default OrganizationTable;
+export default sizeMeHOC(OrganizationTable);
