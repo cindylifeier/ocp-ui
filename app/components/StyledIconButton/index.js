b7f5bb698320382cc793f41532ed447b2c57e916
JiahaoLi <Jiahao.Li@feisystems.com>
2018-04-19 13:58:22 -0400

    Enhancement StyledIconButton to be sizable
    #19420: Update the OCP UI pattern to match with the UX team design - All new UI components introduced.

--- app/components/StyledIconButton/index.js
+++ app/components/StyledIconButton/index.js
@@ -3,20 +3,51 @@
  * StyledIconButton
  *
  */
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import IconButton from 'material-ui-next/IconButton';
 
-const StyledIconButton = styled(IconButton)`
+
+const sizes = ['small', 'medium', 'large'];
+
+function defineSvgIconSize(sizeProp) {
+  switch (sizeProp) {
+    case 'small':
+      return '18px';
+    case 'medium':
+      return '20px';
+    case 'large':
+      return '24px';
+    default:
+      return 'default';
+  }
+}
+
+const StyledIconButton = styled(({ size, disableIconHover, ...other }) => (
+  <IconButton {...other} />
+))`
+  && svg {
+    max-height: ${({ size }) => size && defineSvgIconSize(size)};
+  }
+
   &&:hover {
     background-color: inherit;
   }
 
   &&:hover svg {
-    background-color: #91bdba;
+    background-color: ${({ disableIconHover }) => disableIconHover ? 'default' : '#91bdba'};
     border-radius: 5px;
   }
 `;
 
-StyledIconButton.propTypes = {};
+StyledIconButton.propTypes = {
+  size: PropTypes.oneOf(sizes),
+  disableIconHover: PropTypes.bool,
+};
+
+StyledIconButton.defaultProps = {
+  disableIconHover: false,
+};
 
 export default StyledIconButton;
