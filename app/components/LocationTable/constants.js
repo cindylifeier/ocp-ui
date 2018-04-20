26d60d86703baa95d0cc781e5ed6e6116e82d131
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-04-20 14:11:30 -0400

    Define constants for location table
    #19526 - Fixing all UI related issues reported during Analysis

---
+++ app/components/LocationTable/constants.js
@@ -0,0 +1,14 @@
+/*
+ *
+ * Locations constants
+ *
+ */
+/**
+ * LocationTable Constants
+ *
+ */
+
+export const EXPANDED_TABLE_COLUMNS = '3fr 3fr 3fr 1fr  50px';
+export const SUMMARISED_TABLE_COLUMNS = '3fr 3fr 1fr  50px';
+
+export const SUMMARY_VIEW_WIDTH = 700;
