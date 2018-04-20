6d1b2261b3bb03d15104fce07a9329151fc0f832
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-04-20 14:07:38 -0400

    Remove unnecessary code
    #19526 - Fixing all UI related issues reported during Analysis

--- app/containers/Locations/messages.js
+++ app/containers/Locations/messages.js
@@ -6,46 +6,6 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  locations: {
-    id: 'ocpui.containers.Locations.locations',
-    defaultMessage: 'locations',
-  },
-  filterLabel: {
-    id: 'ocpui.containers.Locations.filterLabel',
-    defaultMessage: 'Include:',
-  },
-  inactive: {
-    id: 'ocpui.containers.Locations.checkbox.inactive',
-    defaultMessage: 'Inactive',
-  },
-  suspended: {
-    id: 'ocpui.containers.Locations.checkbox.suspended',
-    defaultMessage: 'Suspended',
-  },
-  tableHeaderColumnName: {
-    id: 'ocpui.containers.Locations.tableHeaderColumnName',
-    defaultMessage: 'Name',
-  },
-  tableHeaderColumnStatus: {
-    id: 'ocpui.containers.Locations.tableHeaderColumnStatus',
-    defaultMessage: 'Status',
-  },
-  tableHeaderColumnTelecoms: {
-    id: 'ocpui.containers.Locations.tableHeaderColumnTelecoms',
-    defaultMessage: 'Telecoms',
-  },
-  tableHeaderColumnAddress: {
-    id: 'ocpui.containers.Locations.tableHeaderColumnAddress',
-    defaultMessage: 'Address',
-  },
-  actionLabelEdit: {
-    id: 'ocpui.containers.Locations.actionLabelEdit',
-    defaultMessage: 'Edit',
-  },
-  actionLabelAssignHealthCareService: {
-    id: 'ocpui.containers.Locations.actionLabelAssignHealthCareService',
-    defaultMessage: 'Assign HealthCareService',
-  },
   noLocationsFound: {
     id: 'ocpui.containers.Locations.noLocationsFound',
     defaultMessage: 'No locations loaded. Please select an organization to view its locations.',
