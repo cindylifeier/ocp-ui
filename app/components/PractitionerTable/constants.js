04872e69aa099ece34b7407844162c008a79cda2
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-04-30 15:15:45 -0400

    Adjust column to prevent test wrapping
    # 19700:As a Developer, I will show more content when panel is maximized

--- app/components/PractitionerTable/constants.js
+++ app/components/PractitionerTable/constants.js
@@ -1,4 +1,4 @@
-export const EXPANDED_TABLE_COLUMNS = 'repeat(2, .75fr) 1.5fr repeat(3, 1fr) 50px';
+export const EXPANDED_TABLE_COLUMNS = '1.5fr 3fr 1.5fr repeat(2, 1fr) 50px';
 export const SUMMARIZED_TABLE_COLUMNS = 'repeat(3, 1fr) 50px';
 
 export const SUMMARY_PANEL_WIDTH = 500;
