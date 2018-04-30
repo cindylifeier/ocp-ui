ab728f3a8bbea3a209460bfb2752fa9be359af6d
JiahaoLi <Jiahao.Li@feisystems.com>
2018-04-30 15:13:22 -0400

    Implement to apply ExpansionTableRow in location table
    #19678: As a Developer, I will create a quick view for all workspaces

--- app/components/LocationTable/constants.js
+++ app/components/LocationTable/constants.js
@@ -4,6 +4,6 @@
  *
  */
 
-export const EXPANDED_TABLE_COLUMNS = '3fr 3fr 3fr 1fr 50px';
-export const SUMMARIZED_TABLE_COLUMNS = '3fr 3fr 1fr 50px';
+export const EXPANDED_TABLE_COLUMNS = '50px 3fr 3fr 3fr 1fr 50px';
+export const SUMMARIZED_TABLE_COLUMNS = '50px 3fr 3fr 1fr 50px';
 export const SUMMARY_VIEW_WIDTH = 750; // The width of the panel in pixels
