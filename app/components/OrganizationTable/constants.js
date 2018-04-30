35742656dfc58193be015289d284423f1ab79ac0
JiahaoLi <Jiahao.Li@feisystems.com>
2018-04-30 11:12:05 -0400

    Add NavigationIconMenu in organization table
    #19678: As a Developer, I will create a quick view for all workspaces

--- app/components/OrganizationTable/constants.js
+++ app/components/OrganizationTable/constants.js
@@ -1,4 +1,4 @@
 
-export const EXPANDED_TABLE_COLUMNS = '50px 100px 1fr 60px 120px';
+export const EXPANDED_TABLE_COLUMNS = '50px repeat(4, 1fr)';
 export const SUMMARY_VIEW_WIDTH = 450; // The width of the panel in pixels
 export const ENTER_KEY = 'Enter';
