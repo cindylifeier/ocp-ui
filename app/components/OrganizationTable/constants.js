6a51a61690851dfc74e681c13e883363e7116f8f
JiahaoLi <Jiahao.Li@feisystems.com>
2018-04-30 11:28:12 -0400

    Modify Organization table column width
    #19678: As a Developer, I will create a quick view for all workspaces

--- app/components/OrganizationTable/constants.js
+++ app/components/OrganizationTable/constants.js
@@ -1,4 +1,2 @@
-
-export const EXPANDED_TABLE_COLUMNS = '50px repeat(4, 1fr)';
-export const SUMMARY_VIEW_WIDTH = 450; // The width of the panel in pixels
+export const EXPANDED_TABLE_COLUMNS = '50px repeat(2, 1fr) 70px  50px';
 export const ENTER_KEY = 'Enter';
