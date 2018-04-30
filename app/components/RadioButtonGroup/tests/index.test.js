7ff5fc48fce26c34d03f44a4fac1459704d37360
sadhch <sadhana.chandravanka@feisystems.com>
2018-04-30 14:55:29 -0400

    Create a formik bridge for RadioButtonGroup component
    19689 - As a Developer, I will Implement organization/individual consent functionality into OCP

--- app/components/RadioButtonGroup/tests/index.test.js
+++ app/components/RadioButtonGroup/tests/index.test.js
@@ -3,12 +3,12 @@
 // import Adapter from 'enzyme-adapter-react-15';
 // import 'jest-styled-components';
 
-// import RadioButton from '../index';
+// import RadioButtonGroup from '../index';
 
 // configure({ adapter: new Adapter() });
 
-describe('<RadioButton />', () => {
+describe('<RadioButtonGroup />', () => {
   it('Expect to have unit tests specified', () => {
-   // expect(true).toEqual(false);
+    expect(true).toEqual(false);
   });
 });
