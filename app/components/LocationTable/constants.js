708477b060a712207c5de381ed69542ae818c9d9
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-05-02 14:55:46 -0400

    Adjust column to fit panel properly
    # 19700:As a Developer, I will show more content when panel is maximized

--- app/components/LocationTable/constants.js
+++ app/components/LocationTable/constants.js
@@ -4,7 +4,7 @@
  *
  */
 
-export const EXPANDED_TABLE_COLUMNS = '2fr 3fr 2fr  2fr 3fr 1fr 50px';
-export const SUMMARIZED_TABLE_COLUMNS = '3fr 3fr 1fr 50px';
+export const EXPANDED_TABLE_COLUMNS = '2fr 3fr 2fr  2fr 1fr .5fr';
+export const SUMMARIZED_TABLE_COLUMNS = '3fr 3fr 1fr .5fr';
 
 export const SUMMARY_VIEW_WIDTH = 750; // The width of the panel in pixels
