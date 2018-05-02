3e48d81f10e451f12f75af1e06ce82d01f7a5dbd
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-05-02 13:54:14 -0400

    Fix the issue related to the width of the table code getting bigger than the panel width
    # 19700:As a Developer, I will show more content when panel is maximized

--- app/components/PatientSearchResult/constants.js
+++ app/components/PatientSearchResult/constants.js
@@ -1,6 +1,5 @@
 
-export const EXPANDED_TABLE_COLUMNS = '50px 1.5fr 2fr 4fr 1fr 2fr 1fr 1fr 20% 1fr 50px';
-export const SUMMARIZED_TABLE_COLUMNS = '50px 2fr 2fr 1fr 1fr 50px';
-
+export const EXPANDED_TABLE_COLUMNS = '.5fr 1.5fr 2fr 4fr 1fr 2fr 1fr 1fr 1fr 1fr .5fr';
+export const SUMMARIZED_TABLE_COLUMNS = '.5fr 2fr 2fr 1fr 1fr .5fr';
 
 export const SUMMARY_VIEW_WIDTH = 750; // The width of the panel in pixels
