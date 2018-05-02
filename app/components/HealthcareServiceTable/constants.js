6a266cb7f0f1ee723680ea75425b5aea1b180acc
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-05-01 22:55:54 -0400

    Add columns configuration for expanded and summarized view
    # 19700:As a Developer, I will show more content when panel is maximized

---
+++ app/components/HealthcareServiceTable/constants.js
@@ -0,0 +1,10 @@
+/*
+ *
+ * HealthcareServiceTable  constants
+ *
+ */
+
+export const EXPANDED_TABLE_COLUMNS = '2fr 2fr 3fr 3fr 2fr 2fr 50px';
+export const SUMMARIZED_TABLE_COLUMNS = '3fr 3fr 3fr 1.5fr 50px';
+
+export const SUMMARY_VIEW_WIDTH = 750; // The width of the panel in pixels
