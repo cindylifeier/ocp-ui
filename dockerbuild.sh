5dfa747aa1691120943cfb765c92aea4ac62c133
JiahaoLi <Jiahao.Li@feisystems.com>
2018-05-07 12:05:07 -0400

    Increase the memory limit of node in docker build shell
    #19678: As a Developer, I will create a quick view for all workspaces

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+node --max_old_space_size=4096
 export PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 npm run build && docker build --build-arg PACKAGE_VERSION=$PACKAGE_VERSION -t bhits/ocp-ui:$PACKAGE_VERSION .
