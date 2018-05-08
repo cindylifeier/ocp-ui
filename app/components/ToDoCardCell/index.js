a1202d3781a511a1a5c9b343e7dd9535192a471a
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-05-08 11:17:19 -0400

    Adjust columns and update style to fit the new design
    # 19700:As a Developer, I will show more content when panel is maximized

--- app/components/ToDoCardCell/index.js
+++ app/components/ToDoCardCell/index.js
@@ -10,6 +10,7 @@ const ToDoCardCell = Cell.extend`
   border-radius: 5px;
   font-size: 12px;
   padding-bottom: 5px;
+  padding-right: 0 !important;
 `;
 
 ToDoCardCell.propTypes = {};
--- app/components/ToDoList/constants.js
+++ app/components/ToDoList/constants.js
@@ -4,8 +4,8 @@
  * This contains all the Constants for the TodoList component.
  */
 
-export const EXPANDED_TABLE_COLUMNS = '1fr 4fr 4fr 4fr';
-export const SUMMARIZED_TABLE_COLUMNS = '2fr 5fr 5fr .5fr';
+export const EXPANDED_TABLE_COLUMNS = '1fr 4fr 4fr 5fr';
+export const SUMMARIZED_TABLE_COLUMNS = '1fr 4fr 4fr 1.5fr';
 export const STATUS_CODE_CANCELLED = 'cancelled';
 
 export const SUMMARY_VIEW_WIDTH = 330; // The width of the panel in pixels
