db5b56992090262be4a6c2b512972a5d5897858f
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-05-08 09:45:03 -0400

    Set font size
    # 19700:As a Developer, I will show more content when panel is maximized

--- app/components/ToDoCardCell/index.js
+++ app/components/ToDoCardCell/index.js
@@ -8,7 +8,8 @@ import { Cell } from 'styled-css-grid';
 
 const ToDoCardCell = Cell.extend`
   border-radius: 5px;
-  font-size: 100%;
+  font-size: 12px;
+  padding-bottom: 5px;
 `;
 
 ToDoCardCell.propTypes = {};
