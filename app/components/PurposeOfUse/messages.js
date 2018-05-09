30c4f280835506dfe11eae29e19e1280f0e78a2c
sadhch <sadhana.chandravanka@feisystems.com>
2018-05-09 13:42:57 -0400

    Initial component for purpose of use
    19880 - As a Developer, I will Implement organization/individual consent functionality into OCP - Implementation

---
+++ app/components/PurposeOfUse/messages.js
@@ -0,0 +1,13 @@
+/*
+ * PurposeOfUse Messages
+ *
+ * This contains all the text for the PurposeOfUse component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'ocpui.components.PurposeOfUse.header',
+    defaultMessage: 'Purpose Of Use',
+  },
+});
