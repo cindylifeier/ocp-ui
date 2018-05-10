8f7a6b33b14e1eec053bc343321126496a818377
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-05-10 14:06:45 -0400

    Set columns config
    # 19892:UI Enhancements - Maximize/Quickview remaining panels

---
+++ app/components/CommunicationsTable/constants.js
@@ -0,0 +1,5 @@
+
+export const EXPANDED_TABLE_COLUMNS = 'repeat(6, 1fr) 50px';
+export const SUMMARIZED_TABLE_COLUMNS = 'repeat(3, 1fr) 50px';
+
+export const SUMMARY_VIEW_WIDTH = 695; // The width of the panel in pixels
