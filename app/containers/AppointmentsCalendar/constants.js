7cf1defee580c83822f59819fa6c57a5aa216bd4
Shruti Rao <shruti.rao@feisystems.com>
2018-05-21 15:06:01 -0400

    Add Outlook Appointments related constants
    #18450:[Spike] Research API integration of Outlook

--- app/containers/AppointmentsCalendar/constants.js
+++ app/containers/AppointmentsCalendar/constants.js
@@ -12,9 +12,14 @@ export const GET_APPOINTMENTS = 'ocpui/AppointmentsCalendar/GET_APPOINTMENTS';
 export const GET_APPOINTMENTS_SUCCESS = 'ocpui/AppointmentsCalendar/GET_APPOINTMENTS_SUCCESS';
 export const GET_APPOINTMENTS_ERROR = 'ocpui/AppointmentsCalendar/GET_APPOINTMENTS_ERROR';
 
+export const GET_OUTLOOK_APPOINTMENTS = 'ocpui/AppointmentsCalendar/GET_OUTLOOK_APPOINTMENTS';
+export const GET_OUTLOOK_APPOINTMENTS_SUCCESS = 'ocpui/AppointmentsCalendar/GET_OUTLOOK_APPOINTMENTS_SUCCESS';
+export const GET_OUTLOOK_APPOINTMENTS_ERROR = 'ocpui/AppointmentsCalendar/GET_OUTLOOK_APPOINTMENTS_ERROR';
+
 /**
 *  Other constants
 *
 */
 export const LOADING = 'loading';
 export const DATA = 'data';
+export const OUTLOOK_DATA = 'outlookData';
