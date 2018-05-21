4713e4f5bcfdad67949b35558193c4754fedac30
Shruti Rao <shruti.rao@feisystems.com>
2018-05-21 15:05:16 -0400

    Add Outlook Appointments related actions
    #18450:[Spike] Research API integration of Outlook

--- app/containers/AppointmentsCalendar/actions.js
+++ app/containers/AppointmentsCalendar/actions.js
@@ -6,8 +6,11 @@
 
 import {
   GET_APPOINTMENTS,
-  GET_APPOINTMENTS_SUCCESS,
   GET_APPOINTMENTS_ERROR,
+  GET_APPOINTMENTS_SUCCESS,
+  GET_OUTLOOK_APPOINTMENTS,
+  GET_OUTLOOK_APPOINTMENTS_ERROR,
+  GET_OUTLOOK_APPOINTMENTS_SUCCESS,
 } from './constants';
 
 // Get
@@ -28,6 +31,26 @@ export function getAppointmentsSuccess(appointments) {
 export function getAppointmentsError(error) {
   return {
     type: GET_APPOINTMENTS_ERROR,
+    error,
+  };
+}
+
+export function getOutlookAppointments() {
+  return {
+    type: GET_OUTLOOK_APPOINTMENTS,
+  };
+}
+
+export function getOutlookAppointmentsSuccess(outlookAppointments) {
+  return {
+    type: GET_OUTLOOK_APPOINTMENTS_SUCCESS,
+    outlookAppointments,
+  };
+}
+
+export function getOutlookAppointmentsError(error) {
+  return {
+    type: GET_OUTLOOK_APPOINTMENTS_ERROR,
     error,
   };
 }
