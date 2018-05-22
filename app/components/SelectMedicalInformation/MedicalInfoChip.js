1ad3801d5783a121a23bdf618b4a5f7bb5d59615
JiahaoLi <Jiahao.Li@feisystems.com>
2018-05-22 11:51:47 -0400

    Refactor ConsentTable component to apply ExpansionTableRow component
    #19880: As a Developer, I will Implement organization/individual consent functionality into OCP - Implementation

--- app/components/SelectMedicalInformation/MedicalInfoChip.js
+++
@@ -1,9 +0,0 @@
-import Chip from 'material-ui-next/Chip';
-import styled from 'styled-components';
-
-
-const MedicalInfoChip = styled(Chip)`
-  margin: 10px 5px 0 0;
-`;
-
-export default MedicalInfoChip;
