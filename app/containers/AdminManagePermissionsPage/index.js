354497f656f4bd4ba0a9e889217929a2820bd33b
Teng Luo <Teng.Luo@feisystems.com>
2018-05-23 14:43:20 -0400

    Update Admin Manage Permissions page
    #20033:Create User Management workspace in Org Admin workspace

--- app/containers/AdminManagePermissionsPage/index.js
+++ app/containers/AdminManagePermissionsPage/index.js
@@ -13,22 +13,90 @@ import { compose } from 'redux';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
+import GoldenLayout from 'components/GoldenLayout';
+import Page from 'components/Page';
+import renderPermissionGroupsComponent from 'containers/PermissionsGroups/render';
 import makeSelectAdminManagePermissionsPage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 
+export const initialStateMetadata =
+  {
+    settings: {
+      hasHeaders: true,
+      constrainDragToContainer: false,
+      reorderEnabled: true,
+      selectionEnabled: false,
+      popoutWholeStack: false,
+      blockedPopoutsThrowError: true,
+      closePopoutsOnUnload: true,
+      showPopoutIcon: false,
+      showMaximiseIcon: true,
+      showCloseIcon: true,
+      responsiveMode: 'onload',
+      tabOverlapAllowance: 0,
+      reorderOnTabMenuClick: true,
+      tabControlOffset: 10,
+    },
+    dimensions: {
+      borderWidth: 10,
+      borderGrabWidth: 15,
+      minItemHeight: 200,
+      minItemWidth: 450,
+      headerHeight: 40,
+      dragProxyWidth: 300,
+      dragProxyHeight: 200,
+    },
+    labels: {
+      close: 'close',
+      maximise: 'maximise',
+      minimise: 'minimise',
+      popout: 'open in new window',
+      popin: 'pop in',
+      tabDropdown: 'additional tabs',
+    },
+    content: [{
+      type: 'column',
+      isClosable: true,
+      reorderEnabled: true,
+      title: '',
+      content: [
+        {
+          title: 'Permission Groups',
+          type: 'component',
+          componentName: 'permissionGroups',
+          isClosable: true,
+          reorderEnabled: true,
+        },
+      ],
+    }],
+    isClosable: true,
+    reorderEnabled: true,
+    title: '',
+    openPopouts: [],
+    maximisedItemId: null,
+  };
+
+export const componentMetadata = [
+  { name: 'permissionGroups', text: 'permissionGroups', factoryMethod: renderPermissionGroupsComponent },
+];
+
 export class AdminManagePermissionsPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>
+      <Page>
         <Helmet>
-          <title>AdminManagePermissionsPage</title>
-          <meta name="description" content="Description of AdminManagePermissionsPage" />
+          <title>Admin Manage Permissions Page</title>
+          <meta name="description" content="Admin Manage Permissions page of Omnibus Care Plan application" />
         </Helmet>
-        <div>
-          Manage Permissions Page Test
-        </div>
-      </div>
+        <GoldenLayout
+          containerHeight="85vh"
+          containerWidth="90vw"
+          containerId="golden-admin-workspace"
+          componentMetadata={componentMetadata}
+          stateMetadata={initialStateMetadata}
+        />
+      </Page>
     );
   }
 }
