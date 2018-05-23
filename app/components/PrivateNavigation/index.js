672b6477c85723c8dd14c3ac4a0f855eb75205b9
Teng Luo <Teng.Luo@feisystems.com>
2018-05-23 11:15:16 -0400

    Add mange permission button on UI
    #20033:Create User Management workspace in Org Admin workspace

--- app/components/PrivateNavigation/index.js
+++ app/components/PrivateNavigation/index.js
@@ -10,13 +10,20 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { ToolbarGroup } from 'material-ui/Toolbar';
 import ActionHome from '@material-ui/icons/Home';
+import ActionBuild from '@material-ui/icons/Build';
 import StyledToolbar from 'components/StyledToolbar';
 import StyledIconButton from 'components/StyledIconButton';
+import {
+  ADMIN_MANAGE_PERMISSIONS_URL,
+  OCP_ADMIN_ROLE_CODE,
+  ORGANIZATION_ADMIN_ROLE_CODE,
+} from 'containers/App/constants';
 import messages from './messages';
 import NavigationButton from './NavigationButton';
 
 function PrivateNavigation(props) {
   const role = props.user.role;
+  console.log(props.getLinkUrlByRole(role));
   return (
     <StyledToolbar height="30px">
       <ToolbarGroup firstChild>
@@ -26,6 +33,22 @@ function PrivateNavigation(props) {
           </StyledIconButton>
           {<FormattedMessage {...messages.navButton} />}
         </NavigationButton>
+        {role === OCP_ADMIN_ROLE_CODE &&
+          <NavigationButton component={Link} to={ADMIN_MANAGE_PERMISSIONS_URL}>
+            <StyledIconButton size="x-small" svgIconSize="small" disableIconHover>
+              <ActionBuild color="#9cc" />
+            </StyledIconButton>
+            {<FormattedMessage {...messages.managePermissionsButton} />}
+          </NavigationButton>
+        }
+        {role === ORGANIZATION_ADMIN_ROLE_CODE &&
+        <NavigationButton component={Link} to={props.getLinkUrlByRole(role)}>
+          <StyledIconButton size="x-small" svgIconSize="small" disableIconHover>
+            <ActionBuild color="#9cc" />
+          </StyledIconButton>
+          {<FormattedMessage {...messages.manageUsersButton} />}
+        </NavigationButton>
+        }
       </ToolbarGroup>
     </StyledToolbar>
   );
