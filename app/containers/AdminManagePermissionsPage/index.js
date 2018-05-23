2f4097d5e8031ff83721d5a97f70da3e0ffdaa15
Teng Luo <Teng.Luo@feisystems.com>
2018-05-23 11:11:23 -0400

    Create AdminManagePermissionsPage container component
    #20033:Create User Management workspace in Org Admin workspace

---
+++ app/containers/AdminManagePermissionsPage/index.js
@@ -0,0 +1,56 @@
+/**
+ *
+ * AdminManagePermissionsPage
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectAdminManagePermissionsPage from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+
+export class AdminManagePermissionsPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>AdminManagePermissionsPage</title>
+          <meta name="description" content="Description of AdminManagePermissionsPage" />
+        </Helmet>
+      </div>
+    );
+  }
+}
+
+AdminManagePermissionsPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  adminmanagepermissionspage: makeSelectAdminManagePermissionsPage(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+const withReducer = injectReducer({ key: 'adminManagePermissionsPage', reducer });
+const withSaga = injectSaga({ key: 'adminManagePermissionsPage', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(AdminManagePermissionsPage);
