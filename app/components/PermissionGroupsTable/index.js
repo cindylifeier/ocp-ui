e169c8b60d21be7dfcdb152f602d5f305144ef49
Teng Luo <Teng.Luo@feisystems.com>
2018-05-24 10:57:17 -0400

    Create PermissionsGroupsTable Component
    #20033:Create User Management workspace in Org Admin workspace

---
+++ app/components/PermissionGroupsTable/index.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * PermissionGroupsTable
+ *
+ */
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+class PermissionGroupsTable extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        Table here
+      </div>
+    );
+  }
+}
+
+PermissionGroupsTable.propTypes = {
+
+};
+
+export default PermissionGroupsTable;
