1c572042431738a7799381af5414f243277035de
Teng Luo <Teng.Luo@feisystems.com>
2018-05-24 12:57:48 -0400

    Add table to PermissionsGroupsTable Component
    #20033:Create User Management workspace in Org Admin workspace

--- app/components/PermissionGroupsTable/index.js
+++ app/components/PermissionGroupsTable/index.js
@@ -5,17 +5,50 @@
  */
 
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import Table from 'components/Table';
+import TableHeader from 'components/TableHeader';
+import TableRow from 'components/TableRow';
+import TableRowColumn from 'components/TableRowColumn';
+import TableHeaderColumn from 'components/TableHeaderColumn';
+import messages from './messages';
+import { PERMISSION_GROUPS_TABLE_COLUMNS } from './constants';
 // import styled from 'styled-components';
 
+const columns = PERMISSION_GROUPS_TABLE_COLUMNS;
+function createTableHeaders() {
+  return (
+    <TableHeader columns={columns}>
+      <TableHeaderColumn><FormattedMessage {...messages.permissionGroup} /></TableHeaderColumn>
+      <TableHeaderColumn><FormattedMessage {...messages.description} /></TableHeaderColumn>
+      <TableHeaderColumn><FormattedMessage {...messages.action} /></TableHeaderColumn>
+    </TableHeader>
+  );
+}
 
-class PermissionGroupsTable extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <div>
-        Table here
-      </div>
-    );
-  }
+function createTableRows() {
+  return (
+    <TableRow columns={columns}>
+      <TableRowColumn>
+        Administrator
+      </TableRowColumn>
+      <TableRowColumn>
+        Resonsible for granting access and permission to other users.
+      </TableRowColumn>
+      <TableRowColumn>
+        Aciton Button
+      </TableRowColumn>
+    </TableRow>
+  );
+}
+
+function PermissionGroupsTable() { // eslint-disable-line react/prefer-stateless-function
+  return (
+    <Table>
+      {createTableHeaders()}
+      {createTableRows()}
+    </Table>
+  );
 }
 
 PermissionGroupsTable.propTypes = {
---
+++ app/components/PermissionGroupsTable/messages.js
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  permissionGroup: {
+    id: 'ocpui.components.PermissionGroupsTable.permissionGroup',
+    defaultMessage: 'Permission Group',
+  },
+  description: {
+    id: 'ocpui.components.PermissionGroupsTable.description',
+    defaultMessage: 'Description',
+  },
+  action: {
+    id: 'ocpui.components.PermissionGroupsTable.action',
+    defaultMessage: 'Action',
+  },
+});
