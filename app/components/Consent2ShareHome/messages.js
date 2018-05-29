8c6874b00971429acf5ff7a9a71fd94fa93e0a6d
JiahaoLi <Jiahao.Li@feisystems.com>
2018-05-29 14:06:05 -0400

    Implement PatientBanner component in Consent2share home page
    #20034: As a Developer, Create UI for C2S to match wireframe

--- app/components/Consent2ShareHome/messages.js
+++ app/components/Consent2ShareHome/messages.js
@@ -10,6 +10,20 @@ export default defineMessages({
     id: 'ocpui.components.Consent2ShareHome.header',
     defaultMessage: 'This is Consent2ShareHome component!',
   },
+  patientBanner: {
+    identifier: {
+      id: 'ocpui.components.Consent2ShareHome.patientBanner.identifier',
+      defaultMessage: 'Identifier',
+    },
+    birthDate: {
+      id: 'ocpui.components.Consent2ShareHome.patientBanner.birthDate',
+      defaultMessage: 'DOB',
+    },
+    gender: {
+      id: 'ocpui.components.Consent2ShareHome.patientBanner.gender',
+      defaultMessage: 'Gender',
+    },
+  },
   consentPanel: {
     panelSummary: {
       id: 'ocpui.components.Consent2ShareHome.consentPanel.panelSummary',
