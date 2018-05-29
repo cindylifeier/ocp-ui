3005756407a8452046553102deb5e3ff449ef806
mingfan <ming.fan@feisystems.com>
2018-05-29 13:51:32 -0400

    Create permission-assignment container component
    #20033: Create User Management workspace in Org Admin workspace

---
+++ app/containers/PermissionAssignments/tests/selectors.test.js
@@ -0,0 +1,8 @@
+// import { fromJS } from 'immutable';
+// import { selectPermissionAssignmentsDomain } from '../selectors';
+
+describe('selectPermissionAssignmentsDomain', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
