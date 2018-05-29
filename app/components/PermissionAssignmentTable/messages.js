23f43b4a3de1b1202665eaa0be27a8df3a591bab
mingfan <ming.fan@feisystems.com>
2018-05-29 15:03:30 -0400

    Create permission-assignment table component
    #20033: Create User Management workspace in Org Admin workspace

---
+++ app/components/PermissionAssignmentTable/messages.js
@@ -0,0 +1,24 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  providerName: {
+    id: 'ocpui.components.PermissionAssignmentTable.providerName',
+    defaultMessage: 'Provider Name',
+  },
+  role: {
+    id: 'ocpui.components.PermissionAssignmentTable.role',
+    defaultMessage: 'Role',
+  },
+  permissionGroup: {
+    id: 'ocpui.components.PermissionAssignmentTable.permissionGroup',
+    defaultMessage: 'Permission Group',
+  },
+  contact: {
+    id: 'ocpui.components.PermissionAssignmentTable.contact',
+    defaultMessage: 'Contact',
+  },
+  action: {
+    id: 'ocpui.components.PermissionAssignmentTable.action',
+    defaultMessage: 'Action',
+  },
+});
