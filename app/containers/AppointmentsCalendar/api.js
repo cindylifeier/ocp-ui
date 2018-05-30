9df1d7806879d3d917cec8a210eab5684c0a0d6d
Shruti Rao <shruti.rao@feisystems.com>
2018-05-30 17:25:56 -0400

    Add a new endpoint to authenticate outlook username and password
    #20027:As a user I should be see all times of confirmed appointments as unavailable slots in my Calendar. - Outlook entries and OCP entries

--- app/containers/AppointmentsCalendar/api.js
+++ app/containers/AppointmentsCalendar/api.js
@@ -11,5 +11,23 @@ export default function getAppointmentsApi(query) {
 
 export function getOutlookAppointmentsApi() {
   const baseEndpoint = getEndpoint(BASE_OUTLOOK_API_URL);
-  return request(baseEndpoint);
+  const requestURL = `${baseEndpoint}/calendar`;
+  return request(requestURL);
+}
+
+export function loginToOWA(loginCredentials) {
+  const baseEndpoint = getEndpoint(BASE_OUTLOOK_API_URL);
+  const requestURL = `${baseEndpoint}/login`;
+  return request(requestURL, {
+    method: 'POST',
+    body: JSON.stringify(mapToBffCredential(loginCredentials)),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
+function mapToBffCredential(loginCredentials) {
+  const { username, password } = loginCredentials;
+  return { username, password };
 }
