a127a922d8a5b19b4266eb09f5c282127496a268
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-06-18 15:44:07 -0400

    Implement a bridge autosuggestion component
    # 20218 - As a User I will be able to capture Patient's coverage information - Wireframes and Functionality

---
+++ app/components/AutoSuggestion/index.js
@@ -0,0 +1,76 @@
+/**
+*
+* AutoSuggestion
+*
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import { Field } from 'formik';
+
+class AutoSuggestionBridge extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      suggestion: {},
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(selectedOption) {
+    this.setState({ suggestion: selectedOption });
+  }
+
+  render() {
+    const { suggestions, field: { name, value }, form: { setFieldValue, setFieldTouched, errors }, ...rest } = this.props;
+    return (
+      <Select
+        name={name}
+        options={suggestions}
+        value={value || this.state.suggestion}
+        onChange={(event) => {
+          this.handleChange(event);
+          setFieldValue(name, event.value);
+        }}
+        onClick={() => setFieldTouched(name)}
+        errorText={errors[name]}
+        {...rest}
+
+      />
+    );
+  }
+}
+
+
+function AutoSuggestionField(props) {
+  const { name, ...rest } = props;
+  return (
+    <Field
+      name={name}
+      render={(p) => (<AutoSuggestionBridge {...p} {...rest} />)}
+    />);
+}
+
+AutoSuggestionBridge.propTypes = {
+  suggestions: PropTypes.array.isRequired,
+  name: PropTypes.string.isRequired,
+  field: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    value: PropTypes.object,
+  }).isRequired,
+  form: PropTypes.shape({
+    handleChange: PropTypes.func.isRequired,
+    handleBlur: PropTypes.func.isRequired,
+    setFieldTouched: PropTypes.func.isRequired,
+    errors: PropTypes.object,
+  }).isRequired,
+};
+
+AutoSuggestionField.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default AutoSuggestionField;
---
+++ app/components/AutoSuggestion/messages.js
@@ -0,0 +1,13 @@
+/*
+ * AutoSuggestion Messages
+ *
+ * This contains all the text for the AutoSuggestion component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'ocpui.components.AutoSuggestionBridge.header',
+    defaultMessage: 'This is the AutoSuggestionBridge component !',
+  },
+});
