b82bd791a7fbe1fda0d5c76090e3f44a2c9c9b4d
Jenny Ho <jenny.ho@feisystems.com>
2018-06-18 11:01:02 -0400

    Update golden layout panels tabs
    #20250:As a designer, will have cleaned up the styling in the OCP UI

--- app/components/GoldenLayout/GoldenLayoutContainer.js
+++ app/components/GoldenLayout/GoldenLayoutContainer.js
@@ -2,31 +2,26 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const GoldenLayoutContainer = styled.div`
-  /* Set height considering app bar height */
-  & .lm_header {
-    background: #fff;
-    border: 1px solid #099;
-    padding: 0 10px 0 10px;
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-    border-bottom: 0;
+  /* Main golden layout container */
+  & .lm_goldenlayout {
+    background: transparent !important;
   }
 
+  /* Set height considering app bar height */
   & .lm_controls {
     margin-top: 10px;
   }
 
-  & .lm_stack {
-    background: #fff;
-    border-bottom: 0;
-  }
-
   & .lm_header + .lm_items {
-    background: #fff;
+    background: ${(props) => props.secondaryColor};
     border: 1px solid #099;
     border-top: 0;
-    border-bottom-left-radius: 10px;
-    border-bottom-right-radius: 10px;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+  }
+
+  & .lm_header {
+    height: 30px !important;
   }
 
   & .lm_header + .lm_items,
@@ -36,7 +31,6 @@ const GoldenLayoutContainer = styled.div`
   }
 
   & .lm_root {
-    background: #fff;
     height: ${(props) => props.containerHeight};
     width: ${(props) => props.containerWidth};
   }
@@ -44,29 +38,35 @@ const GoldenLayoutContainer = styled.div`
   /* Enable scroll content */
   & .lm_content {
     overflow: auto;
-    border: 0;
+    border-top: 1px solid #099;
+    border-bottom: 0 !important;
+    border-left: 0 !important;
+    border-right: 0 !important;
     background: transparent;
   }
 
-  & .lm_tabs {
-    margin-top: 10px;
-  }
-
   & .lm_tab {
-    font-weight: bold;
+    height: 18px !important;
+    margin-top: 5px !important;
+    border-radius: 5px;
+    border: 1px solid #099 !important;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+    box-shadow: none !important;
   }
 
   & .lm_tab.lm_active {
-    color: ${(props) => props.primaryColor};
-    background: ${(props) => props.secondaryColor};
+    color: ${(props) => props.secondaryColor};
+    background: #099;
     font-size: 13px;
     font-family: Arial Bold, Arial, sans-serif;
   }
 
   & .lm_tab.lm_active:hover,
   .lm_tab:hover {
-    color: #099;
+    color: ${(props) => props.secondaryColor};
     font-size: 13px;
+    background: #62b5b5;
   }
 
   & .lm_title {
