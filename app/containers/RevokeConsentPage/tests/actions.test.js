23a5e40caac4f5ad776a58238238fc2be04ad7f1
sadhch <sadhana.chandravanka@feisystems.com>
2018-06-19 06:58:02 -0400

    unit test temp
    20213 - As a user I should see all my consents. This is to replace consents panel in dashboard.

--- app/containers/RevokeConsentPage/tests/actions.test.js
+++ app/containers/RevokeConsentPage/tests/actions.test.js
@@ -6,7 +6,7 @@ import {
   DEFAULT_ACTION,
 } from '../constants';
 
-xdescribe('RevokeConsentPage actions', () => {
+describe('RevokeConsentPage actions', () => {
   describe('Default Action', () => {
     it('has a type of DEFAULT_ACTION', () => {
       const expected = {
