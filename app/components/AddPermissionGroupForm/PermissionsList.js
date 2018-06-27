babb182d2f276e01bbbd325057d4ecb8013dc4fa
teng-Luo <Teng.Luo@feisystems.com>
2018-06-27 08:14:49 -0400

    Implement swap scope for permissions
    #20196: Patient Panel - view all patients

--- app/components/AddPermissionGroupForm/PermissionsList.js
+++ app/components/AddPermissionGroupForm/PermissionsList.js
@@ -9,51 +9,94 @@ import { Cell, Grid } from 'styled-css-grid';
 import StyledListItem from './StyledListItem';
 import StyledList from './StyledList';
 import PermissionAddButtonsSection from './PermissionAddButtonsSection';
+let selectScope;
+let updateAddedScopes;
+let updateScopes;
+class PermissionsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addedScopes: [],
+      scopes: this.props.scopes,
+    };
+    this.addAddedPermission = this.addAddedPermission.bind(this);
+    this.removeAddedPermission = this.removeAddedPermission.bind(this);
+  }
 
-function PermissionsList(props) {
-  const {
-    scopes,
-    addedScopes,
-  } = props;
-  return (
-    <div>
-      <Grid columns={5}>
-        <Cell row={1} width={2}>
-          <Subheader>Permissions</Subheader>
-          <StyledList defaultValue={1}>
-            {scopes && scopes.map((scope) => (
-              <StyledListItem
-                key={scope.id}
-                primaryText={scope.description}
-              />))
-            }
-          </StyledList>
-        </Cell>
-        <Cell row={1} width={1}>
-          <PermissionAddButtonsSection>
-            <Grid columns={1}>
-              <StyledIconButton svgIconSize="large" onClick={() => addedScopes.add()}>
-                <ArrowFowardIcon />
-              </StyledIconButton>
-              <StyledIconButton svgIconSize="large" onClick={() => addedScopes.remove()}>
-                <ArrowBackIcon />
-              </StyledIconButton>
-            </Grid>
-          </PermissionAddButtonsSection>
-        </Cell>
-        <Cell row={1} width={2}>
-          <Subheader>Added Permissions</Subheader>
-          <StyledList defaultValue={1}>
-          </StyledList>
-        </Cell>
-      </Grid>
-    </div>
-  );
+  addAddedPermission() {
+    updateAddedScopes = this.state.addedScopes;
+    updateAddedScopes.push(selectScope);
+    updateScopes = this.state.scopes;
+    updateScopes = updateScopes.filter((key) => key.id !== selectScope.id);
+    this.setState(
+      { addedScopes: updateAddedScopes,
+        scopes: updateScopes,
+      });
+  }
+
+  removeAddedPermission() {
+    updateScopes = this.state.scopes;
+    updateScopes.push(selectScope);
+    updateAddedScopes = this.state.addedScopes;
+    updateAddedScopes = updateAddedScopes.filter((key) => key.id !== selectScope.id);
+    this.setState(
+      { addedScopes: updateAddedScopes,
+        scopes: updateScopes,
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid columns={5}>
+          <Cell row={1} width={2}>
+            <Subheader>Permissions</Subheader>
+            <StyledList defaultValue={1}>
+              {this.state.scopes && this.state.scopes.map((scope) => (
+                <StyledListItem
+                  key={scope.id}
+                  primaryText={scope.description}
+                  onClick={() => {
+                    selectScope = scope;
+                  }}
+                />))
+              }
+            </StyledList>
+          </Cell>
+          <Cell row={1} width={1}>
+            <PermissionAddButtonsSection>
+              <Grid columns={1}>
+                <StyledIconButton svgIconSize="large" onClick={this.addAddedPermission}>
+                  <ArrowFowardIcon />
+                </StyledIconButton>
+                <StyledIconButton svgIconSize="large" onClick={this.removeAddedPermission}>
+                  <ArrowBackIcon />
+                </StyledIconButton>
+              </Grid>
+            </PermissionAddButtonsSection>
+          </Cell>
+          <Cell row={1} width={2}>
+            <Subheader>Added Permissions</Subheader>
+            <StyledList defaultValue={1}>
+              {this.state.addedScopes && this.state.addedScopes.map((scope) => (
+                <StyledListItem
+                  key={scope.id}
+                  primaryText={scope.description}
+                  onClick={() => {
+                    selectScope = scope;
+                  }}
+                />))
+              }
+            </StyledList>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 PermissionsList.propTypes = {
   scopes: PropTypes.array,
-  addedScopes: PropTypes.array,
 };
 
 export default PermissionsList;
