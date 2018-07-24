a06e304e4a0e3ec053aba565480a468162077bcb
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-07-23 21:14:49 -0400

    Implement assign location table
    # 20759 - Enhancements/Panel within Org Admin workspace to associate practitioners to locationst

--- app/components/AssignLocationTable/index.js
+++ app/components/AssignLocationTable/index.js
@@ -5,21 +5,95 @@
 */
 
 import React from 'react';
-// import styled from 'styled-components';
-
+import PropTypes from 'prop-types';
+import isEmpty from 'lodash/isEmpty';
+// import uniqueId from 'lodash/uniqueId';
+import Checkbox from 'material-ui/Checkbox';
+import { Cell, Grid } from 'styled-css-grid';
 import { FormattedMessage } from 'react-intl';
+
+import sizeMeHOC from 'utils/SizeMeUtils';
+import Table from 'components/Table';
+import TableHeader from 'components/TableHeader';
+import TableHeaderColumn from 'components/TableHeaderColumn';
+import ExpansionTableRow from 'components/ExpansionTableRow';
+import TableRowColumn from 'components/TableRowColumn';
+import {
+  ASSIGNED_TABLE_COLUMNS, EXPANDED_TABLE_COLUMNS,
+  SUMMARIZED_TABLE_COLUMNS, SUMMARY_VIEW_WIDTH, ACTIVE,
+} from 'components/AssignLocationTable/constants';
+import LocationExpansionRowDetails from 'components/AssignLocationTable/LocationExpansionRowDetails';
 import messages from './messages';
 
-function AssignLocationTable() {
+
+function AssignLocationTable({ elements, showAssigned = false, onCheck, relativeTop, size, flattenLocationData }) {
+  const isExpanded = size && size.width && (Math.floor(size.width) > SUMMARY_VIEW_WIDTH);
+
+  let tableColumns = EXPANDED_TABLE_COLUMNS;
+  if (showAssigned) {
+    tableColumns = ASSIGNED_TABLE_COLUMNS;
+  } else {
+    tableColumns = isExpanded ? EXPANDED_TABLE_COLUMNS : SUMMARIZED_TABLE_COLUMNS;
+  }
   return (
     <div>
-      <FormattedMessage {...messages.header} />
+      <Table>
+        <TableHeader columns={tableColumns} relativeTop={relativeTop}>
+          <TableHeaderColumn />
+          <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderAssigned} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderName} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderAddress} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderContact} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderIdentifier} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.tableColumnHeaderStatus} /></TableHeaderColumn>
+        </TableHeader>
+        {!isEmpty(elements) && elements.map((location) => {
+          const flattenedLocation = flattenLocationData(location);
+          const { logicalId, name, status, telecoms, address, identifiers } = flattenedLocation;
+          return (
+            <ExpansionTableRow
+              expansionTableRowDetails={
+                <LocationExpansionRowDetails
+                  location={flattenedLocation}
+                  physicalType={location.physicalType}
+                />
+              }
+              key={logicalId}
+              columns={tableColumns}
+            >
+              <TableRowColumn>
+                <Grid columns={12}>
+                  <Cell left={2} width={1}>
+                    <Checkbox
+                      // checked={element.assignedToCurrentLocation}
+                      onCheck={(evt, checked) => onCheck(evt, checked, logicalId)}
+                    />
+                  </Cell>
+                </Grid>
+              </TableRowColumn>
+              <TableRowColumn>{name}</TableRowColumn>
+              <TableRowColumn>{address}</TableRowColumn>
+              <TableRowColumn>{telecoms}</TableRowColumn>
+              <TableRowColumn>{identifiers}</TableRowColumn>
+              <TableRowColumn>{status === ACTIVE ?
+                <FormattedMessage {...messages.labelActive} /> :
+                <FormattedMessage {...messages.labelInactive} />}
+              </TableRowColumn>
+            </ExpansionTableRow>
+          );
+        })}
+      </Table>
     </div>
   );
 }
 
 AssignLocationTable.propTypes = {
-
+  flattenLocationData: PropTypes.func,
+  relativeTop: PropTypes.number,
+  elements: PropTypes.array,
+  showAssigned: PropTypes.bool,
+  onCheck: PropTypes.func,
+  size: PropTypes.object.isRequired,
 };
 
-export default AssignLocationTable;
+export default sizeMeHOC(AssignLocationTable);
--- app/components/AssignLocationTable/messages.js
+++ app/components/AssignLocationTable/messages.js
@@ -10,4 +10,60 @@ export default defineMessages({
     id: 'ocpui.components.AssignLocationTable.header',
     defaultMessage: 'This is the AssignLocationTable component !',
   },
+  tableColumnHeaderAssigned: {
+    id: 'ocpui.components.AssignLocationTable.tableColumnHeaderAssigned',
+    defaultMessage: 'Assigned',
+  },
+  tableColumnHeaderAddress: {
+    id: 'ocpui.components.AssignLocationTable.tableColumnHeaderAddress',
+    defaultMessage: 'Address',
+  },
+  tableColumnHeaderName: {
+    id: 'ocpui.components.AssignLocationTable.tableColumnHeaderName',
+    defaultMessage: 'Name',
+  },
+  tableColumnHeaderContact: {
+    id: 'ocpui.components.AssignLocationTable.tableColumnHeaderContact',
+    defaultMessage: 'Contact',
+  },
+  tableColumnHeaderIdentifier: {
+    id: 'ocpui.components.AssignLocationTable.tableColumnHeaderIdentifier',
+    defaultMessage: 'Identifier',
+  },
+  tableColumnHeaderProgramAction: {
+    id: 'ocpui.components.AssignLocationTable.tableColumnHeaderProgramAction',
+    defaultMessage: 'Action',
+  },
+  tableColumnHeaderSpecialty: {
+    id: 'ocpui.components.AssignLocationTable.tableColumnHeaderSpecialty',
+    defaultMessage: 'Specialty',
+  },
+  tableColumnHeaderStatus: {
+    id: 'ocpui.components.AssignLocationTable.tableColumnHeaderStatus',
+    defaultMessage: 'Status',
+  },
+  expansionRowDetailsTelecoms: {
+    id: 'ocpui.components.AssignLocationTable.expansionRowDetailsTelecoms',
+    defaultMessage: 'Contacts',
+  },
+  tableColumnHeaderLocationType: {
+    id: 'ocpui.components.AssignLocationTable.tableColumnHeaderLocationType',
+    defaultMessage: 'Location Type',
+  },
+  labelActive: {
+    id: 'ocpui.components.AssignLocationTable.labelActive',
+    defaultMessage: 'Active',
+  },
+  labelInactive: {
+    id: 'ocpui.components.AssignLocationTable.labelInactive',
+    defaultMessage: 'Inactive',
+  },
+  edit: {
+    id: 'ocpui.components.AssignLocationTable.edit',
+    defaultMessage: 'Edit',
+  },
+  remove: {
+    id: 'ocpui.components.AssignLocationTable.remove',
+    defaultMessage: 'Remove',
+  },
 });
