37db3127389a6972fa829f8b5428f3750a73e81e
CindyLiFei <cindy.li@feisystems.com>
2018-07-24 14:00:31 -0400

    Initial manage user component
    #20761: User registration into OCP (OCP admin/Org admin and Care manager) - Create user name and password creation

---
+++ app/components/ManageUser/index.js
@@ -0,0 +1,27 @@
+/**
+*
+* ManageUser
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+
+class ManageUser extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <FormattedMessage {...messages.header} />
+      </div>
+    );
+  }
+}
+
+ManageUser.propTypes = {
+
+};
+
+export default ManageUser;
---
+++ app/components/ManageUser/messages.js
@@ -0,0 +1,13 @@
+/*
+ * ManageUser Messages
+ *
+ * This contains all the text for the ManageUser component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'ocpui.components.ManageUser.header',
+    defaultMessage: 'This is the ManageUser component !',
+  },
+});
