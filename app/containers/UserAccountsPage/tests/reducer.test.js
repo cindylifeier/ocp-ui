1fce1f2d5fbd072af6a0019138d6f8548ec38fe0
JiahaoLi <Jiahao.Li@feisystems.com>
2018-07-24 16:46:40 -0400

    Remove unused UserAccountsPage and UserAccounts components
    #20664: Update Demo environment with QA code by end of this sprint

--- app/containers/UserAccountsPage/tests/reducer.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import userAccountsPageReducer from '../reducer';
-
-describe('userAccountsPageReducer', () => {
-  it('returns the initial state', () => {
-    // expect(userAccountsPageReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
--- app/containers/UserAccountsPage/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { selectUserAccountsPageDomain } from '../selectors';
-
-describe('selectUserAccountsPageDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    // expect(true).toEqual(false);
-  });
-});
