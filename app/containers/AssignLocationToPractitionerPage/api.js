ad7ad38de442a4561986ade9d93a880cae6ebda3
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-07-24 20:08:02 -0400

    Implement unassign locaiton api and saga
    # 20759 - Enhancements/Panel within Org Admin workspace to associate practitioners to locationst

--- app/containers/AssignLocationToPractitionerPage/api.js
+++ app/containers/AssignLocationToPractitionerPage/api.js
@@ -6,9 +6,9 @@ import { BASE_ORGANIZATIONS_API_URL,
   BASE_PRACTITIONERS_API_URL,
 } from '../../utils/endpointService';
 
-export function getLocationWithPractitionerAssignmentData(organizationId, locationId, currentPage) {
+export function getLocationWithPractitionerAssignmentData(organizationId, practitionerId, currentPage) {
   const params = queryString({
-    assignedToLocationId: locationId,
+    assignedToPractitioner: practitionerId,
     pageNumber: currentPage,
     pageSize: DEFAULT_PAGE_SIZE,
   });
@@ -30,4 +30,32 @@ export function assignPractitionerToLocation(practitionerId, organizationId, loc
       'Content-Type': 'application/json',
     },
   });
+}
+
+export function unassignPractitionerToLocation(practitionerId, organizationId, locationId) {
+  const params = queryString({
+    organizationId,
+    locationId,
+  });
+  const baseEndpoint = getEndpoint(BASE_PRACTITIONERS_API_URL);
+  const url = `${baseEndpoint}/${practitionerId}/unassign${params}`;
+  return request(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
+
+export function setLocationAssignedToCurrentPractitioner(locations, locationId, value) {
+  let i = 0;
+  while (i < locations.length) {
+    const location = locations[i];
+    if (location.logicalId === locationId) {
+      location.assignToCurrentPractitioner = value;
+      break;
+    }
+    i += 1;
+  }
 }
--- app/containers/AssignLocationToPractitionerPage/saga.js
+++ app/containers/AssignLocationToPractitionerPage/saga.js
@@ -5,6 +5,7 @@ import {
   getPractitionerLocationAssignmentError,
   assignPractitionerLocationAssignmentError,
   markLocationAsAssigned,
+  unmarkLocationAsAssigned,
 } from 'containers/AssignLocationToPractitionerPage/actions';
 
 import { makeSelectOrganization,
@@ -13,9 +14,13 @@ import { makeSelectOrganization,
 import { showNotification } from 'containers/Notification/actions';
 import { GET_PRACTIONER_LOCATION_ASSIGNMENT,
   ASSIGN_PRACTITIONER_LOCATION_ASSIGNMENT,
+  UNASSIGN_PRACTITIONER_LOCATION_ASSIGNMENT,
 } from 'containers/AssignLocationToPractitionerPage/constants';
-import { getLocationWithPractitionerAssignmentData,
+import {
+  getLocationWithPractitionerAssignmentData,
   assignPractitionerToLocation,
+  unassignPractitionerToLocation,
+  unassignPractitionerLocationAssignmentError,
 } from 'containers/AssignLocationToPractitionerPage/api';
 
 export function* getPractitionerLocationAssignmentSaga(action) {
@@ -41,13 +46,31 @@ export function* assignPractitionerLocationAssignmentSaga(action) {
     if (isEmpty(organization) || isEmpty(practitioner) || isEmpty(organization.logicalId) || isEmpty(practitioner.logicalId)) {
       yield put(showNotification('Failed to assign the location to the current practitioner. Organization and/or practitioner is not selected.'));
     } else {
-      yield call(assignPractitionerToLocation, action.practitionerId, organization.logicalId, action.locationId);
+      yield call(assignPractitionerToLocation, practitioner.logicalId, organization.logicalId, action.locationId);
       yield put(markLocationAsAssigned(action.locationId));
       yield put(showNotification('The location is successfully assigned to current practitioner.'));
     }
   } catch (error) {
     yield put(assignPractitionerLocationAssignmentError(error));
     yield put(showNotification('Failed to assign the location to the current practitioner. Please try again.'));
+  }
+}
+
+
+export function* unassignPractitionerLocationAssignmentSaga(action) {
+  try {
+    const organization = yield select(makeSelectOrganization());
+    const practitioner = yield select(makeSelectPractitioner());
+    if (isEmpty(organization) || isEmpty(practitioner) || isEmpty(organization.logicalId) || isEmpty(practitioner.logicalId)) {
+      yield put(showNotification('Failed to unassign the location to the current practitioner. Organization and/or practitioner is not selected.'));
+    } else {
+      yield call(unassignPractitionerToLocation, practitioner.logicalId, organization.logicalId, action.locationId);
+      yield put(unmarkLocationAsAssigned(action.locationId));
+      yield put(showNotification('The location is successfully unassigned to current practitioner.'));
+    }
+  } catch (error) {
+    yield put(unassignPractitionerLocationAssignmentError(error));
+    yield put(showNotification('Failed to unassign the location to the current practitioner. Please try again.'));
   }
 }
 
@@ -60,9 +83,14 @@ export function* watchAssignPractitionerLocationAssignmentSaga() {
   yield takeLatest(ASSIGN_PRACTITIONER_LOCATION_ASSIGNMENT, assignPractitionerLocationAssignmentSaga);
 }
 
+export function* watchUnassignPractitionerLocationAssignmentSaga() {
+  yield takeLatest(UNASSIGN_PRACTITIONER_LOCATION_ASSIGNMENT, unassignPractitionerLocationAssignmentSaga);
+}
+
 export default function* rootSaga() {
   yield all([
     watchGetPractitionerLocationAssignmentSaga(),
     watchAssignPractitionerLocationAssignmentSaga(),
+    watchUnassignPractitionerLocationAssignmentSaga(),
   ]);
 }
