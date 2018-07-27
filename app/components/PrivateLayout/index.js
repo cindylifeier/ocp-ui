4efdee0f09c8b4337061580f1036ba98774d489a
Ang Zhou.git <Ang.Zhou@feisystems.com>
2018-07-27 14:56:07 -0400

    Update PrivateLayout
    #20395:As a designer, will have cleaned up the styling in the OCP UI Part 2

--- app/components/PrivateLayout/index.js
+++ app/components/PrivateLayout/index.js
@@ -6,30 +6,40 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Cell } from 'styled-css-grid';
+import { Cell, Grid } from 'styled-css-grid';
+import { FormattedMessage } from 'react-intl';
+import brandImg from 'images/omnibus-care-plan-logo.png';
 
 import PrivateHeader from 'components/PrivateHeader';
 import PrivateNavigation from 'components/PrivateNavigation';
+import StyledImage from 'components/StyledImage';
 import LayoutGrid from './LayoutGrid';
 import HeaderGrid from './HeaderGrid';
-import HeaderCell from './HeaderCell';
 import HeaderContainer from './HeaderContainer';
 import ContentContainer from './ContentContainer';
+import messages from './messages';
 
 function PrivateLayout(props) {
   return (
     <LayoutGrid columns={1}>
       <HeaderContainer>
-        <HeaderGrid columns={1}>
-          <HeaderCell>
-            <PrivateHeader user={props.user} />
-          </HeaderCell>
+        <HeaderGrid columns={'100px 1fr'} alignContent="end">
+          <Cell>
+            <StyledImage height="55px" width="75px" src={brandImg} alt={<FormattedMessage {...messages.brandImg} />} />
+          </Cell>
           {props.user.role &&
           <Cell>
-            <PrivateNavigation
-              user={props.user}
-              getLinkUrlByRole={props.getLinkUrlByRole}
-            />
+            <Grid columns={2}>
+              <Cell top={2}>
+                <PrivateNavigation
+                  user={props.user}
+                  getLinkUrlByRole={props.getLinkUrlByRole}
+                />
+              </Cell>
+              <Cell top={2}>
+                <PrivateHeader user={props.user} />
+              </Cell>
+            </Grid>
           </Cell>
           }
         </HeaderGrid>
