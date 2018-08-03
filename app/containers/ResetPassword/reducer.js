3b3241b8813e4beea6aa8d8049af111350e5d72d
JiahaoLi <Jiahao.Li@feisystems.com>
2018-08-03 18:18:18 -0400

    Add reset password reducer
    #20946: Org Admin Manage Users Workspace

--- app/containers/ResetPassword/reducer.js
+++ app/containers/ResetPassword/reducer.js
@@ -5,16 +5,18 @@
  */
 
 import { fromJS } from 'immutable';
-import {
-  DEFAULT_ACTION,
-} from './constants';
+import { RESET_PASSWORD, RESET_PASSWORD_ERROR } from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  error: null,
+});
 
 function resetPasswordReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case RESET_PASSWORD:
+      return state.set('error', null);
+    case RESET_PASSWORD_ERROR:
+      return state.set('error', action.error);
     default:
       return state;
   }
