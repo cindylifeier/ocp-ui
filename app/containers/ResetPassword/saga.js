0a3ebd4569a38848f3adeba1a2024ba1465ed313
JiahaoLi <Jiahao.Li@feisystems.com>
2018-08-03 18:20:03 -0400

    Implement middleware to reset password
    #20946: Org Admin Manage Users Workspace

--- app/containers/ResetPassword/saga.js
+++ app/containers/ResetPassword/saga.js
@@ -1,6 +1,31 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { showNotification } from 'containers/Notification/actions';
+import { resetPasswordError } from './actions';
+import { getErrorDetail, resetPassword } from './api';
+import { RESET_PASSWORD } from './constants';
 
-// Individual exports for testing
-export default function* defaultSaga() {
-  // See example in containers/HomePage/saga.js
+export function* resetPasswordSaga({ userId, newPassword, handleSubmitting, handleCloseDialog }) {
+  try {
+    yield call(resetPassword, userId, newPassword);
+    yield put(showNotification('Successfully reset the password.'));
+    yield call(handleSubmitting);
+    yield call(handleCloseDialog);
+  } catch (error) {
+    yield call(handleSubmitting);
+    yield put(showNotification('Failed to reset the password.'));
+    yield put(resetPasswordError(getErrorDetail(error)));
+  }
+}
+
+export function* watchResetPasswordSaga() {
+  yield takeLatest(RESET_PASSWORD, resetPasswordSaga);
+}
+
+/**
+ * Root saga manages watcher lifecycle
+ */
+export default function* rootSaga() {
+  yield all([
+    watchResetPasswordSaga(),
+  ]);
 }
