43f84f844a47960aa3df8e214c4e2fa4c66c941e
JiahaoLi <Jiahao.Li@feisystems.com>
2018-08-14 19:17:09 -0400

    Implement initial AddContactDetails component
    #21087: OCP Admin Enhancements

---
+++ app/components/AddContactDetails/index.js
@@ -0,0 +1,68 @@
+/**
+ *
+ * AddContactDetails
+ *
+ */
+
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { FieldArray } from 'formik';
+import { DialogTitle } from 'material-ui-next/Dialog';
+import teal from 'material-ui-next/colors/teal';
+
+import FormSubtitle from 'components/FormSubtitle';
+import AddNewItemButton from 'components/PanelToolbar/AddNewItemButton';
+import StyledAddCircleIcon from 'components/StyledAddCircleIcon';
+import StyledDialog from 'components/StyledDialog';
+import messages from './messages';
+
+class AddContactDetails extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      dialogOpen: false,
+    };
+    this.handleOpenDialog = this.handleOpenDialog.bind(this);
+    this.handleCloseDialog = this.handleCloseDialog.bind(this);
+  }
+
+  handleOpenDialog() {
+    this.setState({ dialogOpen: true });
+  }
+
+  handleCloseDialog() {
+    this.setState({
+      dialogOpen: false,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <FormSubtitle margin="1vh 0 0 0">
+          <FormattedMessage {...messages.header} />
+        </FormSubtitle>
+        <AddNewItemButton color="primary" fontWeight="bold" fontSize="15px" onClick={this.handleOpenDialog}>
+          <StyledAddCircleIcon color={teal['500']} />
+          <FormattedMessage {...messages.addContactsButton} />
+        </AddNewItemButton>
+        <FieldArray
+          name="contacts"
+          render={() => (
+            <div>
+              <StyledDialog fullScreen open={this.state.dialogOpen}>
+                <DialogTitle>
+                  <FormattedMessage {...messages.title} />
+                </DialogTitle>
+              </StyledDialog>
+            </div>
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+AddContactDetails.propTypes = {};
+
+export default AddContactDetails;
