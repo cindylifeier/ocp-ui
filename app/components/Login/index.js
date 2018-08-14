3ee8eba663e3127d9c7b2d6648893ae9c1848d06
JiahaoLi <Jiahao.Li@feisystems.com>
2018-08-13 20:26:18 -0400

    Implement PublicLayoutRoute component
    #21061: OCP Admin Manage Users Workspace Enhancements

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -13,7 +13,6 @@ import { FormattedMessage } from 'react-intl';
 import { Cell, Grid } from 'styled-css-grid';
 import CardHeader from 'components/CardHeader';
 import TextField from 'components/TextField';
-import PublicHeader from 'components/PublicHeader';
 import StyledRaisedButton from 'components/StyledRaisedButton';
 import LoginStyledCard from './LoginStyledCard';
 import LoginFieldGrid from './LoginFieldGrid';
@@ -24,92 +23,76 @@ function Login(props) {
   const { onLogin, isShowSampleUserLoginDetails } = props;
 
   return (
-    <div>
-      <Grid
-        columns={3}
-        rows={'120px 1fr 120px'}
-        areas={[
-          'header header header',
-          'leftSide content rightSide',
-          'footer footer footer',
-        ]}
-      >
-        <Cell area="header">
-          <PublicHeader />
-        </Cell>
-        <Cell area="leftSide" />
-        <Cell area="rightSide" />
-        <Cell area="content" center>
-          <LoginStyledCard>
-            <CardHeader title={<FormattedMessage {...messages.title} />} />
-            <Formik
-              onSubmit={(values, actions) => {
-                onLogin(values, actions);
-              }}
-              validationSchema={yup.object().shape({
-                username: yup.string()
-                  .required((<FormattedMessage {...messages.validation.required} />)),
-                password: yup.string()
-                  .required((<FormattedMessage {...messages.validation.required} />)),
-              })}
-              render={(loginFormProps) => {
-                const { isSubmitting, dirty, isValid } = loginFormProps;
-                return (
-                  <Form>
-                    <LoginFieldGrid
-                      columns={1}
-                      rows="120px 120px 45px 100px"
-                      areas={[
-                        'username',
-                        'password',
-                        'forgotLink',
-                        'loginButton',
-                      ]}
-                    >
-                      <Cell>
-                        <TextField
-                          name="username"
-                          hintText={<FormattedMessage {...messages.hintText.username} />}
-                          floatingLabelText={<FormattedMessage {...messages.floatingLabelText.username} />}
-                          fullWidth
-                        />
-                      </Cell>
-                      <Cell>
-                        <TextField
-                          name="password"
-                          type="password"
-                          hintText={<FormattedMessage {...messages.hintText.password} />}
-                          floatingLabelText={<FormattedMessage {...messages.floatingLabelText.password} />}
-                          fullWidth
-                        />
-                      </Cell>
-                      <Cell>
-                        {isShowSampleUserLoginDetails ?
-                          <Link to="/ocp-ui/sample-user-login-details" target="_blank">
-                            <FormattedMessage {...messages.viewUserAccounts} />
-                          </Link> : <FormattedMessage {...messages.forgotLink} />
-                        }
-                      </Cell>
-                      <LoginButtonCell>
-                        <StyledRaisedButton
-                          type="submit"
-                          fullWidth
-                          disabled={!dirty || isSubmitting || !isValid}
-                        >{isSubmitting ?
-                          <FormattedMessage {...messages.authenticatingButton} /> :
-                          <FormattedMessage {...messages.loginButton} />}
-                        </StyledRaisedButton>
-                      </LoginButtonCell>
-                    </LoginFieldGrid>
-                  </Form>
-                );
-              }}
-            />
-          </LoginStyledCard>
-        </Cell>
-        <Cell area="footer" />
-      </Grid>
-    </div>
+    <Grid columns={3}>
+      <Cell left={2} top={3} center>
+        <LoginStyledCard>
+          <CardHeader title={<FormattedMessage {...messages.title} />} />
+          <Formik
+            onSubmit={(values, actions) => {
+              onLogin(values, actions);
+            }}
+            validationSchema={yup.object().shape({
+              username: yup.string()
+                .required((<FormattedMessage {...messages.validation.required} />)),
+              password: yup.string()
+                .required((<FormattedMessage {...messages.validation.required} />)),
+            })}
+            render={(loginFormProps) => {
+              const { isSubmitting, dirty, isValid } = loginFormProps;
+              return (
+                <Form>
+                  <LoginFieldGrid
+                    columns={1}
+                    rows="120px 120px 45px 100px"
+                    areas={[
+                      'username',
+                      'password',
+                      'forgotLink',
+                      'loginButton',
+                    ]}
+                  >
+                    <Cell>
+                      <TextField
+                        name="username"
+                        hintText={<FormattedMessage {...messages.hintText.username} />}
+                        floatingLabelText={<FormattedMessage {...messages.floatingLabelText.username} />}
+                        fullWidth
+                      />
+                    </Cell>
+                    <Cell>
+                      <TextField
+                        name="password"
+                        type="password"
+                        hintText={<FormattedMessage {...messages.hintText.password} />}
+                        floatingLabelText={<FormattedMessage {...messages.floatingLabelText.password} />}
+                        fullWidth
+                      />
+                    </Cell>
+                    <Cell>
+                      {isShowSampleUserLoginDetails ?
+                        <Link to="/ocp-ui/sample-user-login-details" target="_blank">
+                          <FormattedMessage {...messages.viewUserAccounts} />
+                        </Link> : <FormattedMessage {...messages.forgotLink} />
+                      }
+                    </Cell>
+                    <LoginButtonCell>
+                      <StyledRaisedButton
+                        type="submit"
+                        fullWidth
+                        disabled={!dirty || isSubmitting || !isValid}
+                      >{isSubmitting ?
+                        <FormattedMessage {...messages.authenticatingButton} /> :
+                        <FormattedMessage {...messages.loginButton} />}
+                      </StyledRaisedButton>
+                    </LoginButtonCell>
+                  </LoginFieldGrid>
+                </Form>
+              );
+            }}
+          />
+        </LoginStyledCard>
+      </Cell>
+    </Grid>
   );
 }
 
