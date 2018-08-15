3f890c3682a17fc0c028b1053491588429deca1d
JiahaoLi <Jiahao.Li@feisystems.com>
2018-08-14 22:41:36 -0400

    Implement ManageOrganization presentational component
    #21087: OCP Admin Enhancements

--- app/containers/ManageOrganizationPage/index.js
+++ app/containers/ManageOrganizationPage/index.js
@@ -11,10 +11,6 @@ import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
-import { Form, Formik } from 'formik';
-import yup from 'yup';
-import MenuItem from 'material-ui/MenuItem';
-import { Cell, Grid } from 'styled-css-grid';
 
 import injectSaga from 'utils/injectSaga';
 import {
@@ -34,53 +30,16 @@ import {
 } from 'containers/App/lookupSelectors';
 import { makeSelectOrganization } from 'containers/App/contextSelectors';
 import { getOrganization } from 'containers/App/contextActions';
-import GoBackButton from 'components/GoBackButton';
-import TextField from 'components/TextField';
-import SelectField from 'components/SelectField';
 import Page from 'components/Page';
 import PageHeader from 'components/PageHeader';
-import StyledRaisedButton from 'components/StyledRaisedButton';
 import PageContent from 'components/PageContent';
-import FormSubtitle from 'components/FormSubtitle';
-import FieldGroupGrid from 'components/FieldGroupGrid';
-import PrefixCell from 'components/FieldGroupGrid/PrefixCell';
-import MainCell from 'components/FieldGroupGrid/MainCell';
-import AddMultipleAddresses from 'components/AddMultipleAddresses';
-import AddMultipleTelecoms from 'components/AddMultipleTelecoms';
-import AddContactDetails from 'components/AddContactDetails';
+import ManageOrganization from 'components/ManageOrganization';
+import { createOrganization, updateOrganization } from './actions';
 import saga from './saga';
 import messages from './messages';
-import { createOrganization, updateOrganization } from './actions';
-import ManageOrganizationFormGrid from './ManageOrganizationFormGrid';
 
-const minimumNumberOfAddresses = 1;
-const minimumNumberOfTelecoms = 1;
 
 export class ManageOrganizationPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  static validationSchemaShape = {
-    name: yup.string()
-      .required((<FormattedMessage {...messages.validation.required} />)),
-    addresses: yup.array()
-      .required((<FormattedMessage {...messages.validation.required} />))
-      .min(minimumNumberOfAddresses, (
-        <FormattedMessage {...messages.validation.minAddresses} values={{ minimumNumberOfAddresses }} />)),
-    telecoms: yup.array()
-      .required((<FormattedMessage {...messages.validation.required} />))
-      .min(minimumNumberOfTelecoms, (
-        <FormattedMessage {...messages.validation.minTelecoms} values={{ minimumNumberOfTelecoms }} />)),
-    identifierSystem: yup.string()
-      .required((<FormattedMessage {...messages.validation.required} />)),
-    identifierValue: yup.string()
-      .required((<FormattedMessage {...messages.validation.required} />)),
-  };
-  static validationSchemaCreate = yup.object().shape(ManageOrganizationPage.validationSchemaShape);
-
-  static validationSchemaUpdate = yup.object().shape({
-    ...ManageOrganizationPage.validationSchemaShape,
-    status: yup.string()
-      .required((<FormattedMessage {...messages.validation.required} />)),
-  });
-
   constructor(props) {
     super(props);
     this.handleSubmitCreate = this.handleSubmitCreate.bind(this);
@@ -147,111 +106,17 @@ export class ManageOrganizationPage extends React.Component { // eslint-disable-
               <FormattedMessage {...messages.createModeTitle} />}
           />
           <PageContent>
-            <Formik
-              validationSchema={id ? ManageOrganizationPage.validationSchemaUpdate : ManageOrganizationPage.validationSchemaCreate}
+            <ManageOrganization
+              id={id}
               initialValues={initialValues}
-              onSubmit={editingOrganization ? this.handleSubmitUpdate : this.handleSubmitCreate}
-              render={(props) => {
-                const { isSubmitting, dirty, isValid, errors, values } = props;
-                const addAddressesProps = {
-                  uspsStates,
-                  errors,
-                  addresses: values.addresses,
-                };
-                const addTelecomsProps = {
-                  telecomSystems,
-                  telecomUses,
-                  errors,
-                  telecoms: values.telecoms,
-                };
-                return (
-                  <Form>
-                    <ManageOrganizationFormGrid>
-                      <Cell area="generalInformationSubtitle">
-                        <FormSubtitle margin="1vh 0 0 0">
-                          <FormattedMessage {...messages.subtitle} />
-                        </FormSubtitle>
-                      </Cell>
-                      <Cell area="name">
-                        <TextField
-                          name="name"
-                          floatingLabelText={<FormattedMessage {...messages.form.name} />}
-                          fullWidth
-                        />
-                      </Cell>
-                      <Cell area="identifierGroup">
-                        <FieldGroupGrid>
-                          <PrefixCell>
-                            <SelectField
-                              floatingLabelText={<FormattedMessage {...messages.form.identifierSystem} />}
-                              name="identifierSystem"
-                              fullWidth
-                            >
-                              {organizationIdentifierSystems && organizationIdentifierSystems.map(({ uri, display }) => (
-                                <MenuItem
-                                  key={uri}
-                                  value={uri}
-                                  primaryText={display}
-                                />))}
-                            </SelectField>
-                          </PrefixCell>
-                          <MainCell>
-                            <TextField
-                              floatingLabelText={<FormattedMessage {...messages.form.identifierValue} />}
-                              fullWidth
-                              name="identifierValue"
-                            />
-                          </MainCell>
-                        </FieldGroupGrid>
-                      </Cell>
-                      {id &&
-                      <Cell area="status">
-                        <SelectField
-                          floatingLabelText={<FormattedMessage {...messages.form.status} />}
-                          fullWidth
-                          name="status"
-                        >
-                          {organizationStatuses && organizationStatuses.map(({ code, display }) => (
-                            <MenuItem
-                              key={code.toString()}
-                              value={code.toString()}
-                              primaryText={display}
-                            />))}
-                        </SelectField>
-                      </Cell>}
-                      <Cell area="addresses">
-                        <AddMultipleAddresses{...addAddressesProps} />
-                      </Cell>
-                      <Cell area="telecoms">
-                        <AddMultipleTelecoms {...addTelecomsProps} />
-                      </Cell>
-                      <Cell area="contacts">
-                        <AddContactDetails />
-                      </Cell>
-                      <Cell area="buttonGroup">
-                        <Grid columns={2}>
-                          <Cell>
-                            <StyledRaisedButton
-                              fullWidth
-                              type="submit"
-                              disabled={!dirty || isSubmitting || !isValid}
-                            >
-                              {isSubmitting ?
-                                <FormattedMessage {...messages.form.savingButton} /> :
-                                <FormattedMessage {...messages.form.saveButton} />}
-                            </StyledRaisedButton>
-                          </Cell>
-                          <Cell>
-                            <GoBackButton
-                              label={<FormattedMessage {...messages.form.cancelButton} />}
-                            />
-                          </Cell>
-                        </Grid>
-                      </Cell>
-                    </ManageOrganizationFormGrid>
-                  </Form>
-                );
-              }}
+              editingOrganization={editingOrganization}
+              uspsStates={uspsStates}
+              organizationIdentifierSystems={organizationIdentifierSystems}
+              organizationStatuses={organizationStatuses}
+              telecomSystems={telecomSystems}
+              telecomUses={telecomUses}
+              onSubmitCreate={this.handleSubmitCreate}
+              onSubmitUpdate={this.handleSubmitUpdate}
             />
           </PageContent>
         </div>
