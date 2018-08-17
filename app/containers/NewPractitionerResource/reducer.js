602fb42a0e4dd733ad08da8fc512180a989d244a
JiahaoLi <Jiahao.Li@feisystems.com>
2018-08-17 16:43:22 -0400

    Implement to add findPractitioner reducer
    #21087: OCP Admin Enhancements

--- app/containers/NewPractitionerResource/reducer.js
+++ app/containers/NewPractitionerResource/reducer.js
@@ -5,16 +5,33 @@
  */
 
 import { fromJS } from 'immutable';
-import {
-  DEFAULT_ACTION,
-} from './constants';
+import { FIND_PRACTITIONER, FIND_PRACTITIONER_ERROR, FIND_PRACTITIONER_SUCCESS } from './constants';
 
-const initialState = fromJS({});
+
+const initialState = fromJS({
+  loading: false,
+  practitioner: null,
+  queryData: null,
+  error: false,
+});
 
 function newPractitionerResourceReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case FIND_PRACTITIONER:
+      return state
+        .set('loading', true)
+        .setIn(['queryData', 'firstName'], action.firstName)
+        .setIn(['queryData', 'lastName'], action.lastName)
+        .setIn(['queryData', 'identifierType'], action.identifierType)
+        .setIn(['queryData', 'identifier'], action.identifier);
+    case FIND_PRACTITIONER_SUCCESS:
+      return state
+        .set('loading', false)
+        .set('practitioner', fromJS(action.practitioner));
+    case FIND_PRACTITIONER_ERROR:
+      return state
+        .set('loading', false)
+        .set('error', action.error);
     default:
       return state;
   }
--- app/containers/NewPractitionerResource/tests/reducer.test.js
+++ app/containers/NewPractitionerResource/tests/reducer.test.js
@@ -1,9 +1,13 @@
-
 import { fromJS } from 'immutable';
 import newPractitionerResourceReducer from '../reducer';
 
 describe('newPractitionerResourceReducer', () => {
   it('returns the initial state', () => {
-    expect(newPractitionerResourceReducer(undefined, {})).toEqual(fromJS({}));
+    expect(newPractitionerResourceReducer(undefined, {})).toEqual(fromJS({
+      loading: false,
+      practitioner: null,
+      queryData: null,
+      error: false,
+    }));
   });
 });
