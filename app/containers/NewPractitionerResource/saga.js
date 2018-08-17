04e2126e857155739ecc19d0c521208166607837
JiahaoLi <Jiahao.Li@feisystems.com>
2018-08-17 16:54:57 -0400

    Implement to add findPractitioner middleware
    #21087: OCP Admin Enhancements

--- app/containers/NewPractitionerResource/saga.js
+++ app/containers/NewPractitionerResource/saga.js
@@ -1,6 +1,30 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
 
-// Individual exports for testing
-export default function* defaultSaga() {
-  // See example in containers/HomePage/saga.js
+import { getErrorDetail } from 'containers/App/helpers';
+import { findPractitionerError, findPractitionerSuccess } from './actions';
+import { findPractitioner } from './api';
+import { FIND_PRACTITIONER } from './constants';
+
+export function* findPractitionerSaga({ firstName, lastName, identifierType, identifier, handleSubmitting }) {
+  try {
+    const practitioner = yield call(findPractitioner, firstName, lastName, identifierType, identifier);
+    yield put(findPractitionerSuccess(practitioner));
+    yield call(handleSubmitting);
+  } catch (error) {
+    yield put(findPractitionerError(getErrorDetail(error)));
+    yield call(handleSubmitting);
+  }
+}
+
+export function* watchFindPractitionerSaga() {
+  yield takeLatest(FIND_PRACTITIONER, findPractitionerSaga);
+}
+
+/**
+ * Root saga manages watcher lifecycle
+ */
+export default function* rootSaga() {
+  yield all([
+    watchFindPractitionerSaga(),
+  ]);
 }
