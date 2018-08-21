f373bf5580037fc3b645841f16d8eccfef1a4412
JiahaoLi <Jiahao.Li@feisystems.com>
2018-08-20 20:57:24 -0400

    Implement initial AddAssociateOrganizations component
    #21070: Organization Admin Enhancements

---
+++ app/components/AddAssociateOrganizations/index.js
@@ -0,0 +1,69 @@
+/**
+ *
+ * AddAssociateOrganizations
+ *
+ */
+
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { FieldArray } from 'formik';
+import { DialogTitle } from 'material-ui-next/Dialog';
+import teal from 'material-ui-next/colors/teal';
+
+import FormSubtitle from 'components/FormSubtitle';
+import AddNewItemButton from 'components/PanelToolbar/AddNewItemButton';
+import StyledAddCircleIcon from 'components/StyledAddCircleIcon';
+import StyledDialog from 'components/StyledDialog';
+import messages from './messages';
+
+
+class AddAssociateOrganizations extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      dialogOpen: false,
+    };
+    this.handleOpenDialog = this.handleOpenDialog.bind(this);
+    this.handleCloseDialog = this.handleCloseDialog.bind(this);
+  }
+
+  handleOpenDialog() {
+    this.setState({ dialogOpen: true });
+  }
+
+  handleCloseDialog() {
+    this.setState({
+      dialogOpen: false,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <FormSubtitle margin="1vh 0 0 0">
+          <FormattedMessage {...messages.header} />
+        </FormSubtitle>
+        <AddNewItemButton color="primary" fontWeight="bold" fontSize="15px" onClick={this.handleOpenDialog}>
+          <StyledAddCircleIcon color={teal['500']} />
+          <FormattedMessage {...messages.addOrganizationButton} />
+        </AddNewItemButton>
+        <FieldArray
+          name="practitionerRoles"
+          render={() => (
+            <div>
+              <StyledDialog maxWidth="md" fullWidth open={this.state.dialogOpen}>
+                <DialogTitle>
+                  <FormattedMessage {...messages.title} />
+                </DialogTitle>
+              </StyledDialog>
+            </div>
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+AddAssociateOrganizations.propTypes = {};
+
+export default AddAssociateOrganizations;
---
+++ app/components/AddAssociateOrganizations/messages.js
@@ -0,0 +1,21 @@
+/*
+ * AddAssociateOrganizations Messages
+ *
+ * This contains all the text for the AddAssociateOrganizations component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'ocpui.components.AddAssociateOrganizations.header',
+    defaultMessage: 'Associate Organizations',
+  },
+  addOrganizationButton: {
+    id: 'ocpui.components.AddAssociateOrganizations.addOrganizationButton',
+    defaultMessage: 'Add Organization',
+  },
+  title: {
+    id: 'ocpui.components.AddAssociateOrganizations.title',
+    defaultMessage: 'Search Organization',
+  },
+});
