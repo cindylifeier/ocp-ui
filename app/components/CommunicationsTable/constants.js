bc04b59fa99e5158eb8b29c18177900c771dd987
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-08-24 23:07:37 -0400

    Adjust column width to allow space for sender column
    # 20583 - Communication functionalty fixes and gaps

--- app/components/CommunicationsTable/constants.js
+++ app/components/CommunicationsTable/constants.js
@@ -1,5 +1,5 @@
 
-export const EXPANDED_TABLE_COLUMNS = 'repeat(8, 1fr) 50px';
-export const SUMMARIZED_TABLE_COLUMNS = '50px 6fr 3fr 2fr';
+export const EXPANDED_TABLE_COLUMNS = '.5fr repeat(8, 1fr) 1fr';
+export const SUMMARIZED_TABLE_COLUMNS = '.5fr 3fr 2fr 2fr 1fr';
 
 export const SUMMARY_VIEW_WIDTH = 1000; // The width of the panel in pixels
