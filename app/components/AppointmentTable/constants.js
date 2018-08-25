c430f912e9ea664ebf0ced2f0440e67a877731ab
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-08-24 23:42:55 -0400

    Adjust appointment table columns
    # 20583 - Communication functionalty fixes and gaps

--- app/components/AppointmentTable/constants.js
+++ app/components/AppointmentTable/constants.js
@@ -3,6 +3,6 @@ export const EXPANDED_TABLE_COLUMNS = '50px repeat(7, 1fr) 50px';
 export const SUMMARIZED_TABLE_COLUMNS = '50px repeat(4, 1fr) 50px';
 
 export const PATIENT_WORKSPACE_EXPANDED_TABLE_COLUMNS = 'repeat(7, 1fr) 50px';
-export const PATIENT_WORKSPACE_SUMMARIZED_TABLE_COLUMNS = 'repeat(4, 1fr) 50px';
+export const PATIENT_WORKSPACE_SUMMARIZED_TABLE_COLUMNS = '.5fr repeat(2, 1fr) 1.5fr 1fr';
 
 export const SUMMARY_VIEW_WIDTH = 695; // The width of the panel in pixels
