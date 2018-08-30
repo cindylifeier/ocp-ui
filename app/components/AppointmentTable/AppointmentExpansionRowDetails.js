30028237cbd0255302353e2d762e54464ea1873f
Shruti Rao <shruti.rao@feisystems.com>
2018-08-30 13:02:23 -0400

    Add contact details in appointment table expansion details
    #21269:Appointment Bugs and enhancements

--- app/components/AppointmentTable/AppointmentExpansionRowDetails.js
+++ app/components/AppointmentTable/AppointmentExpansionRowDetails.js
@@ -1,33 +1,48 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
-import { Cell, Grid } from 'styled-css-grid';
-import startCase from 'lodash/startCase';
-import camelCase from 'lodash/camelCase';
-import uniqueId from 'lodash/uniqueId';
+import Align from 'components/Align/index';
+import InfoSection from 'components/InfoSection';
+import StyledText from 'components/StyledText/index';
 
 import Table from 'components/Table';
 import TableHeader from 'components/TableHeader';
 import TableHeaderColumn from 'components/TableHeaderColumn';
 import TableRow from 'components/TableRow';
 import TableRowColumn from 'components/TableRowColumn';
-import InfoSection from 'components/InfoSection';
 import TextLabelGroup from 'components/TextLabelGroup';
-import StyledText from 'components/StyledText/index';
-import Align from 'components/Align/index';
+import camelCase from 'lodash/camelCase';
+import startCase from 'lodash/startCase';
+import uniqueId from 'lodash/uniqueId';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Cell, Grid } from 'styled-css-grid';
 import { getRoleName } from 'utils/CommunicationUtils';
 import messages from './messages';
 
 function AppointmentExpansionRowDetails({ participants, appointmentType }) {
-  const column = '2.5fr 3fr 3.5fr 3fr';
+  const column = '2.5fr 3fr 3fr 3.5fr 3fr';
   return (
     <div>
       <InfoSection>
-        <Grid columns={'100%'} justifyContent="space-between">
+        <Grid columns={'25% 25% 25% 25%'} justifyContent="space-between">
           <Cell>
             <TextLabelGroup
               label={<FormattedMessage {...messages.expansionRowDetails.appointmentType} />}
               text={appointmentType}
+            />
+          </Cell>
+          <Cell>
+            <TextLabelGroup
+              label={<FormattedMessage {...messages.expansionRowDetails.serviceCategory} />}
+            />
+          </Cell>
+          <Cell>
+            <TextLabelGroup
+              label={<FormattedMessage {...messages.expansionRowDetails.serviceType} />}
+            />
+          </Cell>
+          <Cell>
+            <TextLabelGroup
+              label={<FormattedMessage {...messages.expansionRowDetails.speciality} />}
             />
           </Cell>
         </Grid>
@@ -43,18 +58,20 @@ function AppointmentExpansionRowDetails({ participants, appointmentType }) {
       </InfoSection>
       <Table>
         <TableHeader columns={column}>
-          <TableHeaderColumn><FormattedMessage {...messages.expansionRowDetails.appointmentName} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.expansionRowDetails.appointmentParticipantName} /></TableHeaderColumn>
           <TableHeaderColumn><FormattedMessage {...messages.expansionRowDetails.participantType} /></TableHeaderColumn>
+          <TableHeaderColumn><FormattedMessage {...messages.expansionRowDetails.contactDetails} /></TableHeaderColumn>
           <TableHeaderColumn><FormattedMessage {...messages.expansionRowDetails.participationType} /></TableHeaderColumn>
           <TableHeaderColumn><FormattedMessage {...messages.expansionRowDetails.status} /></TableHeaderColumn>
         </TableHeader>
         {participants && participants.length > 0 ?
           participants.map((participant) => {
-            const { actorName, actorReference, participationStatusCode, participationTypeDisplay } = participant;
+            const { actorName, actorReference, participationStatusCode, participationTypeDisplay, phone, email } = participant;
             return (
               <TableRow key={uniqueId()} columns={column}>
                 <TableRowColumn>{actorName}</TableRowColumn>
                 <TableRowColumn>{getRoleName(actorReference)}</TableRowColumn>
+                <TableRowColumn>Phone: {phone} <br></br> Email: {email}</TableRowColumn>
                 <TableRowColumn>{startCase(camelCase(participationTypeDisplay))}</TableRowColumn>
                 <TableRowColumn>{startCase(camelCase(participationStatusCode))}</TableRowColumn>
               </TableRow>
