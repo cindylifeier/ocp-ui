d427699fb3d0ce3cad512f6873b53370364f2448
JiahaoLi <Jiahao.Li@feisystems.com>
2018-09-01 23:26:25 -0400

    Implement to add search participants reducer in AddAppointmentParticipant
    #21269: Appointment Bugs and enhancements

--- app/containers/AddAppointmentParticipant/reducer.js
+++ app/containers/AddAppointmentParticipant/reducer.js
@@ -9,12 +9,17 @@ import {
   GET_HEALTHCARE_SERVICE_REFERENCES_SUCCESS,
   GET_LOCATION_REFERENCES_SUCCESS,
   GET_PRACTITIONER_REFERENCES_SUCCESS,
+  SEARCH_PARTICIPANT_SUCCESS,
 } from './constants';
 
 const initialState = fromJS({
   healthcareServices: null,
   locations: null,
   practitioners: null,
+  loading: false,
+  data: null,
+  currentPage: 0,
+  totalNumberOfPages: 0,
 });
 
 function addAppointmentParticipantReducer(state = initialState, action) {
@@ -28,6 +33,14 @@ function addAppointmentParticipantReducer(state = initialState, action) {
     case GET_PRACTITIONER_REFERENCES_SUCCESS:
       return state
         .set('practitioners', action.practitioners);
+    case SEARCH_PARTICIPANT_SUCCESS:
+      return state
+        .set('loading', false)
+        .set('data', fromJS(action.participants.elements))
+        .set('totalElements', action.participants.totalElements)
+        .set('currentPageSize', action.participants.currentPageSize)
+        .set('totalNumberOfPages', action.participants.totalNumberOfPages)
+        .set('currentPage', action.participants.currentPage);
     default:
       return state;
   }
