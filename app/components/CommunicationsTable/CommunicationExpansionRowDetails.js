d58247bdfd116684c61e1af36e8ddcf35fbad798
Shruti Rao <shruti.rao@feisystems.com>
2018-09-06 11:38:08 -0400

    Remove recepient table from expansion details
    #21269:Appointment Bugs and enhancements

--- app/components/CommunicationsTable/CommunicationExpansionRowDetails.js
+++ app/components/CommunicationsTable/CommunicationExpansionRowDetails.js
@@ -1,20 +1,14 @@
-import React from 'react';
+import InfoSection from 'components/InfoSection';
+import TextLabelGroup from 'components/TextLabelGroup';
 import PropTypes from 'prop-types';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Cell, Grid } from 'styled-css-grid';
-import Table from 'components/Table';
-import TableHeader from 'components/TableHeader';
-import TableHeaderColumn from 'components/TableHeaderColumn';
-import TableRow from 'components/TableRow';
-import TableRowColumn from 'components/TableRowColumn';
-import InfoSection from 'components/InfoSection';
-import StyledText from 'components/StyledText';
-import TextLabelGroup from 'components/TextLabelGroup';
 import messages from './messages';
 
 
 function CommunicationExpansionRowDetails({ communication }) {
-  const { payloadContent, note, notDoneReasonValue, recipient } = communication;
+  const { payloadContent, note, notDoneReasonValue } = communication;
   return (
     <div>
       <InfoSection>
@@ -40,29 +34,7 @@ function CommunicationExpansionRowDetails({ communication }) {
             />
           </Cell>
         </Grid>
-        <Grid columns={'100%'} justifyContent="space-between">
-          <Cell>
-            <StyledText >
-              {<FormattedMessage {...messages.expansionRowDetails.recipients} />}
-            </StyledText>
-          </Cell>
-        </Grid>
       </InfoSection>
-      <Table>
-        <TableHeader columns={'4fr 4fr'}>
-          <TableHeaderColumn><FormattedMessage {...messages.expansionRowDetails.columnHeaderName} /></TableHeaderColumn>
-          <TableHeaderColumn><FormattedMessage {...messages.expansionRowDetails.columnHeaderRole} /></TableHeaderColumn>
-        </TableHeader>
-        {recipient && recipient.map((entry) => {
-          const { reference, display } = entry;
-          return (
-            <TableRow key={reference} columns={'4fr 4fr'}>
-              <TableRowColumn>{display}</TableRowColumn>
-              <TableRowColumn>{reference}</TableRowColumn>
-            </TableRow>
-          );
-        })}
-      </Table>
     </div>
   );
 }
