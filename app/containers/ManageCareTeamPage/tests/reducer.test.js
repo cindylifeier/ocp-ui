4582d7650be924ab305a41bd428020c7a9160236
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-09-06 22:31:19 -0400

    Fix fail test
    # 21333 - OCP EOC and Care Team Fixes/Enhancements

--- app/containers/ManageCareTeamPage/tests/reducer.test.js
+++ app/containers/ManageCareTeamPage/tests/reducer.test.js
@@ -3,6 +3,6 @@ import manageCareTeamPageReducer from '../reducer';
 
 describe('manageCareTeamPageReducer', () => {
   it('returns the initial state', () => {
-    expect(manageCareTeamPageReducer(undefined, {})).toEqual(fromJS({ patient: null, careTeam: null }));
+    expect(manageCareTeamPageReducer(undefined, {})).toEqual(fromJS({ patient: null, careTeam: null, eventTypes: [] }));
   });
 });
