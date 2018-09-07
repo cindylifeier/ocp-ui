27e99ca0ff53815950d5a7fb699358da3e3f3715
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2018-09-06 22:03:55 -0400

    Implement actions and selector for getting episode of care
    # 21333 - OCP EOC and Care Team Fixes/Enhancements

--- app/containers/ManageCareTeamPage/actions.js
+++ app/containers/ManageCareTeamPage/actions.js
@@ -9,6 +9,8 @@ import {
   GET_CARE_TEAM_SUCCESS,
   INITIALIZE_MANAGE_CARE_TEAM,
   SAVE_CARE_TEAM,
+  GET_EVENT_TYPES, GET_EVENT_TYPES_ERROR,
+  GET_EVENT_TYPES_SUCCESS,
 } from './constants';
 
 export function initializeManageCareTeam() {
@@ -36,5 +38,27 @@ export function saveCareTeam(careTeamFormData, handleSubmitting) {
     type: SAVE_CARE_TEAM,
     careTeamFormData,
     handleSubmitting,
+  };
+}
+
+
+export function getEventTypes(patientId) {
+  return {
+    type: GET_EVENT_TYPES,
+    patientId,
+  };
+}
+
+export function getEventTypesSuccess(eventTypes) {
+  return {
+    type: GET_EVENT_TYPES_SUCCESS,
+    eventTypes,
+  };
+}
+
+export function getEventTypesError(err) {
+  return {
+    type: GET_EVENT_TYPES_ERROR,
+    err,
   };
 }
--- app/containers/ManageCareTeamPage/constants.js
+++ app/containers/ManageCareTeamPage/constants.js
@@ -14,6 +14,9 @@ export const SAVE_CARE_TEAM = 'ocpui/ManageCareTeamPage/SAVE_CARE_TEAM';
 export const GET_CARE_TEAM = 'ocpui/ManageCareTeamPage/GET_CARE_TEAM';
 export const GET_CARE_TEAM_SUCCESS = 'ocpui/ManageCareTeamPage/GET_CARE_TEAM_SUCCESS';
 
+export const GET_EVENT_TYPES = 'ocpui/ManageCareTeamPage/GET_EVENT_TYPES';
+export const GET_EVENT_TYPES_SUCCESS = 'ocpui/ManageCareTeamPage/GET_EVENT_TYPES_SUCCESS';
+export const GET_EVENT_TYPES_ERROR = 'ocpui/ManageCareTeamPage/GET_EVENT_TYPES_ERROR';
 /**
  *  Other constants
  *
